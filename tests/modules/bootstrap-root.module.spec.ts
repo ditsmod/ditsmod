86e261e35551d36b5329d1e07a6a35fbcebf249c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-01-31 00:53:42 +0200

    Added tests for bootstrap

--- tests/modules/bootstrap-root.module.spec.ts
+++ tests/modules/bootstrap-root.module.spec.ts
@@ -8,6 +8,7 @@ import { BootstrapRootModule } from '../../src/modules/bootstrap-root.module';
 import { ModuleType, Logger, HttpModule, ServerOptions, Server, Router } from '../../src/types/types';
 import { RootModuleDecorator, RootModule } from '../../src/types/decorators';
 import { PreRequest } from '../../src/services/pre-request.service';
+import { defaultProvidersPerApp } from '../../src/types/default-options';
 
 describe('BootstrapRootModule', () => {
   class MockBootstrapRootModule extends BootstrapRootModule {
@@ -35,37 +36,57 @@ describe('BootstrapRootModule', () => {
     }
   }
 
-  let mockBs: MockBootstrapRootModule;
+  let mock: MockBootstrapRootModule;
+  class SomeControllerClass {}
+  class ClassWithoutDecorators {}
 
   beforeEach(() => {
-    mockBs = new MockBootstrapRootModule();
+    mock = new MockBootstrapRootModule();
   });
-
-  class SomeControllerClass {}
-  @RootModule({ controllers: [SomeControllerClass] })
-  class ClassWithDecorators {}
-
-  class ClassWithoutDecorators {}
 
   describe('getAppModuleMetadata()', () => {
     it('should returns ClassWithDecorators metadata', () => {
-      const metadata = mockBs.getAppModuleMetadata(ClassWithDecorators);
+      @RootModule({ controllers: [SomeControllerClass] })
+      class ClassWithDecorators {}
+      const metadata = mock.getAppModuleMetadata(ClassWithDecorators);
       expect(metadata).toEqual(new RootModule({ controllers: [SomeControllerClass] }));
     });
 
     it('should not returns any metadata', () => {
-      const metadata = mockBs.getAppModuleMetadata(ClassWithoutDecorators);
+      const metadata = mock.getAppModuleMetadata(ClassWithoutDecorators);
       expect(metadata).toBeUndefined();
     });
   });
 
   describe('mergeMetadata()', () => {
-    it('should merge default metatada with ClassWithDecorators metadata', () => {
-      const metadata = mockBs.mergeMetadata(ClassWithDecorators);
+    it('should set default metatada', () => {
+      @RootModule()
+      class ClassWithDecorators {}
+      const metadata = mock.mergeMetadata(ClassWithDecorators);
       expect(metadata.serverName).toEqual('restify-ts');
       expect(metadata.serverOptions).toEqual({});
       expect(metadata.httpModule).toBeDefined();
-      expect(metadata.providersPerApp && metadata.providersPerApp.length).toBe(5);
+      expect(metadata.providersPerApp).toEqual(defaultProvidersPerApp);
+      expect(metadata.controllers).toEqual(undefined);
+      expect(metadata.exports).toEqual(undefined);
+      expect(metadata.imports).toEqual(undefined);
+      expect(metadata.listenOptions).toBeDefined();
+      expect(metadata.providersPerMod).toEqual(undefined);
+      expect(metadata.providersPerReq).toEqual(undefined);
+    });
+
+    it('should merge default metatada with ClassWithDecorators metadata', () => {
+      @RootModule({
+        controllers: [SomeControllerClass],
+        providersPerApp: [ClassWithoutDecorators]
+      })
+      class ClassWithDecorators {}
+      const metadata = mock.mergeMetadata(ClassWithDecorators);
+      expect(metadata.serverName).toEqual('restify-ts');
+      expect(metadata.serverOptions).toEqual({});
+      expect(metadata.httpModule).toBeDefined();
+      expect(metadata.providersPerApp).toEqual([...defaultProvidersPerApp, ClassWithoutDecorators]);
+      // Ignore controllers - it's intended behavior.
       expect(metadata.controllers).toEqual(undefined);
       expect(metadata.exports).toEqual(undefined);
       expect(metadata.imports).toEqual(undefined);
@@ -76,35 +97,41 @@ describe('BootstrapRootModule', () => {
 
     it('ClassWithoutDecorators should not have metatada', () => {
       const msg = `Module build failed: module "ClassWithoutDecorators" does not have the "@RootModule()" decorator`;
-      expect(() => mockBs.mergeMetadata(ClassWithoutDecorators)).toThrowError(msg);
+      expect(() => mock.mergeMetadata(ClassWithoutDecorators)).toThrowError(msg);
     });
   });
 
   describe('checkSecureServerOption()', () => {
+    @RootModule({
+      controllers: [SomeControllerClass],
+      providersPerApp: [ClassWithoutDecorators]
+    })
+    class ClassWithDecorators {}
+
     it('should not to throw with http2 and isHttp2SecureServer == true', () => {
-      mockBs.serverOptions = { isHttp2SecureServer: true };
-      mockBs.httpModule = http2;
-      expect(() => mockBs.checkSecureServerOption(ClassWithDecorators)).not.toThrow();
+      mock.serverOptions = { isHttp2SecureServer: true };
+      mock.httpModule = http2;
+      expect(() => mock.checkSecureServerOption(ClassWithDecorators)).not.toThrow();
     });
 
     it('should to throw with http and isHttp2SecureServer == true', () => {
-      mockBs.serverOptions = { isHttp2SecureServer: true };
-      mockBs.httpModule = http;
+      mock.serverOptions = { isHttp2SecureServer: true };
+      mock.httpModule = http;
       const msg = 'serverModule.createSecureServer() not found (see ClassWithDecorators settings)';
-      expect(() => mockBs.checkSecureServerOption(ClassWithDecorators)).toThrowError(msg);
+      expect(() => mock.checkSecureServerOption(ClassWithDecorators)).toThrowError(msg);
     });
 
     it('should not to throw with http and isHttp2SecureServer == false', () => {
-      mockBs.httpModule = http;
+      mock.httpModule = http;
       const msg = 'serverModule.createSecureServer() not found (see ClassWithDecorators settings)';
-      expect(() => mockBs.checkSecureServerOption(ClassWithDecorators)).not.toThrowError(msg);
+      expect(() => mock.checkSecureServerOption(ClassWithDecorators)).not.toThrowError(msg);
     });
 
     it('should to throw with https and isHttp2SecureServer == true', () => {
-      mockBs.serverOptions = { isHttp2SecureServer: true };
-      mockBs.httpModule = https;
+      mock.serverOptions = { isHttp2SecureServer: true };
+      mock.httpModule = https;
       const msg = 'serverModule.createSecureServer() not found (see ClassWithDecorators settings)';
-      expect(() => mockBs.checkSecureServerOption(ClassWithDecorators)).toThrowError(msg);
+      expect(() => mock.checkSecureServerOption(ClassWithDecorators)).toThrowError(msg);
     });
   });
 });
---
+++ tests/modules/bootstrap.module.spec.ts
@@ -0,0 +1,87 @@
+import { Provider } from 'ts-di';
+
+import { BootstrapModule } from '../../src/modules/bootstrap.module';
+import { ModuleType } from '../../src/types/types';
+import { NormalizedProvider, normalizeProviders } from '../../src/utils/ng-utils';
+import { Module } from '../../src/types/decorators';
+import { defaultProvidersPerReq } from '../../src/types/default-options';
+
+fdescribe('BootstrapModule', () => {
+  class MockBootstrapModule extends BootstrapModule {
+    getRawModuleMetadata(mod: ModuleType) {
+      return super.getRawModuleMetadata(mod);
+    }
+
+    mergeMetadata(mod: ModuleType) {
+      return super.mergeMetadata(mod);
+    }
+
+    importProviders(mod: ModuleType, soughtProvider?: NormalizedProvider) {
+      return super.importProviders(mod, soughtProvider);
+    }
+
+    setRoutes() {
+      super.setRoutes();
+    }
+  }
+
+  let mock: MockBootstrapModule;
+  beforeEach(() => {
+    mock = new MockBootstrapModule(null, null, null);
+  });
+
+  class ClassWithoutDecorators {}
+  class SomeControllerClass {}
+
+  describe('getRawModuleMetadata()', () => {
+    it('should returns ClassWithDecorators metadata', () => {
+      @Module({ controllers: [SomeControllerClass] })
+      class ClassWithDecorators {}
+      const metadata = mock.getRawModuleMetadata(ClassWithDecorators);
+      expect(metadata).toEqual(new Module({ controllers: [SomeControllerClass] }));
+    });
+
+    it('should not returns any metadata', () => {
+      const metadata = mock.getRawModuleMetadata(ClassWithoutDecorators);
+      expect(metadata).toBeUndefined();
+    });
+  });
+
+  describe('mergeMetadata()', () => {
+    it('should set default metatada', () => {
+      @Module()
+      class ClassWithDecorators {}
+      const metadata = mock.mergeMetadata(ClassWithDecorators);
+      expect(metadata.controllers).toEqual([]);
+      expect(metadata.exports).toEqual([]);
+      expect(metadata.imports).toEqual([]);
+      expect(metadata.providersPerMod).toEqual([]);
+      expect(metadata.providersPerReq).toEqual(defaultProvidersPerReq);
+    });
+
+    it('should merge default metatada with ClassWithDecorators metadata', () => {
+      @Module({
+        controllers: [SomeControllerClass],
+        providersPerReq: [ClassWithoutDecorators]
+      })
+      class ClassWithDecorators {}
+      const metadata = mock.mergeMetadata(ClassWithDecorators);
+      expect(metadata.controllers).toEqual([SomeControllerClass]);
+      expect(metadata.exports).toEqual([]);
+      expect(metadata.imports).toEqual([]);
+      expect(metadata.providersPerMod).toEqual([]);
+      expect(metadata.providersPerReq).toEqual([...defaultProvidersPerReq, ClassWithoutDecorators]);
+    });
+
+    it('ClassWithoutDecorators should not have metatada', () => {
+      const msg = `Module build failed: module "ClassWithoutDecorators" does not have the "@Module()" decorator`;
+      expect(() => mock.mergeMetadata(ClassWithoutDecorators)).toThrowError(msg);
+    });
+  });
+
+  describe('importProviders()', () => {
+    it('should', () => {});
+  });
+
+  describe('setRoutes()', () => {});
+});
