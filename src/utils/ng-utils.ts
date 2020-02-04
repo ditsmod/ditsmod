cc6cd38a3e462f7629b3842cc7175471d0a441c5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-02-04 12:47:51 +0200

    Removed RouterModule

--- src/utils/ng-utils.ts
+++ src/utils/ng-utils.ts
@@ -6,11 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { InjectionToken, Provider, Type, ValueProvider, ClassProvider, ExistingProvider, FactoryProvider } from 'ts-di';
+import { Provider, Type, ValueProvider, ClassProvider, ExistingProvider, FactoryProvider } from 'ts-di';
 import { format } from 'util';
-
-import { Route } from '../types/types';
-import { AppRouter } from '../app-router';
 
 /**
  * Flattens an array.
@@ -34,44 +31,6 @@ export function flatten(list: any[], dst?: any[]): any[] {
     }
   }
   return dst;
-}
-
-/**
- * The [DI token](guide/glossary/#di-token) for a router configuration.
- */
-export const ROUTES = new InjectionToken<Route[][]>('ROUTES');
-
-/**
- * Registers a [DI provider](guide/glossary#provider) for a set of routes.
- *
- * ```
- * @NgModule({
- *   imports: [RouterModule.forChild(ROUTES)],
- *   providers: [provideRoutes(EXTRA_ROUTES)]
- * })
- * class MyNgModule {}
- * ```
- *
- * @param routes The route configuration to provide.
- */
-export function provideRoutes(routes: Route[]): any {
-  return [{ provide: ROUTES, multi: true, useValue: routes }];
-}
-
-/**
- * A [DI token](guide/glossary/#di-token) for the router service.
- */
-export const ROUTER_CONFIGURATION = new InjectionToken<void>('ROUTER_CONFIGURATION');
-
-export const ROUTER_FORROOT_GUARD = new InjectionToken<void>('ROUTER_FORROOT_GUARD');
-
-export function provideForRootGuard(router: AppRouter): any {
-  if (router) {
-    throw new Error(
-      `RouterModule.forRoot() called twice. Lazy loaded modules should use RouterModule.forChild() instead.`
-    );
-  }
-  return 'guarded';
 }
 
 /**
