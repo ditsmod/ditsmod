49f43059fcbcfefd330182b98eecb78e158fb43b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-05-19 18:37:02 +0300

    Fixed examples

--- examples/2-nested-routes/README.md
+++ examples/2-nested-routes/README.md
@@ -3,5 +3,4 @@ You can to check work:
 ```bash
 curl -isS localhost:8080/api/v1
 curl -isS localhost:8080/api/v2
-curl -isS localhost:8080/api/v2/one/two
 ```
--- examples/2-nested-routes/app/v2/second.module.ts
+++ examples/2-nested-routes/app/v2/second.module.ts
@@ -4,17 +4,5 @@ import { HelloWorldController } from './hello-world.controller';
 
 @Module({
   controllers: [HelloWorldController],
-  routes: [
-    {
-      path: 'one',
-      controller: HelloWorldController,
-      children: [
-        {
-          path: 'two',
-          controller: HelloWorldController,
-        },
-      ],
-    },
-  ],
 })
 export class SecondModule {}
