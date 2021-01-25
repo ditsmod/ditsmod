8cdf84ea22b4bb6c17bfdab2e10ff3036fcab0d4
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-01-25 21:57:05 +0200

    refactor(core): make get-body default body parser

--- src/services/body-parser.ts
+++ src/services/body-parser.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@ts-stack/di';
-import zlib = require('zlib');
+import { parse } from 'get-body';
 
 import { ObjectAny } from '../types/types';
 import { BodyParserConfig } from '../types/types';
@@ -18,84 +18,11 @@ export class BodyParser {
     protected log: Logger
   ) {}
 
-  getRawBody() {
-    if (this.rawBody !== undefined) {
-      return Promise.resolve(this.rawBody);
-    }
-
-    return new Promise<Buffer>((resolve, reject) => {
-      const nodeReq = this.nodeReq;
-      const headers = nodeReq.headers;
-
-      if (
-        !headers['content-type'] ||
-        ((!headers['content-length'] || headers['content-length'] == '0') &&
-          headers['transfer-encoding'] != 'chunked') ||
-        headers['content-type'].includes('multipart/form-data') ||
-        headers['content-type'].includes('application/octet-stream')
-      ) {
-        resolve(null);
-        return;
-      }
-
-      nodeReq.once('error', reject);
-
-      let bytesReceived = 0;
-      let gz: zlib.Gunzip;
-      const buffers: Uint8Array[] = [];
-      const done = () => {
-        this.rawBody = Buffer.concat(buffers);
-        resolve(this.rawBody);
-      };
-
-      if (headers['content-encoding'] == 'gzip') {
-        gz = zlib.createGunzip();
-        gz.once('end', done);
-        nodeReq.once('end', gz.end.bind(gz));
-      } else {
-        nodeReq.once('end', done);
-      }
-
-      const maxBodySize = this.config.maxBodySize || 0;
-
-      nodeReq.on('data', (chunk: Uint8Array) => {
-        if (maxBodySize) {
-          bytesReceived += chunk.length;
-
-          if (bytesReceived > maxBodySize) {
-            reject(new Error('Request body size exceeds ' + maxBodySize));
-            return;
-          }
-        }
-
-        if (gz) {
-          gz.write(chunk);
-        } else {
-          buffers.push(chunk);
-        }
-      });
-
-      nodeReq.resume();
-    });
+  getRawBody(): Promise<Buffer> {
+    return;
   }
 
   getJsonBody(): Promise<any> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        if (!this.nodeReq.headers['content-type']?.includes('application/json')) {
-          resolve(null);
-          return;
-        }
-        const rawBody = (await this.getRawBody()).toString();
-        if (!rawBody) {
-          resolve(null);
-          return;
-        }
-        const body = JSON.parse(rawBody);
-        resolve(body);
-      } catch (e) {
-        reject(e);
-      }
-    });
+    return parse(this.nodeReq, this.nodeReq.headers);
   }
 }
