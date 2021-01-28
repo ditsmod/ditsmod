9f11fd6dd68568bab87a049da6f146b90b41ba86
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-01-28 19:27:14 +0200

    chore(core): added description for getDuplicates()

--- src/utils/get-duplicates.ts
+++ src/utils/get-duplicates.ts
@@ -1,3 +1,6 @@
+/**
+ * Returns array of uniq elements that are duplicates in source array.
+ */
 export function getDuplicates<T>(arr: T[]) {
   arr = arr || [];
   const duplicates = arr.filter((value, index, self) => self.indexOf(value) != index);
