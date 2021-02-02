c588bd3e2e2ad2ef51737b22e0215bd4f6a529fa
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-02 23:57:30 +0200

    feat(services): added maxBodySize limit to body parser

--- src/services/body-parser.ts
+++ src/services/body-parser.ts
@@ -19,6 +19,6 @@ export class BodyParser {
   }
 
   getJsonBody(): Promise<any> {
-    return parse(this.nodeReq, this.nodeReq.headers);
+    return parse(this.nodeReq, this.nodeReq.headers, { limit: this.config.maxBodySize });
   }
 }
