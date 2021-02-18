f552b51debc07edb7a1c9053c5eebf7ca12d51ec
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-02-18 22:50:27 +0200

    test(core): added tests for getPath()

---
+++ src/pre-routing.spec.ts
@@ -0,0 +1,24 @@
+import { PreRouting } from './pre-routing';
+
+describe('PreRouting', () => {
+  let mock: MockPreRouting;
+
+  class MockPreRouting extends PreRouting {
+    getPath(prefix: string, path: string) {
+      return super.getPath(prefix, path);
+    }
+  }
+
+  beforeEach(() => {
+    mock = new MockPreRouting(null, null, null);
+  });
+
+  describe('getPath', () => {
+    it('case 1', () => {
+      const path1 = mock.getPath('/api/posts/:postId', ':postId');
+      const path2 = mock.getPath('/api/posts', ':postId');
+      expect(path1).toBe('/api/posts/:postId');
+      expect(path2).toBe('/api/posts/:postId');
+    });
+  });
+});
