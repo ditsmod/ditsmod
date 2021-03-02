20be6144ecd73b14ff1de78fa47b9b52eaa058e5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-02 12:35:05 +0200

    feat(models): added ProvidersMetadata

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { ServiceProvider } from './types/service-provider';
+export { ProvidersMetadata } from './models/module/providers-metadata';
---
+++ src/models/module/providers-metadata.ts
@@ -0,0 +1,16 @@
+import { ServiceProvider } from '../../types/service-provider';
+
+export class ProvidersMetadata {
+  /**
+   * Providers per the application.
+   */
+  providersPerApp: ServiceProvider[] = [];
+  /**
+   * Providers per module.
+   */
+  providersPerMod: ServiceProvider[] = [];
+  /**
+   * Providers per request.
+   */
+  providersPerReq: ServiceProvider[] = [];
+}
