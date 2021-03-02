98f3ffa6cfbb9ac3d340ccd693aed744bb6847cb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-02 18:19:10 +0200

    feat(utils): added type guard

--- src/utils/type-guards.spec.ts
+++ src/utils/type-guards.spec.ts
@@ -2,8 +2,9 @@ import 'reflect-metadata';
 import { reflector } from '@ts-stack/di';
 
 import { Module } from '../decorators/module';
-import { isModule } from './type-guards';
+import { isModule, isRootModule } from './type-guards';
 import { ModuleMetadata } from '../types/module-metadata';
+import { RootModule } from '../decorators/root-module';
 
 describe('type guards', () => {
   describe('isModule()', () => {
@@ -18,6 +19,21 @@ describe('type guards', () => {
       class Module1 {}
       const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
       expect(isModule(metadata)).toBe(false);
+    });
+  });
+
+  describe('RootModule()', () => {
+    it('class with decorator', () => {
+      @RootModule()
+      class Module1 {}
+      const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
+      expect(isRootModule(metadata)).toBe(true);
+    });
+
+    it('class without decorator', () => {
+      class Module1 {}
+      const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
+      expect(isRootModule(metadata)).toBe(false);
     });
   });
 });
--- src/utils/type-guards.ts
+++ src/utils/type-guards.ts
@@ -1,5 +1,10 @@
 import { ModuleMetadata } from '../types/module-metadata';
+import { RootModuleMetadata } from '../types/root-module-metadata';
 
 export function isModule(moduleMetadata: any): moduleMetadata is ModuleMetadata {
   return moduleMetadata?.ngMetadataName == 'Module';
+}
+
+export function isRootModule(moduleMetadata: any): moduleMetadata is RootModuleMetadata {
+  return moduleMetadata?.ngMetadataName == 'RootModule';
 }
