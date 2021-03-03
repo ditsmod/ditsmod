5943382ebc7da7e202067efbac540e87365b9aa3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-03 11:22:16 +0200

    feat(utils): added type guards for Controller and Route decorators

--- src/utils/type-guards.spec.ts
+++ src/utils/type-guards.spec.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
-import { reflector } from '@ts-stack/di';
+import { Injectable, reflector } from '@ts-stack/di';
 
 import { Module } from '../decorators/module';
-import { isModule, isRootModule } from './type-guards';
+import { isController, isModule, isRootModule, isRoute } from './type-guards';
 import { ModuleMetadata } from '../types/module-metadata';
 import { RootModule } from '../decorators/root-module';
+import { Controller } from '../decorators/controller';
+import { Route, RouteDecoratorMetadata } from '../decorators/route';
+import { CanActivate } from '../types/can-activate';
 
 describe('type guards', () => {
   describe('isModule()', () => {
@@ -34,6 +37,43 @@ describe('type guards', () => {
       class Module1 {}
       const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
       expect(isRootModule(metadata)).toBe(false);
+    });
+  });
+
+  describe('Controller()', () => {
+    it('class with decorator', () => {
+      @Controller()
+      class Module1 {}
+      const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
+      expect(isController(metadata)).toBe(true);
+    });
+
+    it('class without decorator', () => {
+      class Module1 {}
+      const metadata = reflector.annotations(Module1)[0] as ModuleMetadata;
+      expect(isController(metadata)).toBe(false);
+    });
+  });
+
+  describe('isRoute()', () => {
+    @Injectable()
+    class Guard1 implements CanActivate {
+      canActivate() {
+        return true;
+      }
+
+      other() {}
+    }
+
+    @Controller()
+    class ClassWithDecorators {
+      @Route('GET', '', [Guard1])
+      some() {}
+    }
+
+    it('should recognize the route', () => {
+      const propMetadata = reflector.propMetadata(ClassWithDecorators) as RouteDecoratorMetadata;
+      expect(isRoute(propMetadata.some[0])).toBe(true);
     });
   });
 });
--- src/utils/type-guards.ts
+++ src/utils/type-guards.ts
@@ -1,3 +1,5 @@
+import { ControllerMetadata } from '../decorators/controller';
+import { RouteMetadata } from '../decorators/route';
 import { ModuleMetadata } from '../types/module-metadata';
 import { RootModuleMetadata } from '../types/root-module-metadata';
 
@@ -7,4 +9,12 @@ export function isModule(moduleMetadata: any): moduleMetadata is ModuleMetadata 
 
 export function isRootModule(moduleMetadata: any): moduleMetadata is RootModuleMetadata {
   return moduleMetadata?.ngMetadataName == 'RootModule';
+}
+
+export function isController(ctrlMeatada: ControllerMetadata): ctrlMeatada is ControllerMetadata {
+  return (ctrlMeatada as any)?.ngMetadataName == 'Controller';
+}
+
+export function isRoute(propMeatada: RouteMetadata): propMeatada is RouteMetadata {
+  return (propMeatada as any)?.ngMetadataName == 'Route';
 }
