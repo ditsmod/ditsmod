b8f739da229cd797189ab48801c2d934536a2cdb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-05 12:03:41 +0200

    feat(utils): added generic to mergeArrays()

--- src/utils/merge-arrays-options.ts
+++ src/utils/merge-arrays-options.ts
@@ -1,4 +1,4 @@
-export function mergeArrays(arr1: any[], arr2: undefined | any[]) {
+export function mergeArrays<F, S>(arr1: F[] | undefined, arr2: S[] | undefined) {
   return [...(arr1 || []), ...(arr2 || [])];
 }
 // @todo Rename file to `merge-arrays.ts`
