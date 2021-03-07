d534832043d7826938bb49ff75c6f3772b38fb3e
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-07 13:48:43 +0200

    feat(types): added RedirectStatusCodes, Router and server options

---
+++ src/types/router.ts
@@ -0,0 +1,33 @@
+import { HttpMethod } from './http-method';
+import { NodeRequest, NodeResponse } from './server-options';
+
+export class Router {
+  on(method: HttpMethod, path: string, handle: RouteHandler): this {
+    return this;
+  }
+
+  all(path: string, handle: RouteHandler): this {
+    return this;
+  }
+
+  find(method: HttpMethod, path: string): RouterReturns {
+    return { handle: null, params: null };
+  }
+}
+
+export type RouteHandler = (
+  nodeReq: NodeRequest,
+  nodeRes: NodeResponse,
+  params: PathParam[],
+  queryString: any
+) => void;
+
+export class RouterReturns {
+  handle: RouteHandler;
+  params: PathParam[];
+}
+
+export interface PathParam {
+  key: string;
+  value: string;
+}
--- src/types/server-options.ts
+++ src/types/server-options.ts
@@ -1,6 +1,12 @@
 import * as http from 'http';
 import * as https from 'https';
 import * as http2 from 'http2';
+import { Http2ServerRequest, Http2ServerResponse } from 'http2';
+import { InjectionToken } from '@ts-stack/di';
 
 export type Http2SecureServerOptions = http2.SecureServerOptions & { isHttp2SecureServer: boolean };
 export type ServerOptions = http.ServerOptions | https.ServerOptions | http2.ServerOptions | Http2SecureServerOptions;
+export type NodeRequest = http.IncomingMessage | Http2ServerRequest;
+export type NodeResponse = http.ServerResponse | Http2ServerResponse;
+export const NodeReqToken = new InjectionToken<NodeRequest>('NodeRequest');
+export const NodeResToken = new InjectionToken<NodeResponse>('NodeResponse');
