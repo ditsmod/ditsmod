5c273f7f7c8ac486bc29fcd3c3eb02af992166ed
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-07 14:50:20 +0200

    feat(services): added PreRouter

---
+++ src/services/pre-router.spec.ts
@@ -0,0 +1,24 @@
+import { PreRouter } from '../services/pre-router';
+
+describe('PreRouting', () => {
+  let mock: MockPreRouter;
+
+  class MockPreRouter extends PreRouter {
+    getPath(prefix: string, path: string) {
+      return super.getPath(prefix, path);
+    }
+  }
+
+  beforeEach(() => {
+    mock = new MockPreRouter(null, null, null, null);
+  });
+
+  describe('getPath', () => {
+    it('case 1', () => {
+      const path1 = mock.getPath('/api/posts/:postId', ':postId');
+      const path2 = mock.getPath('/api/posts', ':postId');
+      expect(path1).toBe('/api/posts/:postId');
+      expect(path2).toBe('/api/posts/:postId');
+    });
+  });
+});
---
+++ src/services/pre-router.ts
@@ -0,0 +1,218 @@
+import { parse } from 'querystring';
+import { Injectable, ReflectiveInjector, TypeProvider } from '@ts-stack/di';
+
+import { Logger } from '../types/logger';
+import { NodeReqToken, NodeRequest, NodeResponse, NodeResToken, RequestListener } from '../types/server-options';
+import { Request } from './request';
+import { PathParam, Router, RouteHandler } from '../types/router';
+import { Status } from '../utils/http-status-codes';
+import { Extension } from '../types/extension';
+import { PreRouteData } from '../types/route-data';
+import { NormalizedRootModuleMetadata } from '../models/normalized-root-module-metadata';
+import { ModuleType } from '../types/module-type';
+import { ExtensionMetadata } from '../types/extension-metadata';
+import { HttpMethod } from '../types/http-method';
+import { NormalizedGuard } from '../types/normalized-guard';
+import { ControllerErrorHandler } from '../types/controller-error-handler';
+import { CanActivate } from '../types/can-activate';
+import { AnyObj } from '../types/any-obj';
+import { BodyParser } from './body-parser';
+import { PreRoutes } from './pre-routes';
+
+@Injectable()
+export class PreRouter implements Extension<PreRouteData[]> {
+  constructor(
+    protected injectorPerApp: ReflectiveInjector,
+    protected router: Router,
+    protected log: Logger,
+    protected normRootModuleMetadata: NormalizedRootModuleMetadata
+  ) {}
+
+  init(prefixPerApp: string, metadataMap: Map<ModuleType, ExtensionMetadata>) {
+    let preRoutesData: PreRouteData[];
+
+    metadataMap.forEach((metadata, mod) => {
+      const preRoutes = this.injectorPerApp.resolveAndInstantiate(PreRoutes) as PreRoutes;
+      preRoutesData = preRoutes.getPreRoutesData(mod.name, metadata);
+      const { prefixPerMod } = metadata;
+      this.setRoutes(mod.name, prefixPerApp, prefixPerMod, preRoutesData);
+    });
+
+    return preRoutesData;
+  }
+
+  requestListener: RequestListener = (nodeReq, nodeRes) => {
+    const { method: httpMethod, url } = nodeReq;
+    const [uri, queryString] = this.decodeUrl(url).split('?');
+    const { handle, params } = this.router.find(httpMethod as HttpMethod, uri);
+    if (!handle) {
+      this.sendNotFound(nodeRes);
+      return;
+    }
+    handle(nodeReq, nodeRes, params, queryString);
+  };
+
+  protected setRoutes(moduleName: string, prefixPerApp: string, prefixPerMod: string, preRoutesData: PreRouteData[]) {
+    this.checkRoutePath(moduleName, prefixPerApp);
+    this.checkRoutePath(moduleName, prefixPerMod);
+    const prefix = [prefixPerApp, prefixPerMod].filter((s) => s).join('/');
+
+    preRoutesData.forEach((preRouteData) => {
+      const route = preRouteData.route;
+      const path = this.getPath(prefix, route.path);
+      /**
+       * @param injector Injector per module that tied to the route.
+       * @param providers Resolved providers per request.
+       * @param method Method of the class controller.
+       * @param parseBody Need or not to parse body.
+       */
+      const { injector, providers, controller, methodName, parseBody, guards } = preRouteData;
+
+      const handle = ((nodeReq: NodeRequest, nodeRes: NodeResponse, params: PathParam[], queryString: any) => {
+        nodeRes.setHeader('Server', this.normRootModuleMetadata.serverName);
+        const injector1 = injector.resolveAndCreateChild([
+          { provide: NodeReqToken, useValue: nodeReq },
+          { provide: NodeResToken, useValue: nodeRes },
+        ]);
+        const injector2 = injector1.createChildFromResolved(providers);
+        const req = injector2.get(Request) as Request;
+        this.handleRoute(req, params, queryString, controller, methodName, parseBody, guards);
+      }) as RouteHandler;
+
+      if (route.httpMethod == 'ALL') {
+        this.router.all(`/${path}`, handle);
+      } else {
+        this.router.on(route.httpMethod, `/${path}`, handle);
+      }
+
+      const logObj = {
+        module: moduleName,
+        httpMethod: route.httpMethod,
+        path,
+        guards: preRouteData.guards,
+        handler: `${preRouteData.controller.name}.${preRouteData.methodName}()`,
+      };
+
+      if (!logObj.guards.length) {
+        delete logObj.guards;
+      }
+
+      this.log.debug(logObj);
+    });
+  }
+
+  /**
+   * Called by the `Application` before call a router.
+   */
+  protected decodeUrl(url: string) {
+    return decodeURI(url);
+  }
+
+  /**
+   * Called by the `Application` when a route is not found (404).
+   */
+  protected sendNotFound(nodeRes: NodeResponse) {
+    nodeRes.statusCode = Status.NOT_FOUND;
+    nodeRes.end();
+  }
+
+  /**
+   * Called by the `Application` after founded a route.
+   *
+   * @param controller Controller class.
+   * @param methodName Method of the Controller.
+   * @param parseBody Need or not to parsing a body request.
+   */
+  protected async handleRoute(
+    req: Request,
+    pathParamsArr: PathParam[],
+    queryString: string,
+    controller: TypeProvider,
+    methodName: string,
+    parseBody: boolean,
+    guards: NormalizedGuard[]
+  ) {
+    let errorHandler: ControllerErrorHandler;
+    let ctrl: any;
+    let preparedGuards: { guard: CanActivate; params?: any[] }[] = [];
+
+    try {
+      req.pathParamsArr = pathParamsArr;
+      const pathParams: AnyObj = pathParamsArr ? {} : undefined;
+      pathParamsArr?.forEach((param) => (pathParams[param.key] = param.value));
+      req.pathParams = pathParams;
+
+      errorHandler = req.injector.get(ControllerErrorHandler);
+      preparedGuards = guards.map((item) => {
+        return {
+          guard: req.injector.get(item.guard),
+          params: item.params,
+        };
+      });
+      ctrl = req.injector.get(controller);
+    } catch (err) {
+      this.sendInternalServerError(req.nodeRes, err);
+      return;
+    }
+
+    try {
+      for (const item of preparedGuards) {
+        const canActivate = await item.guard.canActivate(item.params);
+        if (canActivate !== true) {
+          const status = typeof canActivate == 'number' ? canActivate : undefined;
+          this.canNotActivateRoute(req.nodeReq, req.nodeRes, status);
+          return;
+        }
+      }
+
+      req.queryParams = parse(queryString);
+      if (parseBody) {
+        const bodyParser = req.injector.get(BodyParser) as BodyParser;
+        req.body = await bodyParser.getBody();
+      }
+
+      await ctrl[methodName]();
+    } catch (err) {
+      errorHandler.handleError(err);
+    }
+  }
+
+  protected canNotActivateRoute(nodeReq: NodeRequest, nodeRes: NodeResponse, status?: Status) {
+    this.log.debug(`Can not activate the route with URL: ${nodeReq.method} ${nodeReq.url}`);
+    nodeRes.statusCode = status || Status.UNAUTHORIZED;
+    nodeRes.end();
+  }
+
+  /**
+   * Logs an error and sends the user message about an internal server error (500).
+   *
+   * @param err An error to logs it (not sends).
+   */
+  protected sendInternalServerError(nodeRes: NodeResponse, err: Error) {
+    this.log.error(err);
+    nodeRes.statusCode = Status.INTERNAL_SERVER_ERROR;
+    nodeRes.end();
+  }
+
+  /**
+   * Compiles the path for the controller given the prefix.
+   *
+   * - If prefix `/api/posts/:postId` and route path `:postId`, this method returns path `/api/posts/:postId`.
+   * - If prefix `/api/posts` and route path `:postId`, this method returns `/api/posts/:postId`
+   */
+  protected getPath(prefix: string, path: string) {
+    const prefixLastPart = prefix?.split('/').slice(-1)[0];
+    if (prefixLastPart?.charAt(0) == ':') {
+      const reducedPrefix = prefix?.split('/').slice(0, -1).join('/');
+      return [reducedPrefix, path].filter((s) => s).join('/');
+    } else {
+      return [prefix, path].filter((s) => s).join('/');
+    }
+  }
+
+  protected checkRoutePath(moduleName: string, path: string) {
+    if (path?.charAt(0) == '/') {
+      throw new Error(`Invalid configuration of route '${path}' (in '${moduleName}'): path cannot start with a slash`);
+    }
+  }
+}
