02293a87de0991c868774b72edaf8b5162962397
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-07 13:51:07 +0200

    feat(services): added DefaultControllerErrorHandler

---
+++ src/services/default-controller-error-handler.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@ts-stack/di';
+
+import { ControllerErrorHandler } from '../types/controller-error-handler';
+import { Logger } from '../types/logger';
+import { Status } from '../utils/http-status-codes';
+import { Response } from './response';
+
+@Injectable()
+export class DefaultControllerErrorHandler implements ControllerErrorHandler {
+  constructor(private res: Response, private log: Logger) {}
+
+  handleError(err: Error) {
+    const { message } = err;
+    this.log.error({ err });
+    if (!this.res.nodeRes.headersSent) {
+      this.res.sendJson({ error: { message } }, Status.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
