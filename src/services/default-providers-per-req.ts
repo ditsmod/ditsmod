04d6a7c1069f31903490243f714853be165f5038
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-08 13:45:03 +0200

    fix(services): fixed import for defaultProvidersPerReq

--- src/services/default-providers-per-req.ts
+++ src/services/default-providers-per-req.ts
@@ -1,6 +1,8 @@
 import { ControllerErrorHandler } from '../types/controller-error-handler';
 import { ServiceProvider } from '../types/service-provider';
 import { DefaultControllerErrorHandler } from './default-controller-error-handler';
+import { Request } from './request';
+import { Response } from './response';
 
 export const defaultProvidersPerReq: Readonly<ServiceProvider[]> = [
   Request,
