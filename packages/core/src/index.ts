ed7a027da8a4cc56ef2020730457a35f012da117
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-13 00:37:49 +0200

    feat(core): added edk to import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -22,24 +22,7 @@ export { GuardItem } from './types/guard-item';
 export { PathParam, RouterReturns, RouteHandler } from './types/router';
 export { RootMetadata } from './models/root-metadata';
 export { NormalizedProvider } from './utils/ng-utils';
-
-// For extensions developers only
-export { ModuleManager } from './services/module-manager';
-export { PreRouter } from './services/pre-router';
-export { ModuleFactory } from './module-factory';
-export { ProvidersMetadata } from './models/providers-metadata';
-export { ControllerMetadata } from './decorators/controller';
-export { Counter } from './services/counter';
-export * from './utils/type-guards';
-export { deepFreeze } from './utils/deep-freeze';
-export { isModule } from './utils/type-guards';
-export { pickProperties } from './utils/pick-properties';
-export { ExtensionMetadata } from './types/extension-metadata';
-export { ControllerType } from './types/controller-type';
-export { ModuleType } from './types/module-type';
-export { AnyObj } from './types/any-obj';
-export { NormalizedGuard } from './types/normalized-guard';
-export { Extension } from './types/extension';
-export { ModuleMetadata } from './types/module-metadata';
-export { ExtensionType } from './types/extension-type';
-export { RouteData, PreRouteData } from './types/route-data';
+/**
+ * Extension Development Kit.
+ */
+export * as edk from './edk';
