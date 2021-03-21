66419a6f733d0520efa9f83e7448bb4592495a74
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-03-21 21:11:45 +0200

    chore(examples): refactoring 8

--- examples/8-http-interceptors/app/my-http-interceptor.ts
+++ examples/8-http-interceptors/app/my-http-interceptor.ts
@@ -5,20 +5,16 @@ import { HttpHandler, HttpInterceptor, Logger, Request } from '@ditsmod/core';
 export class MyHttpInterceptor implements HttpInterceptor {
   constructor(private log: Logger) {}
 
-  async intercept(req: Request, next: HttpHandler, ...args: any[]) {
+  intercept(req: Request, next: HttpHandler, ...args: any[]) {
     // Handling request to `HelloWorldController`
     // and you must setting `...args` here.
-    const promise = next.handle(req, ...args);
-
-    promise
-      .catch((err) => {
-        this.log.error(err);
-      })
-      .finally(() => {
-        // You can to do something after, for example, log status:
+    return next.handle(req, ...args).finally(() => {
+      // You can to do something after, for example, log status:
+      if (req.nodeRes.headersSent) {
         this.log.info(`MyHttpInterceptor works! Status code: ${req.nodeRes.statusCode}`);
-      });
-
-    return promise;
+      } else {
+        this.log.info('MyHttpInterceptor works! But... Do you forgot send response or just an error occurred?');
+      }
+    });
   }
 }
