7a0007b80b5d3d404b83b9ff65a3b356b5e34a3c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-04 15:26:22 +0300

    refactor(core): removed args from init() of Extension type

--- packages/core/src/types/extension.ts
+++ packages/core/src/types/extension.ts
@@ -1,7 +1,7 @@
 import { InjectionToken } from '@ts-stack/di';
 
 export interface Extension<T = any> {
-  init(...args: any): T | Promise<T>;
+  init(): T | Promise<T>;
 }
 
 export const ROUTES_EXTENSIONS = new InjectionToken<Extension[]>('ROUTES_EXTENSIONS');
