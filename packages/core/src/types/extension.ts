9c8d2c4147163fec93298bcca03fbf8ea3571ca9
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-05 16:31:30 +0300

    feat(core): added type to generic for ROUTES_EXTENSIONS

--- packages/core/src/types/extension.ts
+++ packages/core/src/types/extension.ts
@@ -1,8 +1,9 @@
 import { InjectionToken } from '@ts-stack/di';
+import { PreRouteMeta } from './route-data';
 
 export interface Extension<T = any> {
   init(): T | Promise<T>;
 }
 
 export const DEFAULT_EXTENSIONS = new InjectionToken<Extension[]>('DEFAULT_EXTENSIONS');
-export const ROUTES_EXTENSIONS = new InjectionToken<Extension[]>('ROUTES_EXTENSIONS');
+export const ROUTES_EXTENSIONS = new InjectionToken<Extension<PreRouteMeta[]>[]>('ROUTES_EXTENSIONS');
