9d9603cdcf06b495ddae77800fc7d3150d0e4e9b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-08 17:02:34 +0300

    refactor(core): a bit fix in order extension init

--- packages/core/src/services/default-extensions.ts
+++ packages/core/src/services/default-extensions.ts
@@ -2,4 +2,4 @@ import { InjectionToken } from '@ts-stack/di';
 
 import { VOID_EXTENSIONS, ROUTES_EXTENSIONS } from '../types/extension';
 
-export const defaultExtensions: InjectionToken<any>[] = [VOID_EXTENSIONS, ROUTES_EXTENSIONS];
+export const defaultExtensions: InjectionToken<any>[] = [ROUTES_EXTENSIONS, VOID_EXTENSIONS];
