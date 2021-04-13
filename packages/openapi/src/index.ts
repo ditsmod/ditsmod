a0de60138df036a207ac2984a6a876b0ad2fe932
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-13 15:47:44 +0300

    feat(openapi): added OAS_HTTP_METHODS to exports

--- packages/openapi/src/index.ts
+++ packages/openapi/src/index.ts
@@ -1,3 +1,4 @@
 export { OasRoute, OasRouteMetadata } from './decorators/oas-route';
 export { OpenapiModule } from './openapi.module';
 export { OasRouteMeta } from './types/oas-route-meta';
+export { OAS_HTTP_METHODS } from './models/oas-http-methods';
--- packages/openapi/src/models/oas-http-methods.ts
+++ packages/openapi/src/models/oas-http-methods.ts
@@ -1,4 +1,7 @@
 import { HttpMethod } from '@ditsmod/core';
 import { InjectionToken } from '@ts-stack/di';
 
+/**
+ * OpenAPI Specification HTTP methods.
+ */
 export const OAS_HTTP_METHODS = new InjectionToken<HttpMethod[]>('OAS_HTTP_METHODS');
