3e4e9f8090b2aa5ecd057aa9fa2ce8c3c245fd3b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-14 07:36:32 +0300

    feat(openapi): added isReferenceObject() type guard

--- packages/openapi/src/index.ts
+++ packages/openapi/src/index.ts
@@ -2,3 +2,4 @@ export { OasRoute, OasRouteMetadata } from './decorators/oas-route';
 export { OpenapiModule } from './openapi.module';
 export { OasRouteMeta } from './types/oas-route-meta';
 export { OAS_HTTP_METHODS } from './models/oas-http-methods';
+export * from './utils/type-guards';
--- packages/openapi/src/utils/type-guards.ts
+++ packages/openapi/src/utils/type-guards.ts
@@ -1,7 +1,12 @@
 import { edk } from '@ditsmod/core';
+import { ReferenceObject } from '@ts-stack/openapi-spec';
 
 import { OasRouteMetadata } from '../decorators/oas-route';
 
 export function isOasRoute(propMeatada: edk.AnyObj): propMeatada is OasRouteMetadata {
   return (propMeatada as any)?.ngMetadataName == 'OasRoute';
+}
+
+export function isReferenceObject(obj: edk.AnyObj): obj is ReferenceObject {
+  return obj.hasOwnProperty('$ref');
 }
