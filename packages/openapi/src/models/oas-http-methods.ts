46c3a8db1454f5c489d21ca48e63c64557f377e0
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-15 16:04:35 +0300

    feat(openapi): added OAS_COMPILER_EXTENSIONS token

--- packages/openapi/src/models/oas-http-methods.ts
+++ packages/openapi/src/models/oas-http-methods.ts
@@ -1,7 +1,12 @@
-import { HttpMethod } from '@ditsmod/core';
+import { edk, HttpMethod } from '@ditsmod/core';
 import { InjectionToken } from '@ts-stack/di';
+import { XPathsObject } from '@ts-stack/openapi-spec';
 
 /**
  * OpenAPI Specification HTTP methods.
  */
 export const OAS_HTTP_METHODS = new InjectionToken<HttpMethod[]>('OAS_HTTP_METHODS');
+/**
+ * OpenAPI Specification extensions group that returns `XPathsObject`.
+ */
+export const OAS_COMPILER_EXTENSIONS = new InjectionToken<edk.Extension<XPathsObject>[]>('OAS_COMPILER_EXTENSIONS');
--- packages/openapi/src/openapi.module.ts
+++ packages/openapi/src/openapi.module.ts
@@ -1,17 +1,19 @@
 import { Module, edk, ModuleWithParams, ServiceProvider } from '@ditsmod/core';
 
+import { OpenapiCompilerExtension } from './extensions/openapi-compiler.extension';
 import { OpenapiExtension } from './extensions/openapi.extension';
-import { OAS_HTTP_METHODS } from './models/oas-http-methods';
+import { OAS_COMPILER_EXTENSIONS, OAS_HTTP_METHODS } from './models/oas-http-methods';
 import { OasRouteMeta } from './types/oas-route-meta';
 
 @Module({
   providersPerApp: [
     { provide: edk.ROUTES_EXTENSIONS, useClass: OpenapiExtension, multi: true },
+    { provide: OAS_COMPILER_EXTENSIONS, useClass: OpenapiCompilerExtension, multi: true },
     { provide: OAS_HTTP_METHODS, useValue: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE'] },
   ],
   providersPerRou: [OasRouteMeta],
   exports: [{ provide: OasRouteMeta, useExisting: edk.RouteMeta }],
-  extensions: [edk.ROUTES_EXTENSIONS],
+  extensions: [edk.ROUTES_EXTENSIONS, OAS_COMPILER_EXTENSIONS],
 })
 export class OpenapiModule {
   static withParams(providersPerApp: ServiceProvider[]): ModuleWithParams {
