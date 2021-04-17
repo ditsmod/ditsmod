6392e23feb1d9bc18994e8680f7e444a06ee71f3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-18 00:14:45 +0300

    fix(core): improved getUniqProviders() for multi providers

--- packages/core/src/utils/get-uniq-providers.spec.ts
+++ packages/core/src/utils/get-uniq-providers.spec.ts
@@ -1,3 +1,4 @@
+import { ValueProvider } from '@ts-stack/di';
 import { getUniqProviders } from './get-uniq-providers';
 
 describe('getUniqProviders()', () => {
@@ -38,5 +39,47 @@ describe('getUniqProviders()', () => {
     class Provider1 {}
     const obj = { provide: Provider1, useValue: '' };
     expect(getUniqProviders([obj, Provider1, obj])).toEqual([obj]);
+  });
+
+  describe('Multi providers', () => {
+    it('same provide and value', () => {
+      class Provider1 {}
+      const mp1: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const mp2: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const providers = [mp1, mp2];
+      const expectedProviders = [mp1];
+      expect(getUniqProviders(providers)).toEqual(expectedProviders);
+    });
+
+    it('same provide, but non same value', () => {
+      class Provider1 {}
+      const mp1: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const mp2: ValueProvider = { provide: Provider1, useValue: 'two', multi: true };
+      const providers = [mp1, mp2];
+      const expectedProviders = [mp1, mp2];
+      expect(getUniqProviders(providers)).toEqual(expectedProviders);
+    });
+
+    it('same value, but non same provide', () => {
+      class Provider1 {}
+      class Provider2 {}
+      const mp1: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const mp2: ValueProvider = { provide: Provider2, useValue: 'one', multi: true };
+      const providers = [mp1, mp2];
+      const expectedProviders = [mp1, mp2];
+      expect(getUniqProviders(providers)).toEqual(expectedProviders);
+    });
+
+    it('mix', () => {
+      class Provider1 {}
+      class Provider2 {}
+      const mp1: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const mp2: ValueProvider = { provide: Provider1, useValue: 'one', multi: true };
+      const mp3: ValueProvider = { provide: Provider1, useValue: 'two', multi: true };
+      const mp4: ValueProvider = { provide: Provider2, useValue: 'two', multi: true };
+      const providers = [mp1, mp2, mp3, mp4];
+      const expectedProviders = [mp1, mp3, mp4];
+      expect(getUniqProviders(providers)).toEqual(expectedProviders);
+    });
   });
 });
--- packages/core/src/utils/get-uniq-providers.ts
+++ packages/core/src/utils/get-uniq-providers.ts
@@ -1,16 +1,48 @@
 import { ServiceProvider } from '../types/mix';
-import { normalizeProviders } from './ng-utils';
+import { NormalizedProvider, normalizeProviders } from './ng-utils';
+import { isClassProvider, isExistingProvider, isFactoryProvider, isValueProvider } from './type-guards';
 
 /**
  * Returns last provider if the provider has the duplicate.
+ * For the multi providers, removes duplicates only with identical `provide`
+ * and `useValue`, `useClass`, `useExisting` or `useFactory`.
  */
 export function getUniqProviders<T extends ServiceProvider = ServiceProvider>(providers: T[]) {
   const tokens = normalizeProviders(providers).map((np) => np.provide);
   const uniqProviders: T[] = [];
 
   tokens.forEach((currToken, currIndex) => {
-    if (tokens.lastIndexOf(currToken) == currIndex) {
-      uniqProviders.push(providers[currIndex]);
+    const provider = providers[currIndex];
+    const isMultiProvider = (provider as NormalizedProvider).multi;
+    if (isMultiProvider) {
+      let hasDuplicate = true;
+      for (const uniqProvider of uniqProviders) {
+        if ((provider as NormalizedProvider).provide !== (uniqProvider as NormalizedProvider).provide) {
+          // ok, this is uniq provider
+        } else if (isValueProvider(uniqProvider) && isValueProvider(provider)) {
+          if (uniqProvider.useValue === provider.useValue) {
+            continue;
+          }
+        } else if (isClassProvider(uniqProvider) && isClassProvider(provider)) {
+          if (uniqProvider.useClass === provider.useClass) {
+            continue;
+          }
+        } else if (isExistingProvider(uniqProvider) && isExistingProvider(provider)) {
+          if (uniqProvider.useExisting === provider.useExisting) {
+            continue;
+          }
+        } else if (isFactoryProvider(uniqProvider) && isFactoryProvider(provider)) {
+          if (uniqProvider.useFactory === provider.useFactory) {
+            continue;
+          }
+        }
+        hasDuplicate = false;
+      }
+      if (!uniqProviders.length || !hasDuplicate) {
+        uniqProviders.push(provider);
+      }
+    } else if (tokens.lastIndexOf(currToken) == currIndex) {
+      uniqProviders.push(provider);
     }
   });
 
