aed8b78202f14ef4d71a66b0af3f4eaed5fe7f69
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-18 17:17:43 +0300

    chore(examples): improved 10 example

--- examples/10-openapi/app/app.module.ts
+++ examples/10-openapi/app/app.module.ts
@@ -5,14 +5,21 @@ import { XOasObject } from '@ts-stack/openapi-spec';
 
 import { HelloWorldController } from './hello-world.controller';
 
-const oasObject: XOasObject = { openapi: '3.0.0', info: { title: 'Testing @ditsmod/openapi', version: '1.0.0' } };
+const oasObject: XOasObject = {
+  openapi: '3.0.0',
+  // Here works the servers that are described using this OpenAPI documentation.
+  servers: [{ url: 'http://localhost:8080' }],
+  info: { title: 'Testing @ditsmod/openapi', version: '1.0.0' },
+};
 const providersPerApp: ServiceProvider[] = [{ provide: OAS_OBJECT, useValue: oasObject }];
 const openapiModuleWithParams = OpenapiModule.withParams(providersPerApp);
 
 @RootModule({
+  // Here works the application and serve OpenAPI documentation.
+  listenOptions: { host: 'localhost', port: 8080 },
   imports: [openapiModuleWithParams],
   exports: [openapiModuleWithParams],
   controllers: [HelloWorldController],
-  providersPerApp: [{ provide: Router, useClass: DefaultRouter }]
+  providersPerApp: [{ provide: Router, useClass: DefaultRouter }],
 })
 export class AppModule {}
