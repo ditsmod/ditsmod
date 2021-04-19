ed0c689fd16626faeeff9582ba02ad5c3771a04c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-19 09:02:58 +0300

    test(logger): added this module to ignore

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>.+dist/.+'],
+  modulePathIgnorePatterns: ['<rootDir>.+dist/.+', '<rootDir>/packages/logger/*'],
   moduleNameMapper: {
     '@ditsmod/body-parser': '<rootDir>/packages/body-parser/src',
     '@ditsmod/core': '<rootDir>/packages/core/src',
     '@ditsmod/openapi': '<rootDir>/packages/openapi/src',
     '@ditsmod/router': '<rootDir>/packages/router/src',
-  }
+  },
 };
