f22c8afd17df49df4c2eab750b6f8c696c48330c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-04-26 10:22:29 +0300

    chore(examples): simplifying 10 example

--- examples/10-openapi/app/hello-world.controller.ts
+++ examples/10-openapi/app/hello-world.controller.ts
@@ -11,29 +11,15 @@ export class HelloWorldController {
   // Here works route decorator from `@ditsmod/core`.
   @Route('GET')
   hello() {
-    this.res.sendJson({ pathParams: this.req.pathParams || null });
-  }
-
-  @Route('GET', 'one/:param1')
-  test1() {
-    this.res.sendJson({ pathParams: this.req.pathParams });
-  }
-
-  @Route('GET', 'one/:param1/two/:param2')
-  test2() {
-    this.res.sendJson({ pathParams: this.req.pathParams });
+    this.res.send('Hello!');
   }
 
   // Here works new route decorator from `@ditsmod/openapi`.
   @OasRoute('posts/{postId}', [BasicGuard], {
-    parameters: new Parameters()
-      .required('path', Post, 'postId')
-      .optional('cookie', Post, 'postTitle')
-      .getParams(),
     get: {
       parameters: new Parameters()
-        .required('query', 'rubricId')
-        .optional('query', 'contextId')
+        .required('path', Post, 'postId')
+        .optional('query', 'someOptionalParam')
         .getParams(),
       responses: {
         [Status.OK]: {
@@ -46,26 +32,5 @@ export class HelloWorldController {
   getPost() {
     const { postId } = this.req.pathParams;
     this.res.sendJson({ postId, body: `some body for postId ${postId}` });
-  }
-
-  @OasRoute('posts/{postId}/comments/{commentId}', [BasicGuard], {
-    get: {
-      parameters: [
-        { in: 'path', name: 'postId', required: true },
-        { in: 'path', name: 'commentId', required: true },
-      ],
-      responses: {
-        [Status.OK]: {
-          description: 'Single comment',
-          content: {
-            'application/json': {},
-          },
-        },
-      },
-    },
-  })
-  getComment() {
-    const { postId, commentId } = this.req.pathParams;
-    this.res.sendJson({ postId, body: `some body for postId ${postId} and commentId ${commentId}` });
   }
 }
