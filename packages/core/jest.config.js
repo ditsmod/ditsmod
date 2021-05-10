672f25bc3d1c76eba41fe2d6d7fcf481bc4cf2f5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-05-10 12:35:05 +0300

    test(core): removed ref to router

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  moduleNameMapper: {
-    '@ditsmod/router': '<rootDir>/../router/src'
-  }
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
 };
