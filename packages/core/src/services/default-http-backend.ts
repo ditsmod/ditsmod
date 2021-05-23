29b6db36d595575fa757561b6ac79e6f993b0b7e
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-05-23 12:47:38 +0300

    refactor(core): moved guards calls to HttpFrontend

--- packages/core/src/services/default-http-backend.ts
+++ packages/core/src/services/default-http-backend.ts
@@ -2,50 +2,15 @@ import { Injectable } from '@ts-stack/di';
 
 import { HttpBackend } from '../types/http-interceptor';
 import { Request } from './request';
-import { CanActivate } from '../types/mix';
-import { NodeRequest, NodeResponse } from '../types/server-options';
-import { Status } from '../utils/http-status-codes';
-import { RootMetadata } from '../models/root-metadata';
 import { RouteMeta } from '../types/route-data';
-import { Log } from './log';
 
 @Injectable()
 export class DefaultHttpBackend implements HttpBackend {
-  constructor(
-    private req: Request,
-    private log: Log,
-    private rootMetadata: RootMetadata,
-    private routeMeta: RouteMeta
-  ) {}
+  constructor(private req: Request, private routeMeta: RouteMeta) {}
 
   async handle() {
-    const req = this.req;
-    const { controller, methodName, guards } = this.routeMeta;
-    req.nodeRes.setHeader('Server', this.rootMetadata.serverName);
-
-    const preparedGuards: { guard: CanActivate; params?: any[] }[] = guards.map((item) => {
-      return {
-        guard: req.injector.get(item.guard),
-        params: item.params,
-      };
-    });
-
-    for (const item of preparedGuards) {
-      const canActivate = await item.guard.canActivate(item.params);
-      if (canActivate !== true) {
-        const status = typeof canActivate == 'number' ? canActivate : undefined;
-        this.canNotActivateRoute(req.nodeReq, req.nodeRes, status);
-        return;
-      }
-    }
-
-    const ctrl = req.injector.get(controller);
+    const { controller, methodName } = this.routeMeta;
+    const ctrl = this.req.injector.get(controller);
     return await ctrl[methodName]();
-  }
-
-  protected canNotActivateRoute(nodeReq: NodeRequest, nodeRes: NodeResponse, status?: Status) {
-    this.log.youCannotActivateRoute('debug', [nodeReq.method, nodeReq.url]);
-    nodeRes.statusCode = status || Status.UNAUTHORIZED;
-    nodeRes.end();
   }
 }
--- packages/core/src/services/default-http-frontend.ts
+++ packages/core/src/services/default-http-frontend.ts
@@ -4,29 +4,34 @@ import { parse } from 'querystring';
 import { ControllerErrorHandler } from '../services/controller-error-handler';
 import { HttpFrontend, HttpHandler } from '../types/http-interceptor';
 import { Request } from './request';
-import { AnyObj } from '../types/mix';
+import { AnyObj, CanActivate } from '../types/mix';
 import { PathParam } from '../types/router';
 import { PATH_PARAMS, QUERY_STRING } from '../constans';
+import { NodeRequest, NodeResponse } from '../types/server-options';
+import { Status } from '../utils/http-status-codes';
+import { Log } from './log';
+import { RouteMeta } from '../types/route-data';
+import { RootMetadata } from '../models/root-metadata';
 
 @Injectable()
 export class DefaultHttpFrontend implements HttpFrontend {
   constructor(
     @Inject(PATH_PARAMS) protected pathParamsArr: PathParam[],
     @Inject(QUERY_STRING) protected queryString: any,
+    private log: Log,
+    private routeMeta: RouteMeta,
+    private rootMetadata: RootMetadata,
     private req: Request
   ) {}
 
   async intercept(next: HttpHandler) {
     try {
-      if (this.queryString) {
-        this.req.queryParams = parse(this.queryString);
+      const canActivate = await this.canActivate();
+      if (!canActivate) {
+        return;
       }
-      if (this.pathParamsArr) {
-        this.req.pathParamsArr = this.pathParamsArr;
-        const pathParams: AnyObj = {};
-        this.pathParamsArr.forEach((param) => (pathParams[param.key] = param.value));
-        this.req.pathParams = pathParams;
-      }
+      this.setParams();
+      this.req.nodeRes.setHeader('Server', this.rootMetadata.serverName);
     } catch (err) {
       this.loadErrorHandler(err);
     }
@@ -36,12 +41,47 @@ export class DefaultHttpFrontend implements HttpFrontend {
     });
   }
 
+  protected async canActivate() {
+    const { guards } = this.routeMeta;
+    const preparedGuards: { guard: CanActivate; params?: any[] }[] = guards.map((item) => {
+      return {
+        guard: this.req.injector.get(item.guard),
+        params: item.params,
+      };
+    });
+
+    for (const item of preparedGuards) {
+      const canActivate = await item.guard.canActivate(item.params);
+      if (canActivate !== true) {
+        const status = typeof canActivate == 'number' ? canActivate : undefined;
+        this.canNotActivateRoute(this.req.nodeReq, this.req.nodeRes, status);
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  protected canNotActivateRoute(nodeReq: NodeRequest, nodeRes: NodeResponse, status?: Status) {
+    this.log.youCannotActivateRoute('debug', [nodeReq.method, nodeReq.url]);
+    nodeRes.statusCode = status || Status.UNAUTHORIZED;
+    nodeRes.end();
+  }
+
+  protected setParams() {
+    if (this.queryString) {
+      this.req.queryParams = parse(this.queryString);
+    }
+    if (this.pathParamsArr) {
+      this.req.pathParamsArr = this.pathParamsArr;
+      const pathParams: AnyObj = {};
+      this.pathParamsArr.forEach((param) => (pathParams[param.key] = param.value));
+      this.req.pathParams = pathParams;
+    }
+  }
+
   protected loadErrorHandler(err: any) {
     const errorHandler = this.req.injector.get(ControllerErrorHandler);
     errorHandler.handleError(err);
-  }
-
-  protected decodeUrl(url: string) {
-    return decodeURI(url);
   }
 }
