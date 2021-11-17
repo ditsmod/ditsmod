73b793decf1360d18cfc8fb95cf6a659087a5385
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-11-17 18:06:38 +0200

    refactor(core): moved part code to new ImportsResolver

--- packages/core/src/module-factory.ts
+++ packages/core/src/module-factory.ts
@@ -114,7 +114,6 @@ export class ModuleFactory {
     this.importModules();
     this.mergeProviders();
     const controllersMetadata = this.getControllersMetadata();
-    this.resolveImportedProviders();
 
     return this.appMetadataMap.set(modOrObj, {
       prefixPerMod,
@@ -419,94 +418,6 @@ export class ModuleFactory {
     }
 
     return arrControllerMetadata;
-  }
-
-  protected resolveImportedProviders() {
-    this.getImportedProviders().forEach(({ providersPerMod, providersPerRou, providersPerReq }, module) => {
-      const depsPerMod = this.getDeps(providersPerMod);
-      const depsPerRou = this.getDeps(providersPerRou);
-      const depsPerReq = this.getDeps(providersPerReq);
-      if ([...depsPerMod, ...depsPerRou, ...depsPerReq].length) {
-        console.log('='.repeat(80));
-        console.log(module);
-      }
-      this.searchDeps(module, depsPerMod, ['Mod']);
-      this.searchDeps(module, depsPerRou, ['Rou', 'Mod']);
-      this.searchDeps(module, depsPerReq, ['Req', 'Rou', 'Mod']);
-    });
-  }
-
-  protected getDeps(providers: Set<ServiceProvider>, allDeps?: boolean, allTokens: Set<any> = new Set()): Set<any> {
-    const resolved = ReflectiveInjector.resolve(Array.from(providers));
-    const restProviders = new Set<any>();
-
-    resolved.forEach(({ resolvedFactories }) =>
-      resolvedFactories.forEach((rf) =>
-        rf.dependencies.forEach((dep) => {
-          const { token } = dep.key;
-          allTokens.add(token);
-          if (token instanceof Type) {
-            restProviders.add(token);
-          }
-        })
-      )
-    );
-
-    allTokens = this.excludeDefaultTokens(allTokens);
-
-    if (allDeps && restProviders.size) {
-      return this.getDeps(restProviders, true, allTokens);
-    } else {
-      return allTokens;
-    }
-  }
-
-  protected excludeDefaultTokens(tokens: Set<any>) {
-    const someDefaultTokensPerReq = normalizeProviders([...defaultProvidersPerReq]).map((np) => np.provide);
-    const defaultTokens = [
-      ...someDefaultTokensPerReq,
-      NODE_REQ,
-      NODE_RES,
-      PATH_PARAMS,
-      QUERY_STRING,
-      Injector,
-      Log,
-      RouteMeta,
-      RootMetadata,
-    ];
-    const arr = Array.from(tokens).filter((t) => !defaultTokens.includes(t));
-    return new Set(arr);
-  }
-
-  /**
-   * @param module Module from where imports providers.
-   * @param deps Dependencies of imported providers.
-   * @param scopes Search in this scopes. The scope order is important. This is the order in
-   * which the search will be conducted.
-   */
-  protected searchDeps(module: ModuleType | ModuleWithParams, deps: Set<any>, scopes: ('Mod' | 'Rou' | 'Req')[]) {
-    const meta = this.#moduleManager.getMetadata(module);
-    const importedProviders = new Set<ServiceProvider>();
-    deps.forEach((token) => {
-      let found: boolean = false;
-      scopesLoop: for (const scope of scopes) {
-        const providers = getUniqProviders(meta[`providersPer${scope}`]);
-        const tokens = normalizeProviders(providers).map((p) => p.provide);
-        const len = tokens.length;
-        for (let i = 0; i < len; i++) {
-          if (tokens[i] === token) {
-            importedProviders.add(providers[i]);
-            found = true;
-            break scopesLoop;
-          }
-        }
-      }
-      if (!found) {
-        console.log('not found', token);
-      } else {
-        console.log('done!', token);
-      }
-    });
   }
 
   protected getImportedProviders() {
---
+++ packages/core/src/services/imports-resolver.ts
@@ -0,0 +1,104 @@
+import { Injectable, Injector, ReflectiveInjector, Type } from '@ts-stack/di';
+
+import { NODE_REQ, NODE_RES, PATH_PARAMS, QUERY_STRING } from '../constans';
+import { RootMetadata } from '../models/root-metadata';
+import { ImportedProviders, ModuleType, ModuleWithParams, ServiceProvider } from '../types/mix';
+import { RouteMeta } from '../types/route-data';
+import { getUniqProviders } from '../utils/get-uniq-providers';
+import { normalizeProviders } from '../utils/ng-utils';
+import { defaultProvidersPerReq } from './default-providers-per-req';
+import { Log } from './log';
+import { ModuleManager } from './module-manager';
+
+@Injectable()
+export class ImportsResolver {
+  constructor(private moduleManager: ModuleManager) {}
+
+  protected resolveImportedProviders(map: Map<ModuleType | ModuleWithParams, ImportedProviders>) {
+    map.forEach(({ providersPerMod, providersPerRou, providersPerReq }, module) => {
+      const depsPerMod = this.getDeps(providersPerMod);
+      const depsPerRou = this.getDeps(providersPerRou);
+      const depsPerReq = this.getDeps(providersPerReq);
+      if ([...depsPerMod, ...depsPerRou, ...depsPerReq].length) {
+        console.log('='.repeat(80));
+        console.log(module);
+      }
+      this.searchDeps(module, depsPerMod, ['Mod']);
+      this.searchDeps(module, depsPerRou, ['Rou', 'Mod']);
+      this.searchDeps(module, depsPerReq, ['Req', 'Rou', 'Mod']);
+    });
+  }
+
+  protected getDeps(providers: Set<ServiceProvider>, allDeps?: boolean, allTokens: Set<any> = new Set()): Set<any> {
+    const resolved = ReflectiveInjector.resolve(Array.from(providers));
+    const restProviders = new Set<any>();
+
+    resolved.forEach(({ resolvedFactories }) =>
+      resolvedFactories.forEach((rf) =>
+        rf.dependencies.forEach((dep) => {
+          const { token } = dep.key;
+          allTokens.add(token);
+          if (token instanceof Type) {
+            restProviders.add(token);
+          }
+        })
+      )
+    );
+
+    allTokens = this.excludeDefaultTokens(allTokens);
+
+    if (allDeps && restProviders.size) {
+      return this.getDeps(restProviders, true, allTokens);
+    } else {
+      return allTokens;
+    }
+  }
+
+  protected excludeDefaultTokens(tokens: Set<any>) {
+    const someDefaultTokensPerReq = normalizeProviders([...defaultProvidersPerReq]).map((np) => np.provide);
+    const defaultTokens = [
+      ...someDefaultTokensPerReq,
+      NODE_REQ,
+      NODE_RES,
+      PATH_PARAMS,
+      QUERY_STRING,
+      Injector,
+      Log,
+      RouteMeta,
+      RootMetadata,
+    ];
+    const arr = Array.from(tokens).filter((t) => !defaultTokens.includes(t));
+    return new Set(arr);
+  }
+
+  /**
+   * @param module Module from where imports providers.
+   * @param deps Dependencies of imported providers.
+   * @param scopes Search in this scopes. The scope order is important. This is the order in
+   * which the search will be conducted.
+   */
+  protected searchDeps(module: ModuleType | ModuleWithParams, deps: Set<any>, scopes: ('Mod' | 'Rou' | 'Req')[]) {
+    const meta = this.moduleManager.getMetadata(module);
+    const importedProviders = new Set<ServiceProvider>();
+    deps.forEach((token) => {
+      let found: boolean = false;
+      scopesLoop: for (const scope of scopes) {
+        const providers = getUniqProviders(meta[`providersPer${scope}`]);
+        const tokens = normalizeProviders(providers).map((p) => p.provide);
+        const len = tokens.length;
+        for (let i = 0; i < len; i++) {
+          if (tokens[i] === token) {
+            importedProviders.add(providers[i]);
+            found = true;
+            break scopesLoop;
+          }
+        }
+      }
+      if (!found) {
+        console.log('not found', token);
+      } else {
+        console.log('done!', token);
+      }
+    });
+  }
+}
