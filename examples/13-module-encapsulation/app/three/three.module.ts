552f4b1500fcbd335d4b5e4e5a59df95eed32a96
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-11-19 18:40:34 +0200

    chore(example): some fix in example 13

--- examples/13-module-encapsulation/app/three/three.module.ts
+++ examples/13-module-encapsulation/app/three/three.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@ditsmod/core';
 
-import { OneModule } from '../one/one.module';
 import { TwoModule } from '../two/two.module';
 import { ThreeService } from './three.service';
 
 @Module({
-  imports: [TwoModule, OneModule],
+  imports: [TwoModule],
   providersPerReq: [ThreeService],
   exports: [ThreeService],
 })
