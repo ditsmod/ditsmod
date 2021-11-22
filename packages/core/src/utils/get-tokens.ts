69bd007812166fd75bc0caa847065f9f5fb1f311
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-11-22 22:25:34 +0200

    feat(core): improvements getTokens()
    Now getTokens() accept providers or tokens.

--- packages/core/src/utils/get-tokens.ts
+++ packages/core/src/utils/get-tokens.ts
@@ -1,13 +1,25 @@
 import { ServiceProvider } from '../types/mix';
 import { normalizeProviders } from './ng-utils';
+import { isProvider } from './type-guards';
 
 export function getToken(provider: ServiceProvider): any {
   return normalizeProviders([provider]).map((p) => p.provide)[0];
 }
 
-export function getTokens(provider: ServiceProvider): any[];
-export function getTokens(providers: ServiceProvider[] | ReadonlyArray<ServiceProvider>): any[];
-export function getTokens(providerOrProviders: ServiceProvider | ServiceProvider[] | ReadonlyArray<ServiceProvider>): any[] {
-  const arr = Array.isArray(providerOrProviders) ? providerOrProviders : [providerOrProviders] as ServiceProvider[];
-  return normalizeProviders(arr).map((p) => p.provide);
+export function getTokens(providerOrToken: any): any[];
+export function getTokens(providersOrTokens: any[] | ReadonlyArray<any>): any[];
+export function getTokens(
+  providerOrProviders: ServiceProvider | ServiceProvider[] | ReadonlyArray<ServiceProvider>
+): any[] {
+  const arr = Array.isArray(providerOrProviders) ? providerOrProviders : ([providerOrProviders] as ServiceProvider[]);
+  const tokens: any[] = [];
+  arr.forEach((item) => {
+    if (isProvider(item)) {
+      tokens.push(getToken(item));
+    } else {
+      tokens.push(item);
+    }
+  });
+
+  return tokens;
 }
