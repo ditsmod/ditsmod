942e32b3ef50c6ef2c86902c97b61aeab7d2bec0
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-11-24 22:20:25 +0200

    fix(body-parser): exports extensions from BodyParserModule

--- packages/body-parser/src/body-parser.module.ts
+++ packages/body-parser/src/body-parser.module.ts
@@ -3,11 +3,11 @@ import { edk, Module } from '@ditsmod/core';
 import { BodyParserExtension, BODY_PARSER_EXTENSIONS } from './body-parser.extension';
 
 @Module({
-  providersPerApp: [
+  extensions: [
     BodyParserExtension,
     { provide: BODY_PARSER_EXTENSIONS, useExisting: BodyParserExtension, multi: true },
     { provide: `BEFORE ${edk.PRE_ROUTER_EXTENSIONS}`, useExisting: BodyParserExtension, multi: true },
   ],
-  extensions: [BODY_PARSER_EXTENSIONS],
+  exports: [BODY_PARSER_EXTENSIONS, BodyParserExtension, `BEFORE ${edk.PRE_ROUTER_EXTENSIONS}`],
 })
 export class BodyParserModule {}
