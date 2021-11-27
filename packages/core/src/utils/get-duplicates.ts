e41569fed47b47dd5744018da21a0b180f8b0995
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-11-27 16:25:45 +0200

    refactor(core): a bit refactoring getDuplicates()

--- packages/core/src/utils/get-duplicates.ts
+++ packages/core/src/utils/get-duplicates.ts
@@ -3,6 +3,6 @@
  */
 export function getDuplicates<T>(arr: T[]) {
   arr = arr || [];
-  const duplicates = arr.filter((value, index, self) => self.indexOf(value) != index);
+  const duplicates = arr.filter((value, index) => arr.indexOf(value) != index);
   return [...new Set(duplicates)];
 }
