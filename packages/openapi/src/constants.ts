33c415a5d51bd96ed2eb285a30bbd13e8349b23a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-01-09 07:58:45 +0200

    refactor(openapi): removed OAS tags

--- packages/openapi/src/constants.ts
+++ packages/openapi/src/constants.ts
@@ -5,15 +5,7 @@ export const DEFAULT_OAS_OBJECT: XOasObject = {
   openapi,
   servers: [{ url: 'http://localhost:3000' }],
   info: { title: 'Testing @ditsmod/openapi with default value', version: '0.0.0' },
-  tags: [
-    {
-      name: 'NonOasRoutes',
-      description:
-        'Routes that use a decorator `@Route()`. If you want to change this description, ' +
-        '[use tags](https://swagger.io/docs/specification/grouping-operations-with-tags/) ' +
-        'for `@OasRoute()` imported from @ditsmod/openapi.',
-    },
-  ],
+  tags: [],
   components: {}
 };
 
