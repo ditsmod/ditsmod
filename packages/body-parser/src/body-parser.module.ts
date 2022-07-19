99c9b49f3372c5e92b0a0f5dd14f8206b5af7053
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-07-19 18:56:43 +0300

    docs(body-parser): added description for BodyParserModule

--- packages/body-parser/src/body-parser.module.ts
+++ packages/body-parser/src/body-parser.module.ts
@@ -2,6 +2,10 @@ import { Module, PRE_ROUTER_EXTENSIONS } from '@ditsmod/core';
 
 import { BodyParserExtension, BODY_PARSER_EXTENSIONS } from './body-parser.extension';
 
+/**
+ * Adds `BodyParserInterceptor` to all requests with HTTP methods specified in `bodyParserConfig.acceptMethods`.
+ * This is done using `BodyParserExtension` in `BODY_PARSER_EXTENSIONS` group.
+ */
 @Module({
   extensions: [[BODY_PARSER_EXTENSIONS, PRE_ROUTER_EXTENSIONS, BodyParserExtension, true]],
 })
