13b560fbd59717f8cadb723a6b47cae1900f129f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-07-19 18:56:59 +0300

    docs(router): added description for RouterModule

--- packages/router/src/router.module.ts
+++ packages/router/src/router.module.ts
@@ -2,5 +2,8 @@ import { Module, Router } from '@ditsmod/core';
 
 import { DefaultRouter } from './router';
 
+/**
+ * Sets `Router` provider on application scope.
+ */
 @Module({ providersPerApp: [{ provide: Router, useClass: DefaultRouter }] })
 export class RouterModule {}
