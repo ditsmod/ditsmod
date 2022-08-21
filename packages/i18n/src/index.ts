cc7c736df662cdb3151785347cfa92e038dff43a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-21 16:20:03 +0300

    refactor(i18n): added ISO639 to export

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -2,3 +2,4 @@ export { I18nModule } from './i18n.module';
 export { I18N_EXTENSIONS, I18N_TRANSLATIONS, I18nTranslation } from './types/mix';
 export { I18nLogMediator } from './i18n-log-mediator';
 export { I18nService } from './i18n.service';
+export * from './types/iso-639';
