188a0999e172d0cee6e75bf9d2a0e8b436f7ea2d
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-23 14:41:58 +0300

    chore(examples): added more getDictGroup()

--- examples/15-i18n/src/app/first/locales/current/translations.ts
+++ examples/15-i18n/src/app/first/locales/current/translations.ts
@@ -1,7 +1,7 @@
-import { DictGroup } from '@ditsmod/i18n';
+import { DictGroup, getDictGroup } from '@ditsmod/i18n';
 
 import { CommonDict } from '@dict/first/common.dict';
 
 export const currentTranslations: DictGroup[] = [
-  [CommonDict],
+  getDictGroup(CommonDict),
 ];
--- examples/15-i18n/src/app/second/locales/imported/first/translations.ts
+++ examples/15-i18n/src/app/second/locales/imported/first/translations.ts
@@ -1,9 +1,9 @@
-import { DictGroup } from '@ditsmod/i18n';
+import { DictGroup, getDictGroup } from '@ditsmod/i18n';
 
 import { CommonDict } from '@dict/first/common.dict';
 import { CommonEnDict } from './en/common-en.dict';
 import { CommonUkDict } from './uk/common-uk.dict';
 
 export const importedTranslations: DictGroup[] = [
-  [CommonDict, CommonEnDict, CommonUkDict],
+  getDictGroup(CommonDict, CommonEnDict, CommonUkDict),
 ];
