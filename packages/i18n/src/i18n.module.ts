09fb929aee3f2f1807bd11151ecbe2e139120271
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-25 18:07:56 +0300

    refactor(i18n): removed unnecessary dependecy

--- packages/i18n/src/i18n.module.ts
+++ packages/i18n/src/i18n.module.ts
@@ -3,17 +3,12 @@ import { Module, PRE_ROUTER_EXTENSIONS } from '@ditsmod/core';
 import { I18nLogMediator } from './i18n-log-mediator';
 import { I18nExtension } from './i18n.extension';
 import { DictService } from './dict.service';
-import { I18nOptions, I18N_EXTENSIONS, I18N_TRANSLATIONS } from './types/mix';
+import { I18nOptions, I18N_EXTENSIONS } from './types/mix';
 import { DictPerModService } from './dict-per-mod.service';
 
 @Module({
   extensions: [[I18N_EXTENSIONS, PRE_ROUTER_EXTENSIONS, I18nExtension, true]],
-  providersPerMod: [
-    I18nOptions,
-    I18nLogMediator,
-    DictPerModService,
-    { provide: I18N_TRANSLATIONS, useValue: [], multi: true } // @todo Remove this after fix import resolver (issue with @Optional())
-  ],
+  providersPerMod: [I18nOptions, I18nLogMediator, DictPerModService],
   providersPerReq: [DictService],
   exports: [DictService],
 })
