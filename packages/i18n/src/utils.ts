13c8105737289d5defe8a2af1f05fbdb568a6b33
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-27 03:46:46 +0300

    feat(i18n): added plugin for Plugins utils

--- packages/i18n/src/utils.ts
+++ packages/i18n/src/utils.ts
@@ -1,4 +1,4 @@
-import { ServiceProvider } from '@ditsmod/core';
+import { Providers } from '@ditsmod/core';
 import { Type } from '@ts-stack/di';
 
 import { Dictionary, DictGroup, Translations, I18N_TRANSLATIONS, I18nOptions } from './types/mix';
@@ -7,21 +7,18 @@ export function getDictGroup<T extends Type<Dictionary>>(base: T, ...dicts: T[])
   return [base, ...dicts];
 }
 
-/**
- * Stability: 1 - Experimental.
- *
- * @param self Here expect `this` variable.
- *
- * @todo Investigate why an object with the name of the module is passed as a key to "self" parameter.
- */
-export function getI18nProviders(
-  self: any,
-  translations: Translations,
-  i18nOptions = new I18nOptions()
-): ServiceProvider[] {
-  translations.moduleName = translations.moduleName || Object.keys(self)[0];
-  return [
-    { provide: I18N_TRANSLATIONS, useValue: translations, multi: true },
-    { provide: I18nOptions, useValue: i18nOptions },
-  ];
+export class I18nProviders extends Providers {
+  /**
+   * Stability: 1 - Experimental.
+   *
+   * @param self Here expect `this` variable.
+   *
+   * @todo Investigate why an object with the name of the module is passed as a key to "self" parameter.
+   */
+  getI18nProviders(self: any, translations: Translations, i18nOptions = new I18nOptions()) {
+    translations.moduleName = translations.moduleName || Object.keys(self)[0];
+    this.useAnyValue(I18N_TRANSLATIONS, translations, true).useValue(I18nOptions, i18nOptions);
+    
+    return this;
+  }
 }
