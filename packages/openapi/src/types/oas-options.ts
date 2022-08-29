fea3df063d6e9fd8b0e82a05423e5051159dac48
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-30 01:24:10 +0300

    docs(openapi): added more description for OasOptions

--- packages/openapi/src/types/oas-options.ts
+++ packages/openapi/src/types/oas-options.ts
@@ -1,7 +1,8 @@
 import { XParameterObject, ReferenceObject } from '@ts-stack/openapi-spec';
 
 /**
- * Applies to importing `OasModuleWithParams`.
+ * Related to OpenAPI documentation, passed in the module metadata in the `oasOptions` property,
+ * used for centrally adding `paratemers` and `tags` to `@OasRoute()` in the current module.
  */
 export interface OasOptions {
   paratemers?: (XParameterObject | ReferenceObject)[];
