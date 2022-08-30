ef92dc1e0a3adf4b2776381405ed686ec13f0992
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-08-30 13:53:05 +0300

    chore(examples): fixed example 10

--- examples/10-openapi/src/app/modules/routed/first/first.module.ts
+++ examples/10-openapi/src/app/modules/routed/first/first.module.ts
@@ -1,10 +1,10 @@
 import { Module, ModuleWithParams } from '@ditsmod/core';
-import { OpenapiModule } from '@ditsmod//openapi';
+import { OpenapiModule } from '@ditsmod/openapi';
 
 import { openapiModuleWithParams } from '../../services/openapi/openapi.module';
 import { FirstController } from './first.controller';
 
-const firstOpenapiModule: ModuleWithParams<OpenapiModule> = {...openapiModuleWithParams, path: ''}
+const firstOpenapiModule: ModuleWithParams<OpenapiModule> = { ...openapiModuleWithParams, path: '' };
 
 @Module({
   imports: [firstOpenapiModule],
