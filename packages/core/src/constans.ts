cd766885327ad9807d7f17212cae62def20b2139
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-11 13:32:24 +0300

    docs(core): fixed typos in descriptions extensions groups

--- packages/core/src/constans.ts
+++ packages/core/src/constans.ts
@@ -7,11 +7,11 @@ import { Extension, ServiceProvider } from './types/mix';
 import { PathParam } from './types/router';
 import { NodeRequest, NodeResponse } from './types/server-options';
 /**
- * Group extensions that setting routes for router.
+ * A group of extensions that setting routes for router.
  */
 export const PRE_ROUTER_EXTENSIONS = new InjectionToken<Extension<void>[]>('PRE_ROUTER_EXTENSIONS');
 /**
- * Group extensions that returns `MetadataPerMod2[]` for a router.
+ * A group of extensions that returns `MetadataPerMod2[]` for a router.
  */
 export const ROUTES_EXTENSIONS = new InjectionToken<Extension<MetadataPerMod2>[]>('ROUTES_EXTENSIONS');
 export const HTTP_INTERCEPTORS = new InjectionToken<HttpInterceptor[]>('HTTP_INTERCEPTORS');
