f376a3dcf4b0536fc0fbb136850fd8af13177479
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-12 22:08:48 +0300

    refactor(i18n): refactoring I18nExtension

--- packages/i18n/src/i18n.extension.ts
+++ packages/i18n/src/i18n.extension.ts
@@ -1,4 +1,4 @@
-import { Extension, ExtensionsManager, ROUTES_EXTENSIONS } from '@ditsmod/core';
+import { Extension, ExtensionsManager, MetadataPerMod2, ModuleExtract, ROUTES_EXTENSIONS } from '@ditsmod/core';
 import { Inject, Injectable, Optional, Type } from '@ts-stack/di';
 
 import { I18nLogMediator } from './i18n-log-mediator';
@@ -12,7 +12,8 @@ export class I18nExtension implements Extension<void> {
   constructor(
     private log: I18nLogMediator,
     private extensionsManager: ExtensionsManager,
-    @Optional() @Inject(I18N_TRANSLATIONS) private translations?: Translations[],
+    private moduleExtract: ModuleExtract,
+    @Optional() @Inject(I18N_TRANSLATIONS) private translations?: Translations[]
   ) {}
 
   async init() {
@@ -20,8 +21,7 @@ export class I18nExtension implements Extension<void> {
       return;
     }
 
-    const { moduleName } = this.extensionsManager;
-    if (moduleName == 'I18nModule') {
+    if (this.moduleExtract.moduleName == 'I18nModule') {
       return;
     }
 
@@ -33,56 +33,37 @@ export class I18nExtension implements Extension<void> {
 
     const aMetadataPerMod2 = await this.extensionsManager.init(ROUTES_EXTENSIONS);
     for (const translation of this.translations) {
-      for (const dictionariesGroup of translation.current || []) {
-        const token = dictionariesGroup[0]; // First class uses as group's token
-
-        const allLngs = dictionariesGroup.map((d) => d.prototype.getLng());
-        const allMethods = Object.getOwnPropertyNames(token.prototype).filter(p => p != 'constructor' && p != 'getLng').slice(0, 3);
-        this.log.foundLngs(this, token.name, allLngs, allMethods);
-
-        for (const dict of dictionariesGroup) {
-          if (token !== dict) {
-            this.logMissingMethods(token, dict);
-          }
-          for (const metadataPerMod2 of aMetadataPerMod2) {
-            const { providersPerMod } = metadataPerMod2;
-            providersPerMod.push({ provide: token, useClass: dict, multi: true });
-          }
-        }
-      }
       for (const dictionariesGroup of translation.imported || []) {
         const token = dictionariesGroup[0]; // First class uses as group's token
-
-        /**
-         * If the dictionariesGroup contains an overrided dictionary,
-         * then the token is not passed to DI as provider.
-         */
-        let group: Type<Dictionary>[] = dictionariesGroup.slice(1);
-        const lngList = group.map((d) => d.prototype.getLng());
-        if (!lngList.includes(token.prototype.getLng())) {
-          group = dictionariesGroup;
-        }
-
+        const group = dictionariesGroup.slice(1);
+        this.addDictToDi(token, group, aMetadataPerMod2);
         const allLngs = group.map((d) => d.prototype.getLng());
-        const allMethods = Object.getOwnPropertyNames(token.prototype).filter(p => p != 'constructor' && p != 'getLng').slice(0, 5);
-        this.log.overridedLngs(this, token.name, allLngs, allMethods);
-
-        for (const dict of group) {
-          if (token !== dict) {
-            this.logMissingMethods(token, dict);
-          }
-          for (const metadataPerMod2 of aMetadataPerMod2) {
-            const { providersPerMod } = metadataPerMod2;
-            providersPerMod.push({ provide: token, useClass: dict, multi: true });
-          }
-        }
+        this.log.overridedLngs(this, token.name, allLngs);
+      }
+      for (const dictionariesGroup of translation.current || []) {
+        const token = dictionariesGroup[0]; // First class uses as group's token
+        this.addDictToDi(token, dictionariesGroup, aMetadataPerMod2);
+        const allLngs = dictionariesGroup.map((d) => d.prototype.getLng());
+        this.log.foundLngs(this, token.name, allLngs);
       }
     }
 
     this.#inited = true;
   }
 
-  protected logMissingMethods(base: Type<Dictionary>, extended: Type<Dictionary>) {
+  protected addDictToDi(token: Type<Dictionary>, group: Type<Dictionary>[], aMetadataPerMod2: MetadataPerMod2[]) {
+    for (const dict of group) {
+      if (token !== dict) {
+        this.logMissingMethodsIfExists(token, dict);
+      }
+      for (const metadataPerMod2 of aMetadataPerMod2) {
+        const { providersPerMod } = metadataPerMod2;
+        providersPerMod.push({ provide: token, useClass: dict, multi: true });
+      }
+    }
+  }
+
+  protected logMissingMethodsIfExists(base: Type<Dictionary>, extended: Type<Dictionary>) {
     const baseMethods = Object.getOwnPropertyNames(base.prototype);
     const overridedMethods = Object.getOwnPropertyNames(extended.prototype);
     const missingMethods: string[] = [];
