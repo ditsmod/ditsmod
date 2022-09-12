4165007005e8a0e4be411199f8efa210e0bd8d32
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-12 22:11:06 +0300

    docs(i18n): added descriptions for I18N_EXTENSIONS and I18N_TRANSLATIONS

--- packages/i18n/src/types/mix.ts
+++ packages/i18n/src/types/mix.ts
@@ -17,5 +17,11 @@ export class I18nOptions {
   lngParam?: string = 'lng';
 }
 export type DictGroup<T extends Type<Dictionary> = Type<Dictionary>> = [T, ...T[]];
+/**
+ * A group of extensions that add dictionaries to providers for internalization.
+ */
 export const I18N_EXTENSIONS = new InjectionToken<Extension<void>>('I18N_EXTENSIONS');
+/**
+ * Current and imported dictionaries for internalization.
+ */
 export const I18N_TRANSLATIONS = new InjectionToken<DictGroup[][]>('I18N_TRANSLATIONS');
