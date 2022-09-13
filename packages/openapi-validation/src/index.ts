0e4f8da556abba30493369f2f8bd74b218a01c68
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-13 15:13:39 +0300

    fix(openapi-validation): added some types to exports list

--- packages/openapi-validation/src/index.ts
+++ packages/openapi-validation/src/index.ts
@@ -3,3 +3,4 @@ export { AssertService } from './assert.service';
 export { AssertDict } from './locales/current';
 export { ValidationModule } from './validation.module';
 export { VALIDATION_EXTENSIONS } from './constants';
+export { IS_REQUIRED, VALIDATION_ARGS } from './types';
