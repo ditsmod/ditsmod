50492557d91f0ac410ac7d333168cbfcdb94624c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-13 22:42:19 +0300

    chore(examples): migration to i18n-beta.12

--- examples/15-i18n/src/app/third/dict.service.ts
+++ examples/15-i18n/src/app/third/dict.service.ts
@@ -1,16 +1,16 @@
 import { Req } from '@ditsmod/core';
-import { DictPerModService, I18nLogMediator, I18nOptions, ISO639 } from '@ditsmod/i18n';
+import { DictService, I18nLogMediator, I18nOptions, ISO639 } from '@ditsmod/i18n';
 import { Injectable, Injector, Optional } from '@ts-stack/di';
 
 @Injectable()
-export class MyDictService extends DictPerModService {
+export class MyDictService extends DictService {
   constructor(
-    protected req: Req,
     protected override injector: Injector,
     protected override log: I18nLogMediator,
     @Optional() protected override i18nOptions?: I18nOptions,
+    @Optional() protected override req?: Req,
   ) {
-    super(injector, log, i18nOptions);
+    super(injector, log, i18nOptions, req);
   }
 
   override set lng(lng: ISO639) {
@@ -21,12 +21,12 @@ export class MyDictService extends DictPerModService {
     if (this._lng) {
       return this._lng;
     }
-    const lng = this.getHeaderLng() || this.req.queryParams[this.i18nOptions?.lngParam || 'lng'];
+    const lng = this.getHeaderLng() || this.req?.queryParams[this.i18nOptions?.lngParam || 'lng'];
     return lng || this.i18nOptions?.defaultLng;
   }
 
   protected getHeaderLng(): ISO639 | void {
-    const acceptLanguage = this.req.nodeReq.headers['accept-language']; // Here string like: uk,en-US;q=0.9,en;q=0.8
+    const acceptLanguage = this.req?.nodeReq.headers['accept-language']; // Here string like: uk,en-US;q=0.9,en;q=0.8
     // ... here your code for parsing acceptLanguage, after that you should returns result
     console.log('works custom DictService');
   }
