49d575bfe55e799270a86f7d074ab321361981d8
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-18 00:09:32 +0300

    feat(openapi-validation): introduced ValidationArguments

--- packages/openapi-validation/src/types.ts
+++ packages/openapi-validation/src/types.ts
@@ -1,3 +1,5 @@
+import { Type } from '@ts-stack/di';
+import { Dictionary } from '@ditsmod/i18n';
 import { OasRouteMeta } from '@ditsmod/openapi';
 import { XParameterObject, XSchemaObject } from '@ts-stack/openapi-spec';
 
@@ -9,9 +11,11 @@ export class ValidationRouteMeta extends OasRouteMeta {
 /**
  * This OAS property contains validation error arguments.
  */
-export const VALIDATION_ARGS = 'x-validation-args';
+export const VALIDATION_ARGS = 'x-invalid-args';
 
 /**
  * This OAS property indicates whether requestBody property is required or not.
  */
 export const IS_REQUIRED = 'x-body-property-required';
+
+export type ValidationArguments<T extends Type<Dictionary>> = [T, keyof T['prototype'], ...any[]];
