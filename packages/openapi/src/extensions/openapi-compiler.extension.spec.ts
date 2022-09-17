d03922876ccd4ef3c635bf295bc05be705795e92
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-17 22:50:03 +0300

    test(openapi): fixed tests for OpenapiCompilerExtension

--- packages/openapi/src/extensions/openapi-compiler.extension.spec.ts
+++ packages/openapi/src/extensions/openapi-compiler.extension.spec.ts
@@ -33,7 +33,7 @@ describe('OpenapiCompilerExtension', () => {
 
   beforeEach(() => {
     const injector = ReflectiveInjector.resolveAndCreate([]);
-    mock = new MockOpenapiCompilerExtension(injector, injector, null as any, log as any);
+    mock = new MockOpenapiCompilerExtension(injector, injector, null as any, log as any, {});
   });
 
   describe('setSecurityInfo()', () => {
