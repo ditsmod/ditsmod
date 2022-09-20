1b25d27af80073109e999f30374c77e2c2477fc9
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-20 17:32:37 +0300

    chore(examples): added example how add log level

--- examples/01-hello-world/src/app/app.module.ts
+++ examples/01-hello-world/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { RootModule } from '@ditsmod/core';
+import { Providers, RootModule } from '@ditsmod/core';
 import { RouterModule } from '@ditsmod/router';
 
 import { HelloWorldController } from './hello-world.controller';
@@ -6,5 +6,6 @@ import { HelloWorldController } from './hello-world.controller';
 @RootModule({
   imports: [RouterModule],
   controllers: [HelloWorldController],
+  providersPerApp: [...new Providers().useLogConfig({ level: 'info' })],
 })
 export class AppModule {}
