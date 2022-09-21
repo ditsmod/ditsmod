350878a48f29f6020ee3d4d91583b13900ba46ed
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-21 13:29:18 +0300

    fix(i18n): added some providers per app

--- packages/i18n/src/i18n.module.ts
+++ packages/i18n/src/i18n.module.ts
@@ -1,9 +1,13 @@
 import { Module, PRE_ROUTER_EXTENSIONS } from '@ditsmod/core';
 
+import { DictService } from './dict.service';
+import { I18nLogMediator } from './i18n-log-mediator';
+import { I18nTransformer } from './i18n-transformer';
 import { I18nExtension } from './i18n.extension';
 import { I18N_EXTENSIONS } from './types/mix';
 
 @Module({
+  providersPerApp: [DictService, I18nTransformer, I18nLogMediator],
   extensions: [
     { extension: I18nExtension, groupToken: I18N_EXTENSIONS, nextToken: PRE_ROUTER_EXTENSIONS, exported: true },
   ]
