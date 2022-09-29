dba6095bf4120ccea042fce704b509854736fdf5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-09-30 00:58:02 +0300

    refactor(openapi-validation): hided INVALID_ARGS from export in index

--- packages/openapi-validation/src/index.ts
+++ packages/openapi-validation/src/index.ts
@@ -2,7 +2,6 @@ export { AjvService } from './ajv.service';
 export { AssertDict, AssertUkDict } from './locales/current';
 export { ValidationModule } from './validation.module';
 export { VALIDATION_EXTENSIONS, AJV_OPTIONS } from './constants';
-export { INVALID_ARGS } from './types';
 export { ValidationOptions } from './validation-options';
 export { ParametersInterceptor } from './parameters.interceptor';
 export { RequestBodyInterceptor } from './request-body.interceptor';
