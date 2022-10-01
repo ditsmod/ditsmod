21b7eae16eed8ee828d8064c97f7988b2979872f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-01 03:34:38 +0300

    chore(examples): fixed example 16 after upgrade yaml

--- examples/16-openapi-validation/src/app/modules/service/openapi/oas-options.ts
+++ examples/16-openapi-validation/src/app/modules/service/openapi/oas-options.ts
@@ -1,15 +1,15 @@
 import { OasOptions } from '@ditsmod/openapi';
-import { Scalar } from 'yaml/types';
+import { Scalar } from 'yaml';
 
 export const oasOptions: OasOptions = {
   yamlSchemaOptions: {
     customTags: (tags) => {
       tags.push({
-        identify: (token) => token?.prototype?.getLng,
+        identify: (token: any) => token?.prototype?.getLng,
         tag: 'tag:yaml.org,2002:str',
         default: true,
         resolve: () => 'fake resolve for @ditsmod/i18n dictionary',
-        stringify: (node) => (node as Scalar).value.name,
+        stringify: (node: Scalar<any>) => node.value?.name,
       });
       return tags;
     },
