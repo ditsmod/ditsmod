2b214bfac6d816325770ea4223c5e8b2c5ac9acf
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-13 18:57:56 +0300

    refactor(return): refactoring ReturnHttpBackend.

--- packages/return/src/return-http-backend.ts
+++ packages/return/src/return-http-backend.ts
@@ -1,5 +1,5 @@
 import { Injectable, Injector } from '@ts-stack/di';
-import { RouteMeta, HttpBackend, Res, DefaultHttpBackend } from '@ditsmod/core';
+import { RouteMeta, HttpBackend, Res, DefaultHttpBackend, Status, HttpMethod, NODE_REQ } from '@ditsmod/core';
 
 @Injectable()
 export class ReturnHttpBackend extends DefaultHttpBackend implements HttpBackend {
@@ -9,10 +9,25 @@ export class ReturnHttpBackend extends DefaultHttpBackend implements HttpBackend
 
   override async handle() {
     const value = await super.handle(); // Controller's route returned value.
+    let { statusCode } = this.res.nodeRes;
+    if (!statusCode) {
+      const nodeReq = this.injector.get(NODE_REQ);
+      const httpMethod = nodeReq.method as HttpMethod;
+      if (httpMethod == 'GET') {
+        statusCode = Status.OK;
+      } else if (httpMethod == 'POST') {
+        statusCode = Status.CREATED;
+      } else if (httpMethod == 'OPTIONS') {
+        statusCode = Status.NO_CONTENT;
+      } else {
+        statusCode = Status.OK;
+      }
+    }
+
     if (typeof value == 'object' || this.res.nodeRes.getHeader('content-type') == 'application/json') {
-      this.res.sendJson(value);
+      this.res.sendJson(value, statusCode);
     } else {
-      this.res.send(value);
+      this.res.send(value, statusCode);
     }
   }
 }
--- packages/return/src/return.module.ts
+++ packages/return/src/return.module.ts
@@ -2,6 +2,9 @@ import { HttpBackend, Module } from '@ditsmod/core';
 
 import { ReturnHttpBackend } from './return-http-backend';
 
+/**
+ * Allow listen value returned by a controller's route.
+ */
 @Module({
   providersPerReq: [{ provide: HttpBackend, useClass: ReturnHttpBackend }],
   exports: [HttpBackend]
