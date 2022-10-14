0f5ab74880bf083bc93f19cc0bb5bb5d2ffce535
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-14 22:55:21 +0300

    refactor(body-parser): removed MultipartBodyParserOptions

--- packages/body-parser/src/index.ts
+++ packages/body-parser/src/index.ts
@@ -1,4 +1,3 @@
 export { BodyParserModule } from './body-parser.module';
 export { BODY_PARSER_EXTENSIONS } from './body-parser.extension';
 export { BodyParserConfig } from './body-parser-config';
-export { MultipartBodyParserOptions } from './multipart-body-parser-options';
