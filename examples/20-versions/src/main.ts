486c90b117b8ad4372b961d34f2ffc0432a1eadc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-29 01:13:04 +0300

    chore(examples): temporary removed example 20

--- examples/20-versions/src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { Application } from '@ditsmod/core';
-
-import { AppModule } from './app/app.module';
-
-new Application().bootstrap(AppModule).catch((err) => {
-  console.log(err);
-});
