96b37d6945928b321fb65e7e82215103467646d7
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-31 01:49:48 +0200

    test(examples): added tests for example 3

---
+++ examples/03-route-guards/test/main.spec.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { describe, it, jest } from '@jest/globals';
+import { Application } from '@ditsmod/core';
+
+import { AppModule } from '../src/app/app.module';
+
+describe('03-route-guards', () => {
+  console.log = jest.fn(); // Hide logs
+
+  it('should works', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/')
+      .expect(200)
+      .expect('ok');
+
+    server.close();
+  });
+
+  it('should throw 401', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/unauth')
+      .expect(401);
+
+    server.close();
+  });
+
+  it('should throw 403', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/forbidden')
+      .expect(403);
+
+    server.close();
+  });
+});
