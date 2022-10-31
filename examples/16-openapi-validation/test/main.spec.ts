2f32310e31229ce5921ab375e8a84a554b8138d2
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-31 07:19:02 +0200

    test(examples): added tests for example 16

---
+++ examples/16-openapi-validation/test/main.spec.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { describe, it, jest } from '@jest/globals';
+import { Application } from '@ditsmod/core';
+
+import { AppModule } from '../src/app/app.module';
+
+
+describe('16-openapi-validation', () => {
+  console.log = jest.fn(); // Hide logs
+
+  it('works controller', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+
+    await request(server)
+      .get('/users/Kostia')
+      .expect(200)
+      .expect({ username: 'Kostia' });
+    
+    await request(server)
+      .post('/model1')
+      .set('content-type', 'application/json')
+      .send({"numbers": [5]})
+      .expect(200)
+      .expect({ numbers: [ 5 ] });
+    
+    await request(server)
+      .post('/model1')
+      .set('content-type', 'application/json')
+      .send({})
+      .expect(400)
+      .expect({ error: "data must have required property 'numbers'" });
+    
+    await request(server)
+      .post('/model2')
+      .set('content-type', 'application/json')
+      .send({"model1":{"numbers": ["d"]}})
+      .expect(400)
+      .expect({ error: "data/model1/numbers/0 must be number" });
+
+    server.close();
+  });
+});
