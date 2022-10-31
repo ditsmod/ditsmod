75f9a36e59a094169d543b2a97f7a9029ac2f714
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-10-31 03:06:49 +0200

    test(examples): added tests for example 4

---
+++ examples/04-logger/test/main.spec.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata';
+import request from 'supertest';
+import { describe, it, jest } from '@jest/globals';
+import { Application } from '@ditsmod/core';
+
+import { AppModule } from '../src/app/app.module';
+
+describe('04-logger', () => {
+  console.log = jest.fn(); // Hide logs
+
+  it('should works', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/')
+      .expect(200)
+      .expect('ok');
+
+    server.close();
+  });
+
+  it('should works with winston', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/winston')
+      .expect(200);
+
+    server.close();
+  });
+
+  it('should works with bunyan', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/bunyan')
+      .expect(200);
+
+    server.close();
+  });
+
+  it('should works with pino', async () => {
+    const { server } = await new Application().bootstrap(AppModule, false);
+    await request(server)
+      .get('/pino')
+      .expect(200);
+
+    server.close();
+  });
+});
