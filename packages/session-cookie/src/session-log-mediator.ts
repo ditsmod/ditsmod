904126ff69db04b80dff1f52cfc0280c5f22e85c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-11-13 15:06:22 +0200

    refactor(session-cookie): renamed MsgLogFilter to InputLogFilter

--- packages/session-cookie/src/session-log-mediator.ts
+++ packages/session-cookie/src/session-log-mediator.ts
@@ -1,12 +1,12 @@
-import { LogMediator, MsgLogFilter } from '@ditsmod/core';
+import { LogMediator, InputLogFilter } from '@ditsmod/core';
 
 export class SessionLogMediator extends LogMediator {
   cannotSetExpireAndMaxAge(self: object) {
     const className = self.constructor.name;
-    const msgLogFilter = new MsgLogFilter();
-    msgLogFilter.className = className;
-    msgLogFilter.tags = ['session-cookie'];
+    const inputLogFilter = new InputLogFilter();
+    inputLogFilter.className = className;
+    inputLogFilter.tags = ['session-cookie'];
     const msg = `${className}: you cannot set opts.expires and opts.maxAge at the same time. For now, opts.maxAge will be ignored.`;
-    this.setLog('warn', msgLogFilter, msg);
+    this.setLog('warn', inputLogFilter, msg);
   }
 }
