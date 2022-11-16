f954b5d2b2f165c1d493292f73d7b0396ceacc81
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-11-16 20:12:51 +0200

    refactor(core): renamed disableRaisedLogs to disabledRaisedLogs

--- packages/core/src/types/logger.ts
+++ packages/core/src/types/logger.ts
@@ -4,7 +4,7 @@ export class LoggerConfig {
   /**
    * @param level Log level (trace, debug, info etc.)
    * @param hideLogs Specifies whether or not to hide logs.
-   * @param disableRaisedLogs If `LogMediator` is used to throw an error,
+   * @param disabledRaisedLogs If `LogMediator` is used to throw an error,
    * this option allows you to raise the log level. For example,
    * if you set the log level to `info` and the router throws an error
    * about duplicates in routes paths, allowRaisedLog allows you to filter
@@ -12,7 +12,7 @@ export class LoggerConfig {
    * 
    * Default - false.
    */
-  constructor(public level: LogLevel = 'info', public hideLogs?: boolean, public disableRaisedLogs?: boolean) {}
+  constructor(public level: LogLevel = 'info', public hideLogs?: boolean, public disabledRaisedLogs?: boolean) {}
 }
 
 const msg = 'You need to implement "%s" method in "%s"';
