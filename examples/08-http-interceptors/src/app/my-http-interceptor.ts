e84398b24114ea48f831326e3b4270511f7b51bc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-12-28 23:33:30 +0200

    chore(examples): RouteMeta now passing as argument for interceptors.

--- examples/08-http-interceptors/src/app/my-http-interceptor.ts
+++ examples/08-http-interceptors/src/app/my-http-interceptor.ts
@@ -1,12 +1,12 @@
-import { injectable, HttpHandler, HttpInterceptor, Logger, Req, Res } from '@ditsmod/core';
+import { injectable, HttpHandler, HttpInterceptor, Logger, Req, Res, RouteMeta } from '@ditsmod/core';
 
 @injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
   constructor(private req: Req, private res: Res, private logger: Logger) {}
 
-  intercept(next: HttpHandler) {
+  intercept(routeMeta: RouteMeta, next: HttpHandler) {
     // Handling request to `HelloWorldController`
-    return next.handle().finally(() => {
+    return next.handle(routeMeta).finally(() => {
       // You can to do something after, for example, log status:
       if (this.res.nodeRes.headersSent) {
         this.logger.info(`MyHttpInterceptor works! Status code: ${this.res.nodeRes.statusCode}`);
--- examples/16-openapi-validation/test/main.spec.ts
+++ examples/16-openapi-validation/test/main.spec.ts
@@ -4,38 +4,34 @@ import { Application } from '@ditsmod/core';
 
 import { AppModule } from '../src/app/app.module';
 
-
 describe('16-openapi-validation', () => {
   console.log = jest.fn(); // Hide logs
 
   it('controller works', async () => {
     const { server } = await new Application().bootstrap(AppModule, false);
 
-    await request(server)
-      .get('/users/Kostia')
-      .expect(200)
-      .expect({ username: 'Kostia' });
-    
+    await request(server).get('/users/Kostia').expect(200).expect({ username: 'Kostia' });
+
     await request(server)
       .post('/model1')
       .set('content-type', 'application/json')
-      .send({"numbers": [5]})
+      .send({ numbers: [5] })
       .expect(200)
-      .expect({ numbers: [ 5 ] });
-    
+      .expect({ numbers: [5] });
+
     await request(server)
       .post('/model1')
       .set('content-type', 'application/json')
       .send({})
       .expect(400)
       .expect({ error: "data must have required property 'numbers'" });
-    
+
     await request(server)
       .post('/model2')
       .set('content-type', 'application/json')
-      .send({"model1":{"numbers": ["d"]}})
+      .send({ model1: { numbers: ['d'] } })
       .expect(400)
-      .expect({ error: "data/model1/numbers/0 must be number" });
+      .expect({ error: 'data/model1/numbers/0 must be number' });
 
     server.close();
   });
