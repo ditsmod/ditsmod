8044ca109bbf566bd0f65d74b972c34077abf16a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2022-12-28 23:32:49 +0200

    refactor(body-parser): RouteMeta now passing as argument for interceptors.

--- packages/body-parser/src/body-parser.interceptor.ts
+++ packages/body-parser/src/body-parser.interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpHandler, HttpInterceptor, Req } from '@ditsmod/core';
+import { HttpHandler, HttpInterceptor, Req, RouteMeta } from '@ditsmod/core';
 import { injectable, optional } from '@ditsmod/core';
 import { parse, Headers, Options } from 'get-body';
 
@@ -10,15 +10,15 @@ export class BodyParserInterceptor implements HttpInterceptor {
     this.config = Object.assign({}, new BodyParserConfig(), config); // Merge with default.
   }
 
-  async intercept(next: HttpHandler) {
+  async intercept(routeMeta: RouteMeta, next: HttpHandler) {
     const contentType = this.req.nodeReq.headers['content-type'];
     const hasAcceptableHeaders = this.config?.acceptHeaders?.some((type) => contentType?.includes(type));
     if (!hasAcceptableHeaders) {
-      return next.handle();
+      return next.handle(routeMeta);
     }
     const options: Options = { limit: this.config?.maxBodySize };
     this.req.body = await parse(this.req.nodeReq, this.req.nodeReq.headers as Headers, options);
 
-    return next.handle();
+    return next.handle(routeMeta);
   }
 }
