267d3bdec7f7a7343399268f80bd6854019ae708
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-01-06 00:58:13 +0200

    refactor(i18n): introduced RequestContext

--- packages/i18n/src/dict.service.ts
+++ packages/i18n/src/dict.service.ts
@@ -17,7 +17,7 @@ export class DictService {
   ) {}
 
   getAllDictionaries<T extends Class<Dictionary>>(token: T) {
-    const arr = this.injector.get(token, []) as T['prototype'][];
+    const arr = this.injector.get(token, undefined, []) as T['prototype'][];
     return arr.slice(0).reverse();
   }
 
