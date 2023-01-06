7735d3c77929f3552fb152811fdc9e4dad463045
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-01-06 05:57:49 +0200

    refactor(cors): intoduced RequestContext.

--- packages/cors/src/cors.service.ts
+++ packages/cors/src/cors.service.ts
@@ -1,17 +1,16 @@
-import { injectable, optional } from '@ditsmod/core';
+import { injectable, optional, RequestContext } from '@ditsmod/core';
 import { Cookies, CookieOptions } from '@ts-stack/cookies';
-import { Req, Res } from '@ditsmod/core';
 import { CorsOptions, cors, mergeOptions } from '@ts-stack/cors';
 
 @injectable()
 export class CorsService {
-  constructor(private req: Req, private res: Res, @optional() private corsOptions?: CorsOptions) {}
+  constructor(@optional() private corsOptions?: CorsOptions) {}
 
-  setCookie(name: string, value?: any, opts?: CookieOptions) {
-    const cookies = new Cookies(this.req.nodeReq, this.res.nodeRes);
+  setCookie(ctx: RequestContext, name: string, value?: any, opts?: CookieOptions) {
+    const cookies = new Cookies(ctx.req.nodeReq, ctx.res.nodeRes);
     cookies.set(name, value, opts);
     const clonedCorsOptions = { ...(this.corsOptions || {}) };
     clonedCorsOptions.allowCredentials = true;
-    cors(this.req.nodeReq, this.res.nodeRes, mergeOptions(clonedCorsOptions));
+    cors(ctx.req.nodeReq, ctx.res.nodeRes, mergeOptions(clonedCorsOptions));
   }
 }
