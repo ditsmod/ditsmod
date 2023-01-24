7bc29032a4ec3f60e772a82e69211fdf49d1e20d
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-01-24 22:16:07 +0200

    docs(website): fixed config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -136,7 +136,7 @@ module.exports = {
             ...getRedirect(existingPath, '/components-of-ditsmod-app/extensions', ['/extensions/create-extension','/extensions/about-extensions']),
             ...getRedirect(existingPath, '/components-of-ditsmod-app', ['/core']),
             ...getRedirect(existingPath, '/native-modules/', ['/published-modules/']),
-            ...getRedirect(existingPath, '/published-modules/openapi', ['/extensions/openapi']),
+            ...getRedirect(existingPath, '/native-modules/openapi', ['/extensions/openapi']),
             ...getRedirect(existingPath, '/components-of-ditsmod-app/log-mediator', ['/examples/override-core-log-messages']),
           ];
           return arr.length ? arr : undefined; // Return a falsy value: no redirect created
