d24c4819d6dd48b4c12298a6366a82ec00549e41
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-01-30 20:54:02 +0200

    refactor(sqb): added TableMetadata type.

--- packages/sqb/src/utils.ts
+++ packages/sqb/src/utils.ts
@@ -2,14 +2,14 @@ import { Class, reflector } from '@ditsmod/core';
 
 import { TableConfig } from './types';
 
-export function getTableMetadata<T extends Class>(Cls: T, alias: string): [Record<keyof InstanceType<T>, string>, string, string] {
+export function getTableMetadata<T extends Class>(Cls: T, alias: string): TableMetadata<T> {
   const config: TableConfig | undefined = reflector.getClassMetadata(Cls)[0]?.value;
   const tableNameWithAlias = `${config?.tableName || Cls.name} as ${alias}`;
 
   const newObj: any = {
     toString() {
       return tableNameWithAlias;
-    }
+    },
   };
 
   for (const key in new Cls()) {
@@ -18,3 +18,5 @@ export function getTableMetadata<T extends Class>(Cls: T, alias: string): [Recor
 
   return [newObj, tableNameWithAlias, alias];
 }
+
+export type TableMetadata<T extends Class> = [Record<keyof InstanceType<T>, string>, string, string];
