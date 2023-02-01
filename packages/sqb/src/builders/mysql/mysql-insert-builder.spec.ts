4e12d1183a66c134ed1ef7492203742e2354e10a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-02-01 11:41:53 +0200

    feat(sqb): init MysqlInsertBuilder.

---
+++ packages/sqb/src/builders/mysql/mysql-insert-builder.spec.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+
+import { table } from '../../decorators/table';
+import { getTableMetadata } from '../../utils';
+import { MysqlInsertBuilder } from './mysql-insert-builder';
+
+describe('MysqlInsertBuilder', () => {
+  @table({ tableName: 'users' })
+  class Users {
+    userId: number;
+    firstName: string;
+    lastName: string;
+  }
+
+  @table({ tableName: 'posts' })
+  class Posts {
+    postId: number;
+    authodId: number;
+    postBody: string;
+  }
+
+  const [u, users_as_u, uAlias] = getTableMetadata(Users, 'u');
+  const [p, posts_as_p, pAlias] = getTableMetadata(Posts, 'p');
+
+  it('case1', () => {
+    const sql = new MysqlInsertBuilder().insertInto(users_as_u, [u.firstName, u.lastName], (builder) => {
+      return builder
+        .select(u.firstName, u.lastName)
+        .from(users_as_u)
+        .where((eb) => eb.isTrue(u.userId, '=', 1));
+    });
+
+    console.log(sql.toString());
+  });
+});
---
+++ packages/sqb/src/builders/mysql/mysql-insert-builder.ts
@@ -0,0 +1,61 @@
+import { MySqlSelectBuilder } from './mysql-select-builder';
+
+class InsertQuery {
+  insertIntoTable: string = '';
+  insertIntoFields: string[] = [];
+  ignore: boolean = false;
+  selectQuery: string = '';
+}
+
+export class MysqlInsertBuilder {
+  #query = new InsertQuery();
+
+  protected mergeQuery(query: Partial<InsertQuery>) {
+    this.#query.insertIntoTable = query.insertIntoTable || '';
+    this.#query.insertIntoFields.push(...(query.insertIntoFields || []));
+    this.#query.ignore = query.ignore || false;
+    this.#query.selectQuery = query.selectQuery || '';
+    return this.#query;
+  }
+
+  insertInto(
+    table: string,
+    fields: string[],
+    selectCallback: (selectBuilder: MySqlSelectBuilder) => MySqlSelectBuilder
+  ) {
+    const insertBuilder = new MysqlInsertBuilder();
+    const insertQuery = insertBuilder.mergeQuery(this.#query);
+    insertQuery.insertIntoTable = table;
+    insertQuery.insertIntoFields.push(...fields);
+    insertQuery.selectQuery = selectCallback(new MySqlSelectBuilder()).toString();
+    return insertBuilder;
+  }
+
+  ignore() {
+    const insertBuilder = new MysqlInsertBuilder();
+    insertBuilder.mergeQuery(this.#query).ignore = true;
+    return insertBuilder;
+  }
+
+  onDuplicateKeyUpdate(table: string) {}
+
+  toString(): string {
+    const { insertIntoTable, insertIntoFields, ignore, selectQuery } = this.#query;
+    let sql = '';
+
+    if (insertIntoTable) {
+      sql += 'insert';
+      if (ignore) {
+        sql += ' ignore';
+      }
+      sql += ` into ${insertIntoTable}`;
+    }
+    if (insertIntoFields.length) {
+      sql += `\n  ${insertIntoFields.join(',\n  ')}`;
+    }
+    if (selectQuery.length) {
+      sql += `\n${selectQuery}`;
+    }
+    return sql;
+  }
+}
