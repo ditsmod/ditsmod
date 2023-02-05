3f0b5948c8e22d82303014476f32d2c91d1f6fc6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-02-06 00:03:38 +0200

    feat(sqb): added getTableMetadata() to export.

--- packages/sqb/src/index.ts
+++ packages/sqb/src/index.ts
@@ -2,6 +2,7 @@ export { SqbExtension } from './sqb.extension';
 export { SqbModule } from './sqb.module';
 export { TableConfig } from './types';
 export { table } from './decorators/table';
+export { getTableMetadata } from './utils';
 export { MySqlSelectBuilder } from './builders/mysql/mysql-select-builder';
 export { MysqlInsertBuilder } from './builders/mysql/mysql-insert-builder';
 export { MySqlUpdateBuilder } from './builders/mysql/mysql-update-builder';
