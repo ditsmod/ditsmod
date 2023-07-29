3b05638e8dec67d98f47da7a8e089d61283774db
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-07-29 12:34:01 +0300

    test(examples): added tests for example 2

---
+++ examples/02-controller-error-handler/src/app/modules/some/some.controller.spec.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import { Injector, Res } from '@ditsmod/core';
+
+import { SomeController } from './some.controller';
+
+describe('SomeController', () => {
+  const send = jest.fn();
+  const res = { send } as unknown as Res;
+  let someController: SomeController;
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    const injector = Injector.resolveAndCreate([SomeController]);
+    someController = injector.get(SomeController);
+  });
+
+  it('should say "ok"', () => {
+    expect(() => someController.ok(res)).not.toThrow();
+    expect(send).toBeCalledWith('ok');
+    expect(send).toBeCalledTimes(1);
+  });
+
+  it('should to throw an error', () => {
+    expect(() => someController.throwError()).toThrow('Here some error occurred');
+    expect(send).toBeCalledTimes(0);
+  });
+});
