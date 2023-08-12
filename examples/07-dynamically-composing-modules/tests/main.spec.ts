75b0ff97a06b9137f76daf70b324772fcae268d6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-12 19:21:18 +0300

    test(examples): suppress an error log

--- examples/07-dynamically-composing-modules/tests/main.spec.ts
+++ examples/07-dynamically-composing-modules/tests/main.spec.ts
@@ -1,6 +1,6 @@
 import request = require('supertest');
 import { TestApplication } from '@ditsmod/testing';
-import { Server } from '@ditsmod/core';
+import { Providers, Server } from '@ditsmod/core';
 
 import { AppModule } from '../src/app/app.module';
 
@@ -8,7 +8,10 @@ describe('07-dynamically-composing-modules', () => {
   let server: Server;
 
   beforeAll(async () => {
-    server = await new TestApplication(AppModule).getServer();
+    server = await new TestApplication(AppModule)
+      .setLogLevelForInit('fatal')
+      .setProvidersPerApp([...new Providers().useLogConfig({ level: 'fatal' })])
+      .getServer();
   });
 
   afterAll(() => {
@@ -16,52 +19,24 @@ describe('07-dynamically-composing-modules', () => {
   });
 
   it('should works', async () => {
-    await request(server)
-      .get('/')
-      .expect(200)
-      .expect('first module.\n');
+    await request(server).get('/').expect(200).expect('first module.\n');
 
-    await request(server)
-      .get('/get-2')
-      .expect(404);
+    await request(server).get('/get-2').expect(404);
 
-    await request(server)
-      .get('/add-2')
-      .expect(200)
-      .expect('second successfully importing!\n');
+    await request(server).get('/add-2').expect(200).expect('second successfully importing!\n');
 
-    await request(server)
-      .get('/get-2')
-      .expect(200)
-      .expect('second module.\n');
+    await request(server).get('/get-2').expect(200).expect('second module.\n');
 
-    await request(server)
-      .get('/add-3')
-      .expect(500)
-      .expect({ error: 'Internal server error'});
+    await request(server).get('/add-3').expect(500).expect({ error: 'Internal server error' });
 
-    await request(server)
-      .get('/')
-      .expect(200)
-      .expect('first module.\n');
+    await request(server).get('/').expect(200).expect('first module.\n');
 
-    await request(server)
-      .get('/get-2')
-      .expect(200)
-      .expect('second module.\n');
+    await request(server).get('/get-2').expect(200).expect('second module.\n');
 
-    await request(server)
-      .get('/del-2')
-      .expect(200)
-      .expect('second successfully removing!\n');
+    await request(server).get('/del-2').expect(200).expect('second successfully removing!\n');
 
-    await request(server)
-      .get('/get-2')
-      .expect(404);
+    await request(server).get('/get-2').expect(404);
 
-    await request(server)
-      .get('/')
-      .expect(200)
-      .expect('first module.\n');
+    await request(server).get('/').expect(200).expect('first module.\n');
   });
 });
