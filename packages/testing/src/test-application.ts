1bcd1ff7919314556e7953206fbb60af4ba0690c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-13 08:30:00 +0300

    refactor(testing): TestApplication and PreTestApplication
    Simplifying PreTestApplication.

--- packages/testing/src/test-application.ts
+++ packages/testing/src/test-application.ts
@@ -1,38 +1,48 @@
 import { LogLevel, ModuleType, NormalizedProvider } from '@ditsmod/core';
 
 import { PreTestApplication } from './pre-test-application';
+import { TestModuleManager } from './test-module-manager';
 
 // This class is only needed as a wrapper over the PreTestApplication
 // class to hide the bootstrap() method from the public API.
 export class TestApplication {
   protected preTestApplication: PreTestApplication;
+  protected testModuleManager: TestModuleManager;
+  protected logLevel: LogLevel;
 
   constructor(appModule: ModuleType) {
     this.preTestApplication = new PreTestApplication();
-    this.preTestApplication.initRootModule(appModule);
+    this.initRootModule(appModule);
+  }
+
+  protected initRootModule(appModule: ModuleType) {
+    const systemLogMediator = this.preTestApplication.initRootModule(appModule);
+    this.testModuleManager = new TestModuleManager(systemLogMediator);
+    this.testModuleManager.scanRootModule(appModule);
+    return this.testModuleManager;
   }
 
   /**
    * Overrides providers at any level if there are matching providers at those levels
    * (they have the same tokens). Therefore, unlike the `setProvidersPerApp()` method,
    * this method does not always add providers to the DI.
-   * 
+   *
    * In most cases, this is the method you need.
    */
   overrideProviders(providers: NormalizedProvider[]) {
-    this.preTestApplication.overrideProviders(providers);
+    this.testModuleManager.overrideProviders(providers);
     return this;
   }
 
   /**
    * Adds providers at the application level. This method is intended, for example,
    * to set the level of logs during testing, etc.
-   * 
+   *
    * If you need to _override_ a specific provider at the application level,
    * you should use the `overrideProviders()` method instead.
    */
   setProvidersPerApp(providers: NormalizedProvider[]) {
-    this.preTestApplication.setProvidersPerApp(providers);
+    this.testModuleManager.setProvidersPerApp(providers);
     return this;
   }
 
@@ -41,11 +51,16 @@ export class TestApplication {
    * before HTTP request handlers are created.
    */
   setLogLevelForInit(logLevel: LogLevel) {
-    this.preTestApplication.setLogLevelForInit(logLevel);
+    this.logLevel = logLevel;
     return this;
   }
 
-  getServer(listen: boolean = false) {
-    return this.preTestApplication.getServer(listen);
+  async getServer(listen: boolean = false) {
+    const { server } = await this.preTestApplication.bootstrapTestApplication(
+      this.testModuleManager,
+      this.logLevel,
+      listen,
+    );
+    return server;
   }
 }
