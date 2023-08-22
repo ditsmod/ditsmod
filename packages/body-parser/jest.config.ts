e0d8779251aecfa99a082db99b0eb2b8c091a086
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-22 19:22:30 +0300

    test(body-parser): fixed config

--- packages/body-parser/jest.config.ts
+++ packages/body-parser/jest.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/src/'],
   moduleNameMapper: {
-    '@ditsmod/core': '<rootDir>/../core/dist',
-    '@ditsmod/router': '<rootDir>/../router/dist',
+    '@ditsmod/core': '<rootDir>/../core/dist/src',
+    '@ditsmod/router': '<rootDir>/../router/dist/src',
   },
 };
