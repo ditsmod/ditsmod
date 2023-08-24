b4822c88f6c3489a629d8fcf1f3b73dd127b2f3f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-24 04:37:27 +0300

    fix(examples): fixed example 16

--- examples/16-openapi-validation/src/app/modules/service/openapi/openapi.module.ts
+++ examples/16-openapi-validation/src/app/modules/service/openapi/openapi.module.ts
@@ -1,16 +1,10 @@
 import { ExtensionsMetaPerApp } from '@ditsmod/core';
-import { OpenapiModule, SwaggerOAuthOptions } from '@ditsmod/openapi';
+import { OpenapiModule } from '@ditsmod/openapi';
 
 import { oasObject } from './oas-object';
 import { oasOptions } from './oas-options';
 
-const swaggerOAuthOptions: SwaggerOAuthOptions = {
-  appName: 'Swagger UI Webpack Demo',
-  // See https://demo.duendesoftware.com/ for configuration details.
-  clientId: 'implicit',
-};
-
-export const openapiModuleWithParams = OpenapiModule.withParams(oasObject, '', swaggerOAuthOptions);
+export const openapiModuleWithParams = OpenapiModule.withParams(oasObject, '');
 
 openapiModuleWithParams.providersPerApp = [
   ...(openapiModuleWithParams.providersPerApp || []),
