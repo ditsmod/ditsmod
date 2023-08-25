c8b36b626e120fb3e7ae0eaed4bd7ab1d973be3e
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-25 14:54:09 +0300

    test(examples): fixed jest ignore pattern

--- examples/06-body-parser/jest.config.ts
+++ examples/06-body-parser/jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/test/.+ts', '<rootDir>/dist/tmp'],
+  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/test/', '<rootDir>/dist/tmp'],
   moduleNameMapper: {
     '@ditsmod/core': '<rootDir>/../../packages/core/dist/src',
     '@ditsmod/router': '<rootDir>/../../packages/router/dist/src',
