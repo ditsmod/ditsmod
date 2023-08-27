c8a61d5adb3813b1a0b13d4667a2c517c0ada80d
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-28 02:44:51 +0300

    chore(root): refactored package.json's scripts

--- jest.config1.ts
+++ jest.config1.ts
@@ -1,8 +1,10 @@
-import type { Config } from 'jest';
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 // This config is used `ts-jest` to work with TypeScript tests.
-const config: Config = {
-  preset: 'ts-jest',
+// See https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
   modulePathIgnorePatterns: [],
   moduleNameMapper: {
@@ -11,7 +13,15 @@ const config: Config = {
     '@ditsmod/openapi': '<rootDir>/packages/openapi/src',
     '@ditsmod/router': '<rootDir>/packages/router/src',
     '@ditsmod/session-cookie': '<rootDir>/packages/session-cookie/src',
-  }
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
 
 export default config;
