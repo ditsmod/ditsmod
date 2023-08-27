1527ec4a1b46b0be200c8f59ca7bb29605cd468c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-28 02:43:59 +0300

    refactor(examples): integration with new node loader

--- examples/15-i18n/jest.config.ts
+++ examples/15-i18n/jest.config.ts
@@ -2,16 +2,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/test/', '<rootDir>/dist/tmp'],
+  modulePathIgnorePatterns: ['<rootDir>/src/', '<rootDir>/test/', '<rootDir>/dist/tmp', '<rootDir>/.+.ts'],
   moduleNameMapper: {
     '@src/(.+)': '<rootDir>/dist/$1',
     '@ditsmod/core': '<rootDir>/../../packages/core/dist',
     '@ditsmod/testing': '<rootDir>/../../packages/testing/dist',
     '@ditsmod/i18n': '<rootDir>/../../packages/i18n/dist',
     '@ditsmod/router': '<rootDir>/../../packages/router/dist',
-    '@dict/first/first.dict': '<rootDir>/dist/app/first/locales/current/_base-en/first.dict',
-    '@dict/second/second.dict': '<rootDir>/dist/app/second/locales/current/_base-en/second.dict',
-    '@dict/second/errors.dict': '<rootDir>/dist/app/second/locales/current/_base-en/errors.dict',
+    '@dict/first/(.+)': '<rootDir>/dist/app/first/locales/current/_base-en/$1',
+    '@dict/second/(.+)': '<rootDir>/dist/app/second/locales/current/_base-en/$1',
   },
 };
 
