1064e0a032e0502cb1c97b27ebd903d6c2feaf2b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-04 23:09:16 +0300

    format(examples): remove empty line in main.ts

--- examples/04-logger/src/main.ts
+++ examples/04-logger/src/main.ts
@@ -1,5 +1,4 @@
 import { Application } from '@ditsmod/core';
-
 import { AppModule } from './app/app.module.js';
 
 const app = await new Application().bootstrap(AppModule);
--- examples/05-nested-routes/src/main.ts
+++ examples/05-nested-routes/src/main.ts
@@ -1,5 +1,4 @@
 import { Application } from '@ditsmod/core';
-
 import { AppModule } from './app/app.module.js';
 
 new Application().bootstrap(AppModule, { path: 'api' }).then((app) => {
