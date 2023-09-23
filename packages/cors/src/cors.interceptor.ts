65d95347c20b82c8fa4dc1e2995fd64982e027eb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-23 16:12:16 +0300

    refacto(cors): migration to core-2.50.0

--- packages/cors/src/cors.interceptor.ts
+++ packages/cors/src/cors.interceptor.ts
@@ -1,17 +1,13 @@
-import { HttpHandler, HttpInterceptor, inject, NodeRequest, NodeResponse, NODE_REQ, NODE_RES } from '@ditsmod/core';
+import { HttpHandler, HttpInterceptor, InterceptorContext } from '@ditsmod/core';
 import { cors, CorsOptions } from '@ts-stack/cors';
 import { injectable } from '@ditsmod/core';
 
 @injectable()
 export class CorsInterceptor implements HttpInterceptor {
-  constructor(
-    private corsOptions: CorsOptions,
-    @inject(NODE_REQ) private nodeReq: NodeRequest,
-    @inject(NODE_RES) private nodeRes: NodeResponse,
-  ) {}
+  constructor(private corsOptions: CorsOptions) {}
 
-  async intercept(next: HttpHandler) {
-    const headersSent = cors(this.nodeReq, this.nodeRes, this.corsOptions);
+  async intercept(next: HttpHandler, ctx: InterceptorContext) {
+    const headersSent = cors(ctx.nodeReq, ctx.nodeRes, this.corsOptions);
     if (headersSent) {
       return;
     }
