41290f6346ed65e1c79b86f4422aa8d9856216ac
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-23 16:12:50 +0300

    refacto(body-parser): migration to core-2.50.0

--- packages/body-parser/src/body-parser.interceptor.ts
+++ packages/body-parser/src/body-parser.interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpHandler, HttpInterceptor, inject, NODE_REQ, NodeRequest, Injector } from '@ditsmod/core';
+import { HttpHandler, HttpInterceptor, Injector, InterceptorContext } from '@ditsmod/core';
 import { injectable, optional } from '@ditsmod/core';
 import { parse, Headers, Options } from 'get-body';
 
@@ -8,20 +8,19 @@ import { HTTP_BODY, BodyParserConfig } from './body-parser-config.js';
 export class BodyParserInterceptor implements HttpInterceptor {
   constructor(
     private injector: Injector,
-    @inject(NODE_REQ) private nodeReq: NodeRequest,
     @optional() private config?: BodyParserConfig,
   ) {
     this.config = Object.assign({}, new BodyParserConfig(), config); // Merge with default.
   }
 
-  async intercept(next: HttpHandler) {
-    const contentType = this.nodeReq.headers['content-type'];
+  async intercept(next: HttpHandler, ctx: InterceptorContext) {
+    const contentType = ctx.nodeReq.headers['content-type'];
     const hasAcceptableHeaders = this.config?.acceptHeaders?.some((type) => contentType?.includes(type));
     if (!hasAcceptableHeaders) {
       return next.handle();
     }
     const options: Options = { limit: this.config?.maxBodySize };
-    const body = await parse(this.nodeReq, this.nodeReq.headers as Headers, options);
+    const body = await parse(ctx.nodeReq, ctx.nodeReq.headers as Headers, options);
     this.injector.setByToken(HTTP_BODY, body);
 
     return next.handle();
