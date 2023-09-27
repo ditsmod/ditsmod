ad3ef7f3c7072b322ebce130647689cb106eb855
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-27 09:26:22 +0300

    feat(core): added InterceptorContext for HttpErrorHandler.

--- packages/core/src/services/http-error-handler.ts
+++ packages/core/src/services/http-error-handler.ts
@@ -1,5 +1,7 @@
+import { InterceptorContext } from '#types/http-interceptor.js';
+
 export class HttpErrorHandler {
-  handleError(error: any): void | Promise<void> {
+  handleError(error: any, ctx: InterceptorContext): void | Promise<void> {
     throw error;
   }
 }
