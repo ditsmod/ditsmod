186b996fc5c094c28678238b361d51a8f220919b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-27 09:26:49 +0300

    feat(example): added InterceptorContext for HttpErrorHandler.

--- examples/02-http-error-handler/src/app/my-http-error-handler.ts
+++ examples/02-http-error-handler/src/app/my-http-error-handler.ts
@@ -4,34 +4,31 @@ import {
   HttpErrorHandler,
   injectable,
   Res,
-  NodeResponse,
-  inject,
-  NODE_RES,
   cleanErrorTrace,
   Req,
+  InterceptorContext,
 } from '@ditsmod/core';
 
 @injectable()
 export class MyHttpErrorHandler implements HttpErrorHandler {
   constructor(
-    @inject(NODE_RES) private nodeRes: NodeResponse,
     protected req: Req,
     private res: Res,
     private logger: Logger,
   ) {}
 
-  handleError(err: Error) {
+  handleError(err: Error, ctx: InterceptorContext) {
     cleanErrorTrace(err);
     const message = err.message;
     this.logger.log('error', { note: 'This is my implementation of HttpErrorHandler', err });
-    if (!this.nodeRes.headersSent) {
-      this.addRequestIdToHeader();
+    if (!ctx.nodeRes.headersSent) {
+      this.addRequestIdToHeader(ctx);
       this.res.sendJson({ error: { message } }, Status.INTERNAL_SERVER_ERROR);
     }
   }
 
-  protected addRequestIdToHeader() {
+  protected addRequestIdToHeader(ctx: InterceptorContext) {
     const header = 'x-requestId';
-    this.nodeRes.setHeader(header, this.req.requestId);
+    ctx.nodeRes.setHeader(header, this.req.requestId);
   }
 }
