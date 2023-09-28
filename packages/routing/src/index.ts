26f802c86d85c2381d301499fd922ac1d2023132
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-28 16:22:11 +0300

    refactor(routing): renamed RoutingModule's file.

--- packages/routing/src/index.ts
+++ packages/routing/src/index.ts
@@ -1,7 +1,7 @@
 export { DefaultRouter } from './router.js';
 export { Tree } from './tree.js';
 export { RouteParam, ROUTES_EXTENSIONS, PRE_ROUTER_EXTENSIONS } from './types.js';
-export { RoutingModule } from './router.module.js';
+export { RoutingModule } from './routing.module.js';
 export { RouterLogMediator } from './router-log-mediator.js';
 export { RoutesExtension } from './routes.extension.js';
 export { PreRouterExtension } from './pre-router.extension.js';
