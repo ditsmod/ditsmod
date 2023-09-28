8b808394cc991ef876c9b90d9d2b662d361dde76
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-09-28 13:33:51 +0300

    refactor(core): refactoring cleanErrorTrace().

--- packages/core/src/utils/clean-error-trace.ts
+++ packages/core/src/utils/clean-error-trace.ts
@@ -5,12 +5,16 @@ import { StackUtils } from '@ts-stack/stack-utils';
  * you can use the [@ts-stack/stack-utils](https://github.com/ts-stack/stack-utils) library directly.
  */
 export function cleanErrorTrace(error: any, regExp: RegExp[] = []) {
+  const descriptor = Object.getOwnPropertyDescriptor(error, 'stack');
+  if (descriptor?.writable === false || (descriptor?.get && !descriptor?.set)) {
+    return error;
+  }
   const internals: any[] = [
     ...StackUtils.nodeInternals(),
     /@ditsmod/,
     /ditsmod\/packages/,
     /^\s+at Array.forEach \(<anonymous>\)$/,
-    ...regExp
+    ...regExp,
   ];
   const stack = new StackUtils({ internals });
   error.stack = stack.clean(error.stack || '');
