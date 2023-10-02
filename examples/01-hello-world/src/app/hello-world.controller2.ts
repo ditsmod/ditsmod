a346214348e1429eb06bde160d01f958c13c6a62
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-10-02 14:02:25 +0300

    refactor(examples): a bit fix example 1.

--- examples/01-hello-world/src/app/hello-world.controller2.ts
+++ examples/01-hello-world/src/app/hello-world.controller2.ts
@@ -4,6 +4,7 @@ import { controller, route, SingletonRequestContext } from '@ditsmod/core';
 export class HelloWorldController2 {
   @route('GET', 'hello2')
   tellHello(ctx: SingletonRequestContext) {
+    ctx.nodeRes.setHeader('Content-Type', 'text/plain; charset=utf-8');
     ctx.nodeRes.end('Hello, World!');
   }
 }
