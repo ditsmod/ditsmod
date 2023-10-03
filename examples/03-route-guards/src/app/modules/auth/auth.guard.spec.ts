16f36b0e02668785fb6a7839e1fa34b06b2812c1
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-10-03 21:19:19 +0300

    test(examples): refactoring example 3

--- examples/03-route-guards/src/app/modules/auth/auth.guard.spec.ts
+++ examples/03-route-guards/src/app/modules/auth/auth.guard.spec.ts
@@ -1,32 +1,21 @@
-import { Injector, NODE_REQ, RequestContext } from '@ditsmod/core';
+import { RequestContext } from '@ditsmod/core';
 
 import { AuthGuard } from './auth.guard.js';
 
-/**
- * @todo Refactor this.
- */
 describe('AuthGuard#canActivate()', () => {
+  const authGuard = new AuthGuard();
   function getCtx(authorization: string) {
     return { nodeReq: { headers: { authorization } } } as RequestContext;
   }
 
-  function getGuard(authorization: string) {
-    const nodeRequest = getCtx(authorization).nodeReq;
-    const injector = Injector.resolveAndCreate([AuthGuard, { token: NODE_REQ, useValue: nodeRequest }]);
-    return injector.get(AuthGuard) as AuthGuard;
-  }
-
   it('should return false', async () => {
     const ctx = getCtx('');
-    const authGuard = getGuard('');
     await expect(authGuard.canActivate(ctx)).resolves.not.toThrow();
     await expect(authGuard.canActivate(ctx)).resolves.toBe(false);
   });
 
   it('should return true', async () => {
-    const auth = 'Token fake-toke-here';
-    const ctx = getCtx(auth);
-    const authGuard = getGuard(auth);
+    const ctx = getCtx('Token fake-toke-here');
     await expect(authGuard.canActivate(ctx)).resolves.not.toThrow();
     await expect(authGuard.canActivate(ctx)).resolves.toBe(true);
   });
