a6ab696d4e8149f578184abe49665280961e6780
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-01-18 10:12:38 +0200

    refactor(examples): a bit refactoring 05 example.

--- examples/05-nested-routes/src/main.ts
+++ examples/05-nested-routes/src/main.ts
@@ -1,6 +1,5 @@
 import { Application } from '@ditsmod/core';
 import { AppModule } from './app/app.module.js';
 
-new Application().bootstrap(AppModule, { path: 'api' }).then((app) => {
-  app.server.listen(3000, '0.0.0.0');
-});
+const app = await new Application().bootstrap(AppModule, { path: 'api' });
+app.server.listen(3000, '0.0.0.0');
