e54d8b32cbe9f9335580854e407aaa82e3fb5f14
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-03-02 14:20:44 +0200

    test(core): added tests for ConsoleLogger.

---
+++ packages/core/src/logger/console-logger.spec.ts
@@ -0,0 +1,42 @@
+import { jest } from '@jest/globals';
+
+import { ConsoleLogger } from '#logger/console-logger.js';
+import { InputLogLevel } from '#logger/logger.js';
+
+describe('ConsoleLogger', () => {
+  const allLevels: InputLogLevel[] = ['all', 'trace', 'debug', 'info', 'warn', 'error', 'fatal'];
+  const mockConsoleLog = jest.spyOn(console, 'log');
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('"off" log level', () => {
+    const logger = new ConsoleLogger();
+    expect(() => logger.setLevel('off')).not.toThrow();
+    expect(logger.getLevel() == 'off').toBe(true);
+    allLevels.forEach((level) => logger.log(level, 'test'));
+    expect(mockConsoleLog).toHaveBeenCalledTimes(0);
+  });
+
+  it('"info" (default) log level', () => {
+    const logger = new ConsoleLogger();
+    expect(logger.getLevel() == 'info').toBe(true);
+    allLevels.forEach((level) => logger.log(level, 'test'));
+    expect(mockConsoleLog).toHaveBeenCalledTimes(4);
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(1, '[ConsoleLogger:info]', 'test');
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(2, '[ConsoleLogger:warn]', 'test');
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(3, '[ConsoleLogger:error]', 'test');
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(4, '[ConsoleLogger:fatal]', 'test');
+  });
+
+  it('"error" log level', () => {
+    const logger = new ConsoleLogger();
+    expect(() => logger.setLevel('error')).not.toThrow();
+    expect(logger.getLevel() == 'error').toBe(true);
+    allLevels.forEach((level) => logger.log(level, 'test'));
+    expect(mockConsoleLog).toHaveBeenCalledTimes(2);
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(1, '[ConsoleLogger:error]', 'test');
+    expect(mockConsoleLog).toHaveBeenNthCalledWith(2, '[ConsoleLogger:fatal]', 'test');
+  });
+});
