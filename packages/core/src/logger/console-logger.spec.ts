69e0f94a1d53ba45daf8684e3affb96b4e5d2d48
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-03-03 13:18:42 +0200

    test(core): refactoring ConsoleLogger.

--- packages/core/src/logger/console-logger.spec.ts
+++ packages/core/src/logger/console-logger.spec.ts
@@ -5,7 +5,7 @@ import { InputLogLevel } from '#logger/logger.js';
 
 describe('ConsoleLogger', () => {
   const inputLogLevels: InputLogLevel[] = ['all', 'trace', 'debug', 'info', 'warn', 'error', 'fatal'];
-  const mockConsoleLog = jest.spyOn(console, 'log');
+  jest.spyOn(console, 'log');
 
   beforeEach(() => {
     jest.resetAllMocks();
@@ -16,18 +16,18 @@ describe('ConsoleLogger', () => {
     expect(() => logger.setLevel('off')).not.toThrow();
     expect(logger.getLevel() == 'off').toBe(true);
     inputLogLevels.forEach((level) => logger.log(level, 'test'));
-    expect(mockConsoleLog).toHaveBeenCalledTimes(0);
+    expect(console.log).toHaveBeenCalledTimes(0);
   });
 
   it('"info" (default) log level', () => {
     const logger = new ConsoleLogger();
     expect(logger.getLevel() == 'info').toBe(true);
     inputLogLevels.forEach((level) => logger.log(level, 'test'));
-    expect(mockConsoleLog).toHaveBeenCalledTimes(4);
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(1, '[ConsoleLogger:info]', 'test');
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(2, '[ConsoleLogger:warn]', 'test');
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(3, '[ConsoleLogger:error]', 'test');
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(4, '[ConsoleLogger:fatal]', 'test');
+    expect(console.log).toHaveBeenCalledTimes(4);
+    expect(console.log).toHaveBeenNthCalledWith(1, '[ConsoleLogger:info]', 'test');
+    expect(console.log).toHaveBeenNthCalledWith(2, '[ConsoleLogger:warn]', 'test');
+    expect(console.log).toHaveBeenNthCalledWith(3, '[ConsoleLogger:error]', 'test');
+    expect(console.log).toHaveBeenNthCalledWith(4, '[ConsoleLogger:fatal]', 'test');
   });
 
   it('"error" log level', () => {
@@ -35,8 +35,8 @@ describe('ConsoleLogger', () => {
     expect(() => logger.setLevel('error')).not.toThrow();
     expect(logger.getLevel() == 'error').toBe(true);
     inputLogLevels.forEach((level) => logger.log(level, 'test'));
-    expect(mockConsoleLog).toHaveBeenCalledTimes(2);
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(1, '[ConsoleLogger:error]', 'test');
-    expect(mockConsoleLog).toHaveBeenNthCalledWith(2, '[ConsoleLogger:fatal]', 'test');
+    expect(console.log).toHaveBeenCalledTimes(2);
+    expect(console.log).toHaveBeenNthCalledWith(1, '[ConsoleLogger:error]', 'test');
+    expect(console.log).toHaveBeenNthCalledWith(2, '[ConsoleLogger:fatal]', 'test');
   });
 });
