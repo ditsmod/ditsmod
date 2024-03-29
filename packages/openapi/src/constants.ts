b5373f5a3b80f9bd3e8a5fbc445071247fa41a2f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-03-29 09:09:55 +0200

    chore(openapi): changed host name in DEFAULT_OAS_OBJECT.

--- packages/openapi/src/constants.ts
+++ packages/openapi/src/constants.ts
@@ -8,7 +8,7 @@ import { openapi, XOasObject } from '@ts-stack/openapi-spec';
 
 export const DEFAULT_OAS_OBJECT: XOasObject = {
   openapi,
-  servers: [{ url: 'http://localhost:3000' }],
+  servers: [{ url: 'http://0.0.0.0:3000' }],
   info: { title: 'Testing @ditsmod/openapi with default value', version: '0.0.0' },
   tags: [],
   components: {}
