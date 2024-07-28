f6787531cf46a695bb6b5d2ac7b144a68740126f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-07-28 22:32:22 +0300

    feat(examples): introduced Multer for example 06.

--- examples/06-body-parser/src/app/modules/routed/some/some.controller.ts
+++ examples/06-body-parser/src/app/modules/routed/some/some.controller.ts
@@ -1,5 +1,6 @@
+import { createWriteStream, rename } from 'node:fs';
 import { controller, inject, Res, route } from '@ditsmod/core';
-import { HTTP_BODY } from '@ditsmod/body-parser';
+import { MulterParsedForm, HTTP_BODY, MulterHelper } from '@ditsmod/body-parser';
 
 interface Body {
   one: number;
@@ -15,5 +16,54 @@ export class SomeController {
   @route('POST')
   post(res: Res, @inject(HTTP_BODY) body: Body) {
     res.sendJson(body);
+  }
+
+  @route('GET', 'file-upload')
+  getHtmlForm(res: Res) {
+    res.setContentType('text/html').send(`
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uploads files</title>
+</head>
+<body>
+    <form method="post" enctype="multipart/form-data">
+        <label for="files">Select files:</label>
+        <input type="file" id="files" name="files" multiple>
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
+      `);
+  }
+
+  @route('POST', 'file-upload')
+  async downloadFile(res: Res, parse: MulterHelper) {
+    try {
+      const parsedForm = await parse.array('files');
+      await this.saveFiles(parsedForm);
+      res.sendJson(parsedForm);
+    } catch (err: any) {
+      res.sendJson(err);
+    }
+  }
+
+  protected saveFiles(parsedForm: MulterParsedForm) {
+    const promises: Promise<void>[] = [];
+    parsedForm.files.forEach((file) => {
+      const promise = new Promise<void>((resolve, reject) => {
+        const path = `uploaded-files/${file.originalName}`;
+        const writableStream = createWriteStream(path);
+        file.stream.pipe(writableStream);
+        writableStream.on('finish', resolve);
+        writableStream.on('error', reject);
+      });
+      promises.push(promise);
+    });
+
+    return Promise.all(promises);
   }
 }
