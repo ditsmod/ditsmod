ad2843635a686a87cc8c2c2d5a2aaf78e22c0fc5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-08-01 09:01:26 +0300

    refactor(body-parser): simplifying passing value for MulterExtendedOptions.

--- examples/06-body-parser/src/app/modules/routed/some/some.module.ts
+++ examples/06-body-parser/src/app/modules/routed/some/some.module.ts
@@ -1,18 +1,17 @@
 import { featureModule } from '@ditsmod/core';
 import { RoutingModule } from '@ditsmod/routing';
+import { BodyParserModule, MulterExtendedOptions } from '@ditsmod/body-parser';
 
 import { SomeController } from './some.controller.js';
 import { SingletonController } from './singleton-some.controller.js';
-import { MulterExtendedOptions, MulterOptions } from '@ditsmod/body-parser';
 
 const multerOptions: MulterExtendedOptions = { limits: { files: 20 }, errorLogLevel: 'debug' };
 
 @featureModule({
-  imports: [RoutingModule],
+  imports: [RoutingModule, BodyParserModule],
   controllers: [SomeController, SingletonController],
-  providersPerRou: [
-    { token: MulterExtendedOptions, useValue: multerOptions },
-    { token: MulterOptions, useToken: MulterExtendedOptions },
+  providersPerMod: [
+    { token: MulterExtendedOptions, useValue: multerOptions }
   ],
 })
 export class SomeModule {}
