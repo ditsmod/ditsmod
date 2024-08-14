c47c1d5b067893e88c16848bbaf792ccd7a14171
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-08-14 13:00:34 +0300

    feat(i18n): introduced `$if()` method for Providers helper.

--- packages/i18n/src/i18n-providers.ts
+++ packages/i18n/src/i18n-providers.ts
@@ -10,12 +10,16 @@ export class I18nProviders extends Providers {
    * Helper that adds providers in a type safe way.
    */
   i18n(translations: Translations, i18nOptions?: I18nOptions) {
+    if (!this.true) {
+      return this.self;
+    }
+
     this.useValue<Translations>(I18N_TRANSLATIONS, translations, true);
 
     if (i18nOptions) {
       this.useValue(I18nOptions, i18nOptions);
     }
 
-    return this;
+    return this.self;
   }
 }
