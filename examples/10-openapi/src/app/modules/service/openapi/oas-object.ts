463cde847c188bff04f9f4b76ec5a0d11b421af2
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-09-02 09:11:57 +0300

    refactor(examples): a bit refactoring default oasObject.

--- examples/10-openapi/src/app/modules/service/openapi/oas-object.ts
+++ examples/10-openapi/src/app/modules/service/openapi/oas-object.ts
@@ -1,7 +1,7 @@
-import { XOasObject } from '@ts-stack/openapi-spec';
+import { XOasObject, openapi } from '@ts-stack/openapi-spec';
 
 export const oasObject: XOasObject = {
-  openapi: '3.0.0',
+  openapi,
   // Here works the servers that are described using this OpenAPI documentation.
   servers: [{ url: 'http://localhost:3000' }],
   info: { title: 'Testing @ditsmod/openapi', version: '1.0.0' },
