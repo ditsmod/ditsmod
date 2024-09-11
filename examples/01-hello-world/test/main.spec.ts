27f0d62095404a19f74e537a51ddd938946a8b82
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-09-11 19:08:33 +0300

    test(examples): refactoring tests for example 01.

--- examples/01-hello-world/test/main.spec.ts
+++ examples/01-hello-world/test/main.spec.ts
@@ -18,16 +18,16 @@ describe('01-hello-world', () => {
   });
 
   it('controller works', async () => {
-    await testAgent
-      .get('/hello')
-      .expect(200)
-      .expect('Hello, World!');
+    const { status, text, type } = await testAgent.get('/hello');
+    expect(type).toBe('text/plain');
+    expect(status).toBe(200);
+    expect(text).toBe('Hello, World!');
   });
 
   it('controller as singleton works', async () => {
-    await testAgent
-      .get('/hello2')
-      .expect(200)
-      .expect('Hello, World!');
+    const { status, text, type } = await testAgent.get('/hello2');
+    expect(type).toBe('text/plain');
+    expect(status).toBe(200);
+    expect(text).toBe('Hello, World!');
   });
 });
