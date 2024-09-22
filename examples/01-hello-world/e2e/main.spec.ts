17d99ec3e2f65ff6b52fe64e82294dfe438ecbcb
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-09-22 13:23:05 +0300

    test(examples): added e2e for HEAD method.

--- examples/01-hello-world/e2e/main.spec.ts
+++ examples/01-hello-world/e2e/main.spec.ts
@@ -24,6 +24,17 @@ describe('01-hello-world', () => {
     expect(text).toBe('Hello, World!');
   });
 
+  it('controller handles HEAD method', async () => {
+    const { type, status, text, headers } = await testAgent.head('/hello');
+    expect(type).toBe('text/plain');
+    expect(status).toBe(200);
+    expect(text).toBeUndefined();
+    expect(headers).toMatchObject({
+      'content-type': 'text/plain; charset=utf-8',
+      'content-length': '13',
+    });
+  });
+
   it('controller as singleton works', async () => {
     const { type, status, text } = await testAgent.get('/hello2');
     expect(type).toBe('text/plain');
