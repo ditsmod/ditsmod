ce1af86edb1ff80389049eeae73f6b7464461b9c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-09-28 13:16:44 +0300

    refactor(testing): fixed PreTestApplication.
    Passing testAppInitializer to property of PreTestApplication.

--- packages/testing/src/pre-test-application.ts
+++ packages/testing/src/pre-test-application.ts
@@ -12,6 +12,7 @@ export class PreTestApplication extends Application {
     return new Promise<{ server: Server }>(async (resolve, reject) => {
       try {
         const testAppInitializer = new TestAppInitializer(this.appOptions, testModuleManager, this.systemLogMediator);
+        this.appInitializer = testAppInitializer;
         testAppInitializer.setInitLogLevel(logLevel);
         await this.bootstrapApplication(testAppInitializer);
         await this.createServerAndBindToListening(testAppInitializer, resolve);
