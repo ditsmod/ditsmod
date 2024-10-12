4b0895876fe1f5d41ef022dde9d8bfef9a272f07
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-10-12 20:59:18 +0300

    refactor(examples): removed OverriddenBearerGuard from example 03.

--- examples/03-route-guards/src/app/modules/auth/bearer.guard.ts
+++ examples/03-route-guards/src/app/modules/auth/bearer.guard.ts
@@ -15,19 +15,3 @@ export class BearerGuard implements CanActivate {
     return Boolean(token);
   }
 }
-
-@guard()
-export class OverriddenBearerGuard implements CanActivate {
-  async canActivate(ctx: RequestContext, params?: any[]) {
-    const authValue = ctx.nodeReq.headers.authorization?.split(' ');
-    if (authValue?.[0] != 'Bearer') {
-      return false;
-    }
-
-    /**
-     * Here you need implement more logic.
-     */
-    const token = authValue[1];
-    return Boolean(token);
-  }
-}
--- examples/03-route-guards/src/app/modules/module3/module3.ts
+++ examples/03-route-guards/src/app/modules/module3/module3.ts
@@ -4,14 +4,10 @@ import { RoutingModule } from '@ditsmod/routing';
 import { AuthModule, BearerGuard } from '#auth';
 import { Controller1 } from './controller1.js';
 import { Module2 } from '../module2/module2.js';
-import { OverriddenBearerGuard } from '../auth/bearer.guard.js';
 
 @featureModule({
   imports: [RoutingModule, AuthModule],
   appends: [{ path: '', module: Module2, guards: [BearerGuard] }],
-  providersPerReq: [
-    { token: BearerGuard, useClass: OverriddenBearerGuard },
-  ],
   controllers: [Controller1],
 })
 export class Module3 {}
