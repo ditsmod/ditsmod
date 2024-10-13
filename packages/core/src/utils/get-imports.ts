cecdec65c2b1ba51b2c79b67a261ac44714f3281
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-10-13 10:53:06 +0300

    fix(core): fixed `getImported*` helpers.

--- packages/core/src/utils/get-imports.ts
+++ packages/core/src/utils/get-imports.ts
@@ -2,17 +2,17 @@ import { ImportObj } from '#types/metadata-per-mod.js';
 import { Provider } from '#types/mix.js';
 
 export function getImportedTokens(map: Map<any, ImportObj<Provider>> | undefined) {
-  return [...(map || [])].map(([key]) => key);
+  return [...(map || []).keys()];
 }
 
 export function getImportedProviders(map: Map<any, ImportObj<Provider>> | undefined) {
   const providers: Provider[] = [];
-  [...(map || [])].map(([,importObj]) => providers.push(...importObj.providers));
+  for (const importObj of (map || []).values()) {
+    providers.push(...importObj.providers);
+  }
   return providers;
 }
 
 export function getImportedObjects(map: Map<any, ImportObj<Provider>> | undefined) {
-  const objects: ImportObj<Provider>[] = [];
-  [...(map || [])].map(([,importObj]) => objects.push(importObj));
-  return objects;
+  return [...(map || []).values()];
 }
