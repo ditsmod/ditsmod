a42e31ed266366bdb3995b5213bb85f6e388fcfa
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-10-19 14:42:52 +0300

    fix(openapi): fixed path for UI.

--- packages/openapi/src/swagger-ui/index.ts
+++ packages/openapi/src/swagger-ui/index.ts
@@ -3,7 +3,7 @@ import 'swagger-ui/dist/swagger-ui.css';
 
 import type { SwaggerOAuthOptions } from './o-auth-options.js';
 
-const swaggerOptions = { url: 'http://0.0.0.0:3000/api/openapi.yaml', dom_id: '#swagger', queryConfigEnabled: true };
+const swaggerOptions = { url: 'openapi.yaml', dom_id: '#swagger', queryConfigEnabled: true };
 
 const oauthOptions: SwaggerOAuthOptions = {
   appName: 'Swagger UI Webpack',
