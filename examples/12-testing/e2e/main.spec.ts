7ab95adcc97a6b52004f3492824d7c87c6c7cac2
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-10-21 12:47:41 +0300

    test(examples): fixed test for example 12.

--- examples/12-testing/e2e/main.spec.ts
+++ examples/12-testing/e2e/main.spec.ts
@@ -58,7 +58,7 @@ describe('12-testing', () => {
       const { status } = await request(server).get('/fail1');
       expect(status).toBe(500);
       const errMsg = 'No provider for non-existing-token!; this error during calling Controller1.prototype.method1!';
-      const traceRegExp = /^Error: No provider for non-existing-token![^\n]+\n\s+at Controller1./;
+      const traceRegExp = /^Error: No provider for non-existing-token![^\n]+[\S\s]+at Controller1./;
       const errStack = expect.stringMatching(traceRegExp);
       expect(setError).toHaveBeenCalledWith(errMsg, errStack);
       expect(setError).toHaveBeenCalledTimes(1);
