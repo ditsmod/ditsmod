e9aa959d2831fc2d4df1a59cdbbcc10a25185b23
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-10-27 16:50:37 +0200

    test(core): added tests for `ClassMetaIterator`.

---
+++ packages/core/src/di/class-meta-iterator.spec.ts
@@ -0,0 +1,38 @@
+import { jest } from '@jest/globals';
+import { ClassMetaIterator } from './class-meta-iterator.js';
+
+describe('ClassMetaIterator', () => {
+  it('works with nested loops', () => {
+    const for1 = jest.fn();
+    const for2 = jest.fn();
+    const sym = Symbol();
+    const classMetaIterator = new ClassMetaIterator();
+    (classMetaIterator as any)['key1'] = '';
+    (classMetaIterator as any)[sym] = '';
+    (classMetaIterator as any)['key2'] = '';
+    classMetaIterator.init();
+
+    for (const v of classMetaIterator) {
+      for1(v);
+
+      for (const v of classMetaIterator) {
+        for2(v);
+      }
+    }
+    expect(for1).toHaveBeenCalledTimes(3);
+    expect(for1).toHaveBeenNthCalledWith(1, 'key1');
+    expect(for1).toHaveBeenNthCalledWith(2, 'key2');
+    expect(for1).toHaveBeenNthCalledWith(3, sym);
+
+    expect(for2).toHaveBeenCalledTimes(9);
+    expect(for2).toHaveBeenNthCalledWith(1, 'key1');
+    expect(for2).toHaveBeenNthCalledWith(2, 'key2');
+    expect(for2).toHaveBeenNthCalledWith(3, sym);
+    expect(for2).toHaveBeenNthCalledWith(4, 'key1');
+    expect(for2).toHaveBeenNthCalledWith(5, 'key2');
+    expect(for2).toHaveBeenNthCalledWith(6, sym);
+    expect(for2).toHaveBeenNthCalledWith(7, 'key1');
+    expect(for2).toHaveBeenNthCalledWith(8, 'key2');
+    expect(for2).toHaveBeenNthCalledWith(9, sym);
+  });
+});
