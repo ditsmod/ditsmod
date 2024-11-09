d1c4f354f9a0ebc8c486826d6fe5c8a63ceadf21
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-09 22:31:50 +0200

    refactor(examples):  removed obsolete code for extension init cache.

--- examples/09-one-extension/src/app/my-extension.ts
+++ examples/09-one-extension/src/app/my-extension.ts
@@ -3,21 +3,13 @@ import { ROUTES_EXTENSIONS } from '@ditsmod/routing';
 
 @injectable()
 export class MyExtension implements Extension<void> {
-  #inited: boolean;
-
   constructor(
     private extensionsManager: ExtensionsManager,
     private logger: Logger,
   ) {}
 
   async stage1() {
-    if (this.#inited) {
-      return;
-    }
-
     const groupStage1Meta = await this.extensionsManager.stage1(ROUTES_EXTENSIONS);
     this.logger.log('info', groupStage1Meta.groupData[0].aControllerMetadata);
-
-    this.#inited = true;
   }
 }
