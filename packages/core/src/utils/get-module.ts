eeabcdaa43147af4adf294e002eef331f95975c3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-11 13:40:20 +0200

    fix(core): fixed type for `getModule()`.

--- packages/core/src/utils/get-module.ts
+++ packages/core/src/utils/get-module.ts
@@ -2,6 +2,6 @@ import { ModuleType } from '#types/mix.js';
 import { ModuleWithParams } from '#types/module-metadata.js';
 import { isModuleWithParams } from './type-guards.js';
 
-export function getModule(modOrObj: ModuleType | ModuleWithParams) {
+export function getModule(modOrObj: ModuleType | ModuleWithParams): ModuleType {
   return isModuleWithParams(modOrObj) ? modOrObj.module : modOrObj;
 }
