d7533f777680ea15b86610799e0c5951f9a042ad
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-12 14:14:27 +0200

    fix(return): adde `RoutingModule` to deps.

--- packages/return/src/return.module.ts
+++ packages/return/src/return.module.ts
@@ -1,14 +1,16 @@
 import { HttpBackend, featureModule } from '@ditsmod/core';
+import { RoutingModule } from '@ditsmod/routing';
 
 import { ReturnHttpBackend } from './return-http-backend.js';
 import { SingletonReturnHttpBackend } from './singleton-return-http-backend.js';
 
 /**
- * Allow listen value returned by a controller's route.
+ * Allow listen value returned by a controller's route. Re-exports `RoutingModule`.
  */
 @featureModule({
+  imports: [RoutingModule],
   providersPerRou: [{ token: HttpBackend, useClass: SingletonReturnHttpBackend }],
   providersPerReq: [{ token: HttpBackend, useClass: ReturnHttpBackend }],
-  exports: [HttpBackend]
+  exports: [HttpBackend, RoutingModule]
 })
 export class ReturnModule {}
