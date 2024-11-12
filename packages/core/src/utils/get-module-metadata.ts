86a4bb9478f41b8258255457f5ac447078ff9d01
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-12 17:30:41 +0200

    refactor(core): refactoring `getModuleMetadata()`.

--- packages/core/src/utils/get-module-metadata.ts
+++ packages/core/src/utils/get-module-metadata.ts
@@ -1,26 +1,24 @@
 import { DecoratorAndValue, reflector, resolveForwardRef } from '#di';
-import { ModuleWithParams, ModuleMetadata } from '#types/module-metadata.js';
-import { AnyFn, GuardItem, ModuleType, Scope } from '#types/mix.js';
-import { getModuleName } from './get-module-name.js';
+import { ModuleMetadata } from '#types/module-metadata.js';
+import { AnyFn, GuardItem, ModRefId, Scope } from '#types/mix.js';
 import { mergeArrays } from './merge-arrays.js';
-import { isFeatureModule, isModuleWithParams, isRootModule } from './type-guards.js';
+import { isAppendsWithParams, isFeatureModule, isModuleWithParams, isRootModule } from './type-guards.js';
 import { getLastProviders } from './get-last-providers.js';
+import { getDebugModuleName } from './get-debug-module-name.js';
 
 /**
  * Merges metadata passed in `rootModule` or `featureModule` decorators with metadata passed
  * in `ModuleWithParams`. Additionally, converts the `Providers` instances passed in the `providersPer*`
  * property to arrays.
  */
-export function getModuleMetadata(
-  modOrObj: ModuleType | ModuleWithParams,
-  isRoot?: boolean,
-): ModuleMetadataWithContext | undefined {
+export function getModuleMetadata(modRefId: ModRefId, isRoot?: boolean): ModuleMetadataWithContext | undefined {
   const typeGuard = isRoot ? isRootModule : (m: DecoratorAndValue) => isFeatureModule(m) || isRootModule(m);
 
-  modOrObj = resolveForwardRef(modOrObj);
+  modRefId = resolveForwardRef(modRefId);
+  const scopes = ['App', 'Mod', 'Rou', 'Req'] as Scope[];
 
-  if (isModuleWithParams(modOrObj)) {
-    const modWitParams = modOrObj;
+  if (isModuleWithParams(modRefId) || isAppendsWithParams(modRefId)) {
+    const modWitParams = modRefId;
     const decoratorAndValue = reflector
       .getMetadata<ModuleMetadataValue>(modWitParams.module)
       ?.constructor.decorators.find(typeGuard);
@@ -30,7 +28,7 @@ export function getModuleMetadata(
     }
 
     if (modMetadata.id) {
-      const modName = getModuleName(modWitParams.module);
+      const modName = getDebugModuleName(modWitParams.module);
       const msg =
         `${modName} must not have an "id" in the metadata of the decorator @featureModule. ` +
         'Instead, you can specify the "id" in the object that contains the module parameters.';
@@ -38,29 +36,32 @@ export function getModuleMetadata(
     }
     const metadata = Object.assign({}, modMetadata);
     metadata.id = modWitParams.id;
-    metadata.providersPerApp = getLastProviders(mergeArrays(modMetadata.providersPerApp, modWitParams.providersPerApp));
-    metadata.providersPerMod = getLastProviders(mergeArrays(modMetadata.providersPerMod, modWitParams.providersPerMod));
-    metadata.providersPerRou = getLastProviders(mergeArrays(modMetadata.providersPerRou, modWitParams.providersPerRou));
-    metadata.providersPerReq = getLastProviders(mergeArrays(modMetadata.providersPerReq, modWitParams.providersPerReq));
-    metadata.exports = getLastProviders(mergeArrays(modMetadata.exports, modWitParams.exports));
-    metadata.extensionsMeta = { ...modMetadata.extensionsMeta, ...modWitParams.extensionsMeta };
+    if (isModuleWithParams(modWitParams)) {
+      scopes.forEach((scope) => {
+        const arr1 = modMetadata[`providersPer${scope}`];
+        const arr2 = modWitParams[`providersPer${scope}`];
+        metadata[`providersPer${scope}`] = getLastProviders(mergeArrays(arr1, arr2));
+      });
+      metadata.exports = getLastProviders(mergeArrays(modMetadata.exports, modWitParams.exports));
+      metadata.extensionsMeta = { ...modMetadata.extensionsMeta, ...modWitParams.extensionsMeta };
+    }
     const declaredInDir = decoratorAndValue.declaredInDir || '';
     return {
       ...metadata,
       decoratorFactory: decoratorAndValue.decorator,
       declaredInDir,
-      guards: modOrObj.guards || [],
+      guards: modRefId.guards || [],
     };
   } else {
     const decoratorAndValue = reflector
-      .getMetadata<ModuleMetadataValue>(modOrObj)
+      .getMetadata<ModuleMetadataValue>(modRefId)
       ?.constructor.decorators.find(typeGuard);
     const modMetadata = decoratorAndValue?.value.data;
     if (!modMetadata) {
       return modMetadata;
     }
     const metadata = Object.assign({}, modMetadata);
-    (['App', 'Mod', 'Rou', 'Req'] as Scope[]).forEach((scope) => {
+    scopes.forEach((scope) => {
       const arr = [...(modMetadata[`providersPer${scope}`] || [])];
       if (arr.length) {
         metadata[`providersPer${scope}`] = arr;
