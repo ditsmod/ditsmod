b103064af329464d88d65b8bc6948d6410f5ed08
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-11-12 14:28:11 +0200

    refactor(examples): refactoring example 18.

--- examples/18-return/src/app/app.module.ts
+++ examples/18-return/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Providers, rootModule } from '@ditsmod/core';
-import { RoutingModule } from '@ditsmod/routing';
 
 import { FirstModule } from './modules/first/first.module.js';
 import { SecondModule } from './modules/second/second.module.js';
 
 @rootModule({
-  imports: [RoutingModule],
   appends: [FirstModule, SecondModule],
   providersPerApp: new Providers().useLogConfig({ level: 'info' }),
 })
--- examples/18-return/src/app/modules/second/second.module.ts
+++ examples/18-return/src/app/modules/second/second.module.ts
@@ -1,11 +1,10 @@
 import { HttpBackend, featureModule } from '@ditsmod/core';
-import { RoutingModule } from '@ditsmod/routing';
 import { ReturnModule } from '@ditsmod/return';
 
 import { SecondController, SecondSingletonController } from './second.controller.js';
 
 @featureModule({
-  imports: [RoutingModule, ReturnModule],
+  imports: [ReturnModule],
   controllers: [SecondController, SecondSingletonController],
   resolvedCollisionsPerReq: [
     [HttpBackend, ReturnModule]
