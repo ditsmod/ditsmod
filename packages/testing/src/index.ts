582b5d0c13c56b121174009af806edfa9016f58b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-01 16:28:40 +0200

    feat(testing): added `TestModuleManager` to exports.

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,2 +1,3 @@
 export { TestApplication } from './test-application.js';
+export { TestModuleManager } from './test-module-manager.js';
 export { TestProvider, TestClassProvider, TestFactoryProvider, TestInlineProviders } from './types.js';
