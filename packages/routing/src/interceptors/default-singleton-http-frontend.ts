03166a63f6a82f6bd79d30167b33c32213f242f9
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-04 21:28:45 +0200

    refactor(routing): added `send` to `DefaultSingletonHttpFrontend`.

--- packages/routing/src/interceptors/default-singleton-http-frontend.ts
+++ packages/routing/src/interceptors/default-singleton-http-frontend.ts
@@ -1,7 +1,8 @@
 import { parse } from 'node:querystring';
-import { AnyObj, injectable, SingletonRequestContext } from '@ditsmod/core';
+import { AnyObj, HttpMethod, injectable, RequestContext, SingletonRequestContext, Status } from '@ditsmod/core';
 
 import { HttpFrontend, HttpHandler } from './tokens-and-types.js';
+import { SILENT_RES } from '../constants.js';
 
 @injectable()
 export class DefaultSingletonHttpFrontend implements HttpFrontend {
@@ -14,6 +15,31 @@ export class DefaultSingletonHttpFrontend implements HttpFrontend {
       ctx.aPathParams.forEach((param) => (pathParams[param.key] = param.value));
       ctx.pathParams = pathParams;
     }
-    return next.handle();
+    this.send(ctx, await next.handle()); // Controller's route returned value.
+  }
+
+  send(ctx: RequestContext, val: any) {
+    if (ctx.rawRes.headersSent || val === SILENT_RES) {
+      return;
+    }
+    let { statusCode } = ctx.rawRes;
+    if (!statusCode) {
+      const httpMethod = ctx.rawReq.method as HttpMethod;
+      if (httpMethod == 'GET') {
+        statusCode = Status.OK;
+      } else if (httpMethod == 'POST') {
+        statusCode = Status.CREATED;
+      } else if (httpMethod == 'OPTIONS') {
+        statusCode = Status.NO_CONTENT;
+      } else {
+        statusCode = Status.OK;
+      }
+    }
+
+    if (typeof val == 'object' || ctx.rawRes.getHeader('content-type')?.toString().includes('application/json')) {
+      ctx.sendJson(val, statusCode);
+    } else {
+      ctx.send(val, statusCode);
+    }
   }
 }
