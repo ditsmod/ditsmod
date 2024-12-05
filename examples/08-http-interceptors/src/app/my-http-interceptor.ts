a7f09acdb1b15d768da54963a73990f4039908fd
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-05 16:13:07 +0200

    refactor(examples): a bit refactoring `MyHttpInterceptor`.

--- examples/08-http-interceptors/src/app/my-http-interceptor.ts
+++ examples/08-http-interceptors/src/app/my-http-interceptor.ts
@@ -5,18 +5,19 @@ import { HttpHandler, HttpInterceptor } from '@ditsmod/routing';
 export class MyHttpInterceptor implements HttpInterceptor {
   constructor(private logger: Logger) {}
 
-  intercept(next: HttpHandler, ctx: RequestContext) {
-    // Handling request to `HelloWorldController`
-    return next.handle().then((originalMsg: string) => {
-      // You can to do something after, for example, log status:
-      if (ctx.rawRes.headersSent) {
-        const msg = `MyHttpInterceptor works! Status code: ${ctx.rawRes.statusCode}`;
-        this.logger.log('info', msg);
-      } else {
-        ctx.rawRes.setHeader('Content-Type', 'application/json; charset=utf-8');
-        const msg = JSON.stringify({ originalMsg, msg: 'message that attached by interceptor' });
-        ctx.send(msg);
-      }
-    });
+  async intercept(next: HttpHandler, ctx: RequestContext) {
+    const originalMsg = await next.handle(); // Handling request to `HelloWorldController`
+
+    // You can to do something after, for example, log status:
+    if (ctx.rawRes.headersSent) {
+      const msg = `MyHttpInterceptor works! Status code: ${ctx.rawRes.statusCode}`;
+      this.logger.log('info', msg);
+    } else {
+      ctx.rawRes.setHeader('Content-Type', 'application/json; charset=utf-8');
+      const msg = JSON.stringify({ originalMsg, msg: 'message that attached by interceptor' });
+      ctx.send(msg);
+    }
+
+    return originalMsg;
   }
 }
