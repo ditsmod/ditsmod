1e416352a065c0e911af32996e5d88e0b73aff17
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-11 08:30:24 +0200

    refactor(core): remove unused `dfs()` and `hasCycle()`.

--- packages/core/src/extension/tarjan-graph.ts
+++ packages/core/src/extension/tarjan-graph.ts
@@ -5,41 +5,6 @@ type GroupConfig<T> = {
 
 type Graph<T> = Map<T, T[]>;
 
-export function hasCycle<T>(configs: GroupConfig<T>[]): boolean {
-  const graph = buildGraph(configs);
-  const visited = new Set<T>();
-  const stack = new Set<T>();
-
-  for (const [node] of graph) {
-    if (dfs(node, graph, visited, stack)) {
-      return true; // Cycle found
-    }
-  }
-  return false;
-}
-
-function dfs<T>(node: T, graph: Graph<T>, visited: Set<T>, stack: Set<T>): boolean {
-  if (stack.has(node)) {
-    return true; // Cycle found
-  }
-
-  if (visited.has(node)) {
-    return false; // There is no cycle here
-  }
-
-  visited.add(node);
-  stack.add(node);
-
-  for (const neighbor of graph.get(node) || []) {
-    if (dfs(neighbor, graph, visited, stack)) {
-      return true;
-    }
-  }
-
-  stack.delete(node);
-  return false;
-}
-
 function buildGraph<T>(configs: GroupConfig<T>[]): Graph<T> {
   const graph = new Map() as Graph<T>;
 
@@ -88,9 +53,6 @@ function dfsWithPath<T>(node: T, graph: Graph<T>, visited: Set<T>, stack: Set<T>
 
   for (const neighbor of graph.get(node) || []) {
     if (dfsWithPath(neighbor, graph, visited, stack, path)) {
-      if (path[0] === path[path.length - 1]) {
-        return true; // The cycle is complete
-      }
       return true;
     }
   }
