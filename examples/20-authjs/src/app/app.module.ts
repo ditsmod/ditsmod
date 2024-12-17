e212c7c3cab3a2a965526b1db6516f8bed755e27
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-17 19:31:44 +0200

    feat(examples): init example 20.

---
+++ examples/20-authjs/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { controller, rootModule, Providers } from '@ditsmod/core';
+import { route, RoutingModule } from '@ditsmod/routing';
+
+@controller()
+export class DefaultController {
+  @route('GET', 'default-controller')
+  tellHello() {
+    return 'Hello, World!';
+  }
+
+  @route(['GET', 'POST'], 'method-name-as-symbol')
+  [Symbol()]() {
+    return 'Hello, World!';
+  }
+}
+
+@controller({ scope: 'module' })
+export class SingletonController {
+  @route('GET', 'singleton-controller')
+  tellHello() {
+    return 'Hello, World!';
+  }
+}
+
+@rootModule({
+  imports: [RoutingModule],
+  controllers: [DefaultController, SingletonController],
+  providersPerApp: new Providers().useLogConfig({ level: 'info' }),
+})
+export class AppModule {}
---
+++ examples/20-authjs/src/main.ts
@@ -0,0 +1,10 @@
+import { Application } from '@ditsmod/core';
+import { AppModule } from './app/app.module.js';
+
+const authSecret = process.env.AUTH_SECRET;
+if (!authSecret) {
+  throw new Error('You need setup AUTH_SECRET variable in ".env" file.');
+}
+
+const app = await Application.create(AppModule);
+app.server.listen(3000, '0.0.0.0');
