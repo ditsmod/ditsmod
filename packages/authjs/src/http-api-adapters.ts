e07291a3b0d7a157a3b6afcebba7d9dbd3fb9048
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-18 19:58:22 +0200

    feat(authjs): init adapters.

---
+++ packages/authjs/src/http-api-adapters.ts
@@ -0,0 +1,94 @@
+import { AnyObj, RawResponse, Res, SingletonRequestContext } from '@ditsmod/core';
+
+/**
+ * Adapts Ditsmod Request to a Web Request, returning the Web Request.
+ */
+export function toWebRequest(ctx: SingletonRequestContext) {
+  const url = `${ctx.protocol}://${process.env.HOST ?? 'localhost'}${ctx.rawReq.url}`;
+
+  const headers = new Headers();
+
+  Object.entries(ctx.rawReq.headers).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((v) => v && headers.append(key, v));
+      return;
+    }
+
+    if (value) {
+      headers.append(key, value);
+    }
+  });
+
+  // GET and HEAD not allowed to receive body
+  const body = /GET|HEAD/.test(ctx.rawReq.method || '') ? undefined : encodeRequestBody(ctx);
+
+  const request = new Request(url, {
+    method: ctx.rawReq.method,
+    headers,
+    body,
+  } satisfies RequestInit);
+
+  return request;
+}
+
+/**
+ * Encodes Ditsmod Request body based on the content type header.
+ */
+function encodeRequestBody(ctx: SingletonRequestContext): string {
+  const contentType = ctx.rawReq.headers['content-type'];
+
+  if (contentType?.includes('application/x-www-form-urlencoded')) {
+    return encodeUrlEncoded(ctx.body);
+  }
+
+  if (contentType?.includes('application/json')) {
+    return encodeJson(ctx.body);
+  }
+
+  return JSON.stringify(ctx.body);
+}
+
+/**
+ * Encodes an object as url-encoded string.
+ */
+function encodeUrlEncoded(body: AnyObj = {}) {
+  const params = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(body)) {
+    if (Array.isArray(value)) {
+      value.forEach((v) => params.append(key, v));
+    } else {
+      params.append(key, value);
+    }
+  }
+
+  return params.toString();
+}
+
+/**
+ * Encodes an object as JSON
+ */
+function encodeJson(obj?: AnyObj) {
+  return JSON.stringify(obj);
+}
+
+/**
+ * Adapts a Web Response to Ditsmod Response, invoking appropriate
+ * Ditsmod response methods to handle the response.
+ */
+export async function toDitsmodResponse(response: Response, rawRes: RawResponse) {
+  response.headers.forEach((value, key) => {
+    if (value) {
+      rawRes.setHeader(key, value);
+    }
+  });
+
+  // Explicitly write the headers for content-type
+  // https://stackoverflow.com/a/59449326/13944042
+  rawRes.writeHead(response.status, response.statusText, {
+    'Content-Type': response.headers.get('content-type') || '',
+  });
+
+  // res.write(await response.text());
+  rawRes.end(await response.text());
+}
