09241266219f374a3fec480f44e2e9abfe3de00e
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-22 19:05:49 +0200

    test(root): fixed vitest config.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
     passWithNoTests: true,
     clearMocks: true,
     include: [
-      'packages/*/dist/**/*.spec.js',
+      'packages/*/dist*/**/*.spec.js',
     ],
     exclude: ['**/node_modules/**'],
     watch: false
