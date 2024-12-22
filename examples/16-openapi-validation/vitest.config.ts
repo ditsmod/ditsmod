a502cefc4ccba48a7bdfa6025fa20200bfe86cb2
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-22 21:02:26 +0200

    test(examples): fixed vitest config.

--- examples/16-openapi-validation/vitest.config.ts
+++ examples/16-openapi-validation/vitest.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
     passWithNoTests: true,
     clearMocks: true,
     include: [
-      './dist*/**/*.spec.js',
+      './dist/**/*.spec.js',
+      // './dist-e2e/**/*.spec.js', // For now, comment this becuse issue with `import.meta.resolve` , see https://github.com/vitest-dev/vitest/issues/6953
     ],
     exclude: ['**/node_modules/**'],
     watch: false
--- examples/vitest.config.ts
+++ examples/vitest.config.ts
@@ -13,8 +13,13 @@ export default defineConfig({
     environment: 'node',
     passWithNoTests: true,
     clearMocks: true,
-    include: ['examples/*/dist/**/*.spec.js'],
-    exclude: ['**/node_modules/**'],
+    include: ['examples/*/dist*/**/*.spec.js'],
+    exclude: [
+      '**/node_modules/**',
+      // For now, comment this becuse issue with `import.meta.resolve` , see https://github.com/vitest-dev/vitest/issues/6953
+      'examples/10-openapi/dist-e2e/**/*.spec.js',
+      'examples/16-openapi-validation/dist-e2e/**/*.spec.js',
+    ],
     env: {
       ...config({ path: 'examples/03-route-guards/.env' }).parsed,
     },
