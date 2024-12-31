03424424cb45abe8e3f9054d1fd4be1fd30428c6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-31 14:35:35 +0200

    fix(core): fixed `isExtensionProvider()`

--- packages/core/src/extension/type-guards.ts
+++ packages/core/src/extension/type-guards.ts
@@ -1,7 +1,10 @@
 import { Class, Provider } from '#di/types-and-models.js';
 import { Extension } from '#extension/extension-types.js';
 
-export function isExtensionProvider(provider?: Provider): provider is Class<Extension> {
-  const init = (provider as Class<Extension>)?.prototype?.init;
-  return typeof init == 'function';
+export function isExtensionProvider(provider?: Class<Extension>): provider is Class<Extension> {
+  return (
+    typeof (provider?.prototype as Extension)?.stage1 == 'function' ||
+    typeof (provider?.prototype as Extension)?.stage2 == 'function' ||
+    typeof (provider?.prototype as Extension)?.stage3 == 'function'
+  );
 }
