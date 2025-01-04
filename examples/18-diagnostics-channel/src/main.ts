7dabaa59fcab114eb55970daccdc8ae84174553a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-01-04 03:05:12 +0200

    refactor(examples): removed example 18

--- examples/18-diagnostics-channel/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { Application } from '@ditsmod/core';
-import { AppModule } from './app/app.module.js';
-
-const app = await Application.create(AppModule);
-app.server.listen(3000, '0.0.0.0');
