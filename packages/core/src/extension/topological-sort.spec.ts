27d77b6a37d2438fe0f309d7bed4a14bb9d7ef94
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-01-05 13:15:42 +0200

    refactor(core): refactoring `topologicalSort()`
    Now this function returns `Set`

--- packages/core/src/extension/topological-sort.spec.ts
+++ packages/core/src/extension/topological-sort.spec.ts
@@ -8,7 +8,8 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS2', beforeGroup: 'MY_EXTENSIONS1' },
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS2' },
     ];
-    expect(topologicalSort(configs, true)).toEqual(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    const expectedSet = new Set(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 2', () => {
@@ -17,7 +18,8 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS2', beforeGroup: 'MY_EXTENSIONS1' },
       { group: 'MY_EXTENSIONS1' },
     ];
-    expect(topologicalSort(configs, true)).toEqual(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    const expectedSet = new Set(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 3', () => {
@@ -26,7 +28,8 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS2' },
       { group: 'MY_EXTENSIONS1' },
     ];
-    expect(topologicalSort(configs, true)).toEqual(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    const expectedSet = new Set(['MY_EXTENSIONS3', 'MY_EXTENSIONS2', 'MY_EXTENSIONS1']);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 4', () => {
@@ -35,7 +38,8 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS2' },
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS2' },
     ];
-    expect(topologicalSort(configs, true)).toEqual(['MY_EXTENSIONS1', 'MY_EXTENSIONS3', 'MY_EXTENSIONS2']);
+    const expectedSet = new Set(['MY_EXTENSIONS1', 'MY_EXTENSIONS3', 'MY_EXTENSIONS2']);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 5', () => {
@@ -44,7 +48,8 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS2', beforeGroup: 'MY_EXTENSIONS1' },
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS10' },
     ];
-    expect(topologicalSort(configs, true)).toEqual(['MY_EXTENSIONS2', 'MY_EXTENSIONS1', 'MY_EXTENSIONS3']);
+    const expectedSet = new Set(['MY_EXTENSIONS2', 'MY_EXTENSIONS1', 'MY_EXTENSIONS3']);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 6', () => {
@@ -55,13 +60,14 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS4', beforeGroup: 'MY_EXTENSIONS1' },
       { group: 'MY_EXTENSIONS5', beforeGroup: 'MY_EXTENSIONS4' },
     ];
-    expect(topologicalSort(configs, true)).toEqual([
+    const expectedSet = new Set([
       'MY_EXTENSIONS3',
       'MY_EXTENSIONS2',
       'MY_EXTENSIONS5',
       'MY_EXTENSIONS4',
       'MY_EXTENSIONS1',
     ]);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 7', () => {
@@ -72,13 +78,14 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS2' },
       { group: 'MY_EXTENSIONS4', beforeGroup: 'MY_EXTENSIONS1' },
     ];
-    expect(topologicalSort(configs, true)).toEqual([
+    const expectedSet = new Set([
       'MY_EXTENSIONS5',
       'MY_EXTENSIONS3',
       'MY_EXTENSIONS2',
       'MY_EXTENSIONS4',
       'MY_EXTENSIONS1',
     ]);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 
   it('case 8', () => {
@@ -89,12 +96,13 @@ describe('topologicalSort()', () => {
       { group: 'MY_EXTENSIONS3', beforeGroup: 'MY_EXTENSIONS2' },
       { group: 'MY_EXTENSIONS4', beforeGroup: 'MY_EXTENSIONS1' },
     ];
-    expect(topologicalSort(configs, true)).toEqual([
+    const expectedSet = new Set([
       'MY_EXTENSIONS3',
       'MY_EXTENSIONS2',
       'MY_EXTENSIONS5',
       'MY_EXTENSIONS4',
       'MY_EXTENSIONS1',
     ]);
+    expect(topologicalSort(configs, true)).toEqual(expectedSet);
   });
 });
--- packages/core/src/extension/topological-sort.ts
+++ packages/core/src/extension/topological-sort.ts
@@ -11,36 +11,37 @@ type Graph<T> = Map<T, T[]>;
 export function topologicalSort<T = any, R extends GroupConfig<T> = GroupConfig<any>>(
   configs: AnyObj[],
   groupsOnly: true,
-): T[];
+): Set<T>;
 export function topologicalSort<T = any, R extends GroupConfig<T> = GroupConfig<any>>(
   configs: AnyObj[],
   groupsOnly?: false,
-): R[];
+): Set<R>;
 export function topologicalSort<T = any, R extends GroupConfig<T> = GroupConfig<any>>(
   inputConfigs: AnyObj[],
   groupsOnly?: boolean,
-): R[] | T[] {
+): Set<R> | Set<T> {
   const configs = inputConfigs.filter(isGroupConfig) as R[];
   const { origin, graph } = getGraph<T>(configs);
   const visited = new Set<T>();
-  const orderedGroups: T[] = [];
+  const orderedGroupsSet = new Set<T>();
 
   // Running DFS for each group.
   for (const group of origin) {
     if (!visited.has(group)) {
-      dfs(graph, visited, orderedGroups, group);
+      dfs(graph, visited, orderedGroupsSet, group);
     }
   }
 
   if (groupsOnly) {
-    return orderedGroups;
+    return orderedGroupsSet;
   }
   // Mapping the sorted result to GroupConfig<T>
-  return orderedGroups.map((group) => configs.find((config) => config.group === group)!);
+  const orderedGroups = [...orderedGroupsSet].map((group) => configs.find((config) => config.group === group)!);
+  return new Set(orderedGroups);
 }
 
 // Recursive depth-first search.
-function dfs<T>(graph: Graph<T>, visited: Set<T>, orderedGroups: T[], group: T) {
+function dfs<T>(graph: Graph<T>, visited: Set<T>, orderedGroups: Set<T>, group: T) {
   if (visited.has(group)) {
     return;
   }
@@ -51,5 +52,5 @@ function dfs<T>(graph: Graph<T>, visited: Set<T>, orderedGroups: T[], group: T) 
     dfs(graph, visited, orderedGroups, neighbor);
   }
 
-  orderedGroups.push(group); // Adding a group after processing all dependencies.
+  orderedGroups.add(group); // Adding a group after processing all dependencies.
 }
