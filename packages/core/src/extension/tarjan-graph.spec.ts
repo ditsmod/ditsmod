1292e7c1b9b0c67e8f0b7fff3c284fdc6f2421e0
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-01-06 18:13:29 +0200

    test(core): a bit refactoring tests for `findCycle()`

--- packages/core/src/extension/tarjan-graph.spec.ts
+++ packages/core/src/extension/tarjan-graph.spec.ts
@@ -2,52 +2,54 @@ import { describe, expect, it } from 'vitest';
 import { findCycle, GroupConfig } from './tarjan-graph.js';
 
 describe("Tarjan's algorithm", () => {
-  it('no circular dependencies (case 1)', () => {
-    const configs: GroupConfig<string>[] = [
-      { group: 'ext2', beforeGroup: 'ext1' },
-      { group: 'ext1', beforeGroup: 'ext3' },
-    ];
+  describe('circular dependencies', () => {
+    it('case 1', () => {
+      const configs: GroupConfig<string>[] = [
+        { group: 'ext2', beforeGroup: 'ext1' },
+        { group: 'ext1', beforeGroup: 'ext3' },
+      ];
 
-    expect(findCycle(configs)).toBe(null);
-  });
+      expect(findCycle(configs)).toBe(null);
+    });
 
-  it('no circular dependencies (case 2)', () => {
-    const configs: GroupConfig<string>[] = [
-      { group: 'ext3', beforeGroup: 'ext2' },
-      { group: 'ext2', beforeGroup: 'ext1' },
-      { group: 'ext1', beforeGroup: 'ext4' },
-    ];
+    it('case 2', () => {
+      const configs: GroupConfig<string>[] = [
+        { group: 'ext3', beforeGroup: 'ext2' },
+        { group: 'ext2', beforeGroup: 'ext1' },
+        { group: 'ext1', beforeGroup: 'ext4' },
+      ];
 
-    expect(findCycle(configs)).toBe(null);
-  });
+      expect(findCycle(configs)).toBe(null);
+    });
 
-  it('has direct circular dependencies', () => {
-    const configs: GroupConfig<string>[] = [
-      { group: 'ext2', beforeGroup: 'ext1' },
-      { group: 'ext1', beforeGroup: 'ext2' },
-    ];
+    it('case 3', () => {
+      const configs: GroupConfig<string>[] = [
+        { group: 'ext2', beforeGroup: 'ext1' },
+        { group: 'ext1', beforeGroup: 'ext2' },
+      ];
 
-    expect(findCycle(configs)).toEqual(['ext2', 'ext1', 'ext2']);
-  });
+      expect(findCycle(configs)).toEqual(['ext2', 'ext1', 'ext2']);
+    });
 
-  it('has circular dependencies with mediator', () => {
-    const configs: GroupConfig<string>[] = [
-      { group: 'ext3', beforeGroup: 'ext2' },
-      { group: 'ext2', beforeGroup: 'ext1' },
-      { group: 'ext1', beforeGroup: 'ext3' },
-    ];
-    expect(findCycle(configs)).toEqual(['ext3', 'ext2', 'ext1', 'ext3']);
-  });
+    it('has circular dependencies with mediator', () => {
+      const configs: GroupConfig<string>[] = [
+        { group: 'ext3', beforeGroup: 'ext2' },
+        { group: 'ext2', beforeGroup: 'ext1' },
+        { group: 'ext1', beforeGroup: 'ext3' },
+      ];
+      expect(findCycle(configs)).toEqual(['ext3', 'ext2', 'ext1', 'ext3']);
+    });
 
-  it('has circular dependencies with mediator and other deps', () => {
-    const configs: GroupConfig<string>[] = [
-      { group: 'ext3', beforeGroup: 'ext2' },
-      { group: 'ext3', beforeGroup: 'ext4' },
-      { group: 'ext2', beforeGroup: 'ext1' },
-      { group: 'ext2', beforeGroup: 'ext4' },
-      { group: 'ext1', beforeGroup: 'ext3' },
-    ];
+    it('has circular dependencies with mediator and other deps', () => {
+      const configs: GroupConfig<string>[] = [
+        { group: 'ext3', beforeGroup: 'ext2' },
+        { group: 'ext3', beforeGroup: 'ext4' },
+        { group: 'ext2', beforeGroup: 'ext1' },
+        { group: 'ext2', beforeGroup: 'ext4' },
+        { group: 'ext1', beforeGroup: 'ext3' },
+      ];
 
-    expect(findCycle(configs)).toEqual(['ext3', 'ext2', 'ext1', 'ext3']);
+      expect(findCycle(configs)).toEqual(['ext3', 'ext2', 'ext1', 'ext3']);
+    });
   });
 });
