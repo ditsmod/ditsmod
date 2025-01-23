4dae9c9d4d5413ea2847f3204b2c19648888ce99
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-01-23 16:33:08 +0200

    refactor(routing): refactoring imports into `RoutingModule`

--- packages/routing/src/module/routing.module.ts
+++ packages/routing/src/module/routing.module.ts
@@ -1,13 +1,13 @@
 import { featureModule } from '@ditsmod/core';
 
-import { DefaultRouter, Router } from '../services/router.js';
-import { RoutingErrorMediator } from '../services/router-error-mediator.js';
-import { RoutesExtension } from '../extensions/routes.extension.js';
-import { PreRouterExtension } from '../extensions/pre-router.extension.js';
-import { RouteMeta } from '../types/route-data.js';
+import { DefaultRouter, Router } from '#services/router.js';
+import { RoutingErrorMediator } from '#services/router-error-mediator.js';
+import { RoutesExtension } from '#extensions/routes.extension.js';
+import { PreRouterExtension } from '#extensions/pre-router.extension.js';
+import { RouteMeta } from '#types/route-data.js';
 import { UseInterceptorExtension } from '#extensions/use-interceptor.extension.js';
-import { RoutingModuleNormalizer } from './module-normalizer.js';
-import { RoutingModuleMetadata } from './module-metadata.js';
+import { RoutingModuleNormalizer } from '#module/module-normalizer.js';
+import { RoutingModuleMetadata } from '#module/module-metadata.js';
 
 /**
  * Sets `Router` provider on application level, and adds `RoutesExtension` with `PreRouterExtension`.
