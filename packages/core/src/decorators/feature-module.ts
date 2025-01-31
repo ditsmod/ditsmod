23b6a1f16821367755281110e55b1156ab951d2d
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-02-01 00:59:20 +0200

    refactor(core): refactoring `AttachedMetadata`

--- packages/core/src/decorators/feature-module.ts
+++ packages/core/src/decorators/feature-module.ts
@@ -1,9 +1,10 @@
-import { makeClassDecorator } from '#di';
+import { DecoratorAndValue, makeClassDecorator } from '#di';
 import { ModuleMetadata, ModuleWithParams } from '#types/module-metadata.js';
-import { AnyFn, AnyObj, ModuleType } from '#types/mix.js';
+import { AnyFn, AnyObj, ModuleType, Override } from '#types/mix.js';
 import { objectKeys } from '#utils/object-keys.js';
 import { Providers } from '#utils/providers.js';
 import { CallsiteUtils } from '#utils/callsites.js';
+import { mergeArrays } from '#utils/merge-arrays.js';
 
 export const featureModule: FeatureModuleDecorator = makeClassDecorator(transformModule);
 
@@ -11,7 +12,28 @@ export interface FeatureModuleDecorator {
   (data?: ModuleMetadata): any;
 }
 
-export function transformModule(data?: ModuleMetadata) {
+function mergeModuleWithParams(modWitParams: ModuleWithParams, decorAndVal: DecoratorAndValue<AttachedMetadata>) {
+  const rawMeta = Object.assign({}, decorAndVal.value.metadata) as RawMeta;
+  if (modWitParams.id) {
+    rawMeta.id = modWitParams.id;
+  }
+  for (const param of modWitParams.params) {
+    if (param.decorator !== decorAndVal.decorator) {
+      continue;
+    }
+    objectKeys(param.metadata).forEach((p) => {
+      // If here is object with [Symbol.iterator]() method, this transform it to an array.
+      if (Array.isArray(param.metadata[p]) || param.metadata[p] instanceof Providers) {
+        (rawMeta as any)[p] = mergeArrays((rawMeta as any)[p], param.metadata[p]);
+      }
+    });
+
+    rawMeta.extensionsMeta = { ...rawMeta.extensionsMeta, ...param.metadata.extensionsMeta };
+  }
+  return rawMeta;
+}
+
+export function transformModule(data?: ModuleMetadata): Override<AttachedMetadata, { metadata: RawMeta }> {
   const metadata = Object.assign({}, data) as RawMeta;
   objectKeys(metadata).forEach((p) => {
     // If here is object with [Symbol.iterator]() method, this transform it to an array.
@@ -21,16 +43,15 @@ export function transformModule(data?: ModuleMetadata) {
   });
   metadata.decorator = featureModule;
   metadata.declaredInDir = CallsiteUtils.getCallerDir() || '.';
-  return { isAttachedMetadata: true, metadata };
+  return { isAttachedMetadata: true, metadata, mergeModuleWithParams };
 }
-
 /**
  * A metadata attached to the `rootModule` or `featureModule` decorators.
  */
 export interface AttachedMetadata {
   isAttachedMetadata: true;
   metadata: AnyObj;
-  normalize?: (arg: any) => AnyObj;
+  mergeModuleWithParams?: (modWitParams: ModuleWithParams, decorAndVal: DecoratorAndValue<AttachedMetadata>) => AnyObj;
 }
 
 /**
