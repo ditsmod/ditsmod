1a42afb7c5fa46cf97b29678b0242356518ee3d2
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-02-01 06:08:33 +0200

    refactor(routing): refactoring `routingMetadata` decorator

--- packages/routing/src/decorators/routing-metadata.ts
+++ packages/routing/src/decorators/routing-metadata.ts
@@ -1,4 +1,15 @@
-import { makeClassDecorator, Providers, AnyFn, objectKeys, AttachedMetadata } from '@ditsmod/core';
+import {
+  makeClassDecorator,
+  Providers,
+  AnyFn,
+  objectKeys,
+  AttachedMetadata,
+  mergeArrays,
+  DecoratorAndValue,
+  ModuleWithParams,
+  RawMeta,
+  Override,
+} from '@ditsmod/core';
 
 import { RoutingMetadata } from '#module/module-metadata.js';
 
@@ -8,15 +19,31 @@ export interface RoutingMetadataDecorator {
   (data?: RoutingMetadata): any;
 }
 
-export function transformMetadata(data?: RoutingMetadata): AttachedMetadata {
-  const metadata = Object.assign({}, data);
+function mergeModuleWithParams(modWitParams: ModuleWithParams, decorAndVal: DecoratorAndValue<AttachedMetadata>) {
+  const rawMeta = Object.assign({}, decorAndVal.value.metadata) as RawMeta;
+  for (const param of modWitParams.params) {
+    if (param.decorator !== decorAndVal.decorator) {
+      continue;
+    }
+    objectKeys(param.metadata).forEach((p) => {
+      // If here is object with [Symbol.iterator]() method, this transform it to an array.
+      if (Array.isArray(param.metadata[p]) || param.metadata[p] instanceof Providers) {
+        (rawMeta as any)[p] = mergeArrays((rawMeta as any)[p], param.metadata[p]);
+      }
+    });
+  }
+  return rawMeta;
+}
+
+export function transformMetadata(data?: RoutingMetadata): Override<AttachedMetadata, { metadata: RawMeta }> {
+  const metadata = Object.assign({}, data) as RawMeta;
   objectKeys(metadata).forEach((p) => {
     // If here is object with [Symbol.iterator]() method, this transform it to an array.
     if (metadata[p] instanceof Providers) {
       (metadata as any)[p] = [...metadata[p]];
     }
   });
-  return { isAttachedMetadata: true, metadata};
+  return { isAttachedMetadata: true, metadata, mergeModuleWithParams };
 }
 
 /**
