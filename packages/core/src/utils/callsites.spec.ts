a8058de8612956e242af9d2fcb2a3280649871e9
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-02-08 20:31:47 +0200

    test(core): fixed tests for `CallsiteUtils.getCallerDir()`

--- packages/core/src/utils/callsites.spec.ts
+++ packages/core/src/utils/callsites.spec.ts
@@ -1,32 +1,32 @@
 import { CallsiteUtils } from '#utils/callsites.js';
 
 describe('CallsiteUtils.getCallerDir()', function some1() {
-  const expectedPath = import.meta.dirname;
+  const expectedPath = 'ditsmod/ditsmod/packages/core/dist/utils';
 
   it('inside class method and common function', function some2() {
     class Test3 {
       method1() {
         const path = CallsiteUtils.getCallerDir();
-        expect(path).contain(expectedPath);
+        expect(path).toContain(expectedPath);
       }
     }
 
     expect(function some3() {
       new Test3().method1();
-    }).not.throw();
+    }).not.toThrow();
   });
 
   it('inside common function', function some2() {
     expect(function some3() {
       const path = CallsiteUtils.getCallerDir();
-      expect(path).contain(expectedPath);
-    }).not.throw();
+      expect(path).toContain(expectedPath);
+    }).not.toThrow();
   });
 
   it('inside narrow function', () => {
     expect(() => {
       const path = CallsiteUtils.getCallerDir();
-      expect(path).contain(expectedPath);
-    }).not.throw();
+      expect(path).toContain(expectedPath);
+    }).not.toThrow();
   });
 });
