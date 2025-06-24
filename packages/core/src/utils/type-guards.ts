57a4fe9cadf174cad4b1e673903d79dccc787cd3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-06-24 19:28:30 +0300

    fix(core): fixed `isModuleWithParams()`

--- packages/core/src/utils/type-guards.ts
+++ packages/core/src/utils/type-guards.ts
@@ -67,7 +67,7 @@ export function hasDeclaredInDir(
 }
 
 export function isModuleWithParams(modRefId?: ModRefId): modRefId is ModuleWithParams {
-  return (modRefId as ModuleWithParams)?.module !== undefined;
+  return (modRefId as ModuleWithParams)?.module !== undefined && Array.isArray((modRefId as ModuleWithParams).params);
 }
 
 export function isProvider(maybeProvider?: any): maybeProvider is Provider {
