481e8e8318416fc5f4b3f70840e78096c091c761
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-07-01 14:27:40 +0300

    refactor(routing): refactoring `routingMetadata` decorator

--- packages/routing/src/decorators/routing-metadata.ts
+++ packages/routing/src/decorators/routing-metadata.ts
@@ -14,10 +14,8 @@ import {
   objectKeys,
 } from '@ditsmod/core';
 
-import { RoutingModuleParams, RoutingMetadata } from '#module/module-metadata.js';
+import { RoutingModuleParams, RoutingMetadata, RoutingMetadataWithParams } from '#module/module-metadata.js';
 import { RoutingMetadataNormalizer } from '#module/routing-metadata-normalizer.js';
-import { GuardItem, NormalizedGuard } from '#interceptors/guard.js';
-import { RoutingNormalizedMeta } from '#types/routing-normalized-meta.js';
 import { RoutingModuleFactory } from '#module/routing-module-factory.js';
 
 export const routingMetadata: RoutingMetadataDecorator = makeClassDecorator(transformMetadata);
@@ -31,50 +29,33 @@ export interface RoutingMetadataDecorator {
 }
 
 function mergeModuleWithParams(modWitParams: ModuleWithParams, decorAndVal: DecoratorAndValue<AttachedMetadata>) {
-  const meta = decorAndVal.value.metadata as RoutingNormalizedMeta;
-  for (const param of (modWitParams.params || [])) {
+  const metadata = decorAndVal.value.metadata as RoutingMetadata;
+  const metadata1 = Object.assign({}, metadata) as RoutingMetadataWithParams;
+  for (const param of modWitParams.params || []) {
     if (param.decorator !== decorAndVal.decorator) {
       continue;
     }
     objectKeys(param.metadata).forEach((p) => {
       // If here is object with [Symbol.iterator]() method, this transform it to an array.
       if (Array.isArray(param.metadata[p]) || param.metadata[p] instanceof Providers) {
-        (meta as any)[p] = mergeArrays((meta as any)[p], param.metadata[p]);
+        (metadata1 as any)[p] = mergeArrays((metadata1 as any)[p], param.metadata[p]);
       }
     });
-    if ((param.metadata as RoutingModuleParams).guards?.length) {
-      meta.guardsPerMod.push(...normalizeGuards((param.metadata as RoutingModuleParams).guards));
-      checkGuardsPerMod(meta.guardsPerMod);
-    }
+    metadata1.path = (param.metadata as RoutingModuleParams).path;
+    metadata1.absolutePath = (param.metadata as RoutingModuleParams).absolutePath;
     break;
   }
-  return meta;
-}
-
-function normalizeGuards(guards?: GuardItem[]) {
-  return (guards || []).map((item) => {
-    if (Array.isArray(item)) {
-      return { guard: item[0], params: item.slice(1) } as NormalizedGuard;
-    } else {
-      return { guard: item } as NormalizedGuard;
-    }
-  });
-}
-
-function checkGuardsPerMod(guards: NormalizedGuard[]) {
-  for (const Guard of guards.map((n) => n.guard)) {
-    const type = typeof Guard?.prototype.canActivate;
-    if (type != 'function') {
-      throw new TypeError(`Import with guards failed: Guard.prototype.canActivate must be a function, got: ${type}`);
-    }
-  }
+  return metadata1;
 }
 
 export function transformMetadata(data?: RoutingMetadata): AttachedMetadata {
+  const metadata = Object.assign({}, data);
   return {
     isAttachedMetadata: true,
-    metadata: data || {},
-    normalize: (baseMeta: NormalizedMeta) => new RoutingMetadataNormalizer().normalize(baseMeta, data),
+    metadata,
+    mergeModuleWithParams,
+    normalize: (baseMeta: NormalizedMeta, metadataWithParams: RoutingMetadataWithParams) =>
+      new RoutingMetadataNormalizer().normalize(baseMeta, metadataWithParams),
     exportGlobalProviders: (moduleManager: ModuleManager, baseMeta: NormalizedMeta, providersPerApp: Provider[]) => {
       new RoutingModuleFactory().exportGlobalProviders(moduleManager, baseMeta, providersPerApp);
     },
@@ -87,6 +68,5 @@ export function transformMetadata(data?: RoutingMetadata): AttachedMetadata {
         unfinishedScanModules: Set<ModRefId>,
       ]
     ) => new RoutingModuleFactory().bootstrap(...args),
-    mergeModuleWithParams,
   };
 }
