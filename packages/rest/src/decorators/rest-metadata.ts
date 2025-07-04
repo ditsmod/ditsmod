e2ab668b365b97f8acc27f5a3ba9d74e53318cf3
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-07-04 20:41:05 +0300

    refactor(rest): refactoring `transformMetadata()`

--- packages/rest/src/decorators/rest-metadata.ts
+++ packages/rest/src/decorators/rest-metadata.ts
@@ -11,6 +11,7 @@ import {
 import { RoutingMetadata } from '#module/module-metadata.js';
 import { RoutingMetadataNormalizer } from '#module/rest-metadata-normalizer.js';
 import { RoutingModuleFactory } from '#module/rest-module-factory.js';
+import { RoutingNormalizedMeta } from '#types/rest-normalized-meta.js';
 
 export const restMetadata: RoutingMetadataDecorator = makeClassDecorator(transformMetadata);
 
@@ -23,12 +24,16 @@ export function transformMetadata(data?: RoutingMetadata): AttachedMetadata {
   return {
     isAttachedMetadata: true,
     metadata,
-    normalize: (baseMeta: NormalizedMeta, metadataWithParams: RoutingMetadata) =>
-      new RoutingMetadataNormalizer().normalize(baseMeta, metadataWithParams),
-    exportGlobalProviders: (moduleManager: ModuleManager, baseMeta: NormalizedMeta, providersPerApp: Provider[]) => {
-      new RoutingModuleFactory().exportGlobalProviders(moduleManager, baseMeta, providersPerApp);
+    normalize(baseMeta: NormalizedMeta, metadataWithParams: RoutingMetadata) {
+      return new RoutingMetadataNormalizer().normalize(baseMeta, metadataWithParams);
     },
-    bootstrap: (
+    getImportsOrExports(meta: RoutingNormalizedMeta) {
+      return meta.appendsModules.concat(meta.appendsWithParams as any[]);
+    },
+    exportGlobalProviders(moduleManager: ModuleManager, baseMeta: NormalizedMeta, providersPerApp: Provider[]) {
+      return new RoutingModuleFactory().exportGlobalProviders(moduleManager, baseMeta, providersPerApp);
+    },
+    bootstrap(
       ...args: [
         providersPerApp: Provider[],
         globalProviders: GlobalProviders,
@@ -36,6 +41,8 @@ export function transformMetadata(data?: RoutingMetadata): AttachedMetadata {
         moduleManager: ModuleManager,
         unfinishedScanModules: Set<ModRefId>,
       ]
-    ) => new RoutingModuleFactory().bootstrap(...args),
+    ) {
+      return new RoutingModuleFactory().bootstrap(...args);
+    },
   };
 }
