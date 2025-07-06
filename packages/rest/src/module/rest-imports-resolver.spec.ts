2dabcd6599a9a352364201ff397dbe34a698a856
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-07-06 18:46:58 +0300

    feat(rest): introducing `RestImportsResolver`

---
+++ packages/rest/src/module/rest-imports-resolver.spec.ts
@@ -0,0 +1,97 @@
+import { forwardRef, inject, injectable, Injector, KeyRegistry } from '#di';
+
+import { ImportsResolver } from '#init/imports-resolver.js';
+import { NormalizedMeta } from '#types/normalized-meta.js';
+import { ImportedTokensMap } from '#types/metadata-per-mod.js';
+import { ModRefId, ModuleType, Level } from '#types/mix.js';
+import { Provider } from '#di/types-and-models.js';
+import { ModuleWithParams } from '#types/module-metadata.js';
+import { ModuleFactory } from '#init/module-factory.js';
+import { ModuleManager } from '#init/module-manager.js';
+import { SystemLogMediator } from '#logger/system-log-mediator.js';
+import { SystemErrorMediator } from '#error/system-error-mediator.js';
+import { clearDebugClassNames } from '#utils/get-debug-class-name.js';
+
+describe('ImportsResolver', () => {
+  @injectable()
+  class ImportsResolverMock extends ImportsResolver {
+    declare unfinishedSearchDependecies: [ModuleType | ModuleWithParams, Provider][];
+    override resolveImportedProviders(
+      targetProviders: NormalizedMeta,
+      importedTokensMap: ImportedTokensMap,
+      levels: Level[],
+    ) {
+      return super.resolveImportedProviders(targetProviders, importedTokensMap, levels);
+    }
+    override addToUnfinishedSearchDependecies(module: ModuleType | ModuleWithParams, provider: Provider) {
+      return super.addToUnfinishedSearchDependecies(module, provider);
+    }
+    override deleteFromUnfinishedSearchDependecies(module: ModuleType | ModuleWithParams, provider: Provider) {
+      return super.deleteFromUnfinishedSearchDependecies(module, provider);
+    }
+
+    override grabDependecies(
+      targetMeta: NormalizedMeta,
+      sourceModule: ModRefId,
+      importedProvider: Provider,
+      levels: Level[],
+      path: any[] = [],
+    ) {
+      return super.grabDependecies(targetMeta, sourceModule, importedProvider, levels, path);
+    }
+  }
+
+  let mock: ImportsResolverMock;
+  let moduleFactory: ModuleFactory;
+  let moduleManager: ModuleManager;
+  let systemLogMediator: SystemLogMediator;
+  let errorMediator: SystemErrorMediator;
+
+  beforeEach(() => {
+    clearDebugClassNames();
+    const injectorPerApp = Injector.resolveAndCreate([ModuleFactory]);
+    moduleFactory = injectorPerApp.get(ModuleFactory);
+    systemLogMediator = new SystemLogMediator({ moduleName: 'fakeName' });
+    errorMediator = new SystemErrorMediator({ moduleName: 'fakeName' });
+    moduleManager = new ModuleManager(systemLogMediator);
+    mock = new ImportsResolverMock(moduleManager, null as any, null as any, null as any, null as any);
+  });
+
+  describe('resolveImportedProviders', () => {
+    describe('addToUnfinishedSearchDependecies(), deleteFromUnfinishedSearchDependecies() and throwCircularDependencies()', () => {
+      class Module1 {}
+      class Provider1 {}
+      class Module2 {}
+      class Provider2 {}
+      class Module3 {}
+      class Provider3 {}
+
+      it('adding and removing dependecies', () => {
+        expect(mock.unfinishedSearchDependecies).toEqual([]);
+        mock.addToUnfinishedSearchDependecies(Module1, Provider1);
+        mock.addToUnfinishedSearchDependecies(Module2, Provider2);
+        mock.addToUnfinishedSearchDependecies(Module3, Provider3);
+        expect(mock.unfinishedSearchDependecies).toEqual([
+          [Module1, Provider1],
+          [Module2, Provider2],
+          [Module3, Provider3],
+        ]);
+        mock.deleteFromUnfinishedSearchDependecies(Module2, Provider2);
+        expect(mock.unfinishedSearchDependecies).toEqual([
+          [Module1, Provider1],
+          [Module3, Provider3],
+        ]);
+      });
+
+      it('throw properly message', () => {
+        expect(mock.unfinishedSearchDependecies).toEqual([]);
+        mock.addToUnfinishedSearchDependecies(Module1, Provider1);
+        mock.addToUnfinishedSearchDependecies(Module2, Provider2);
+        mock.addToUnfinishedSearchDependecies(Module3, Provider3);
+        const msg =
+          'Detected circular dependencies: [Provider2 in Module2] -> [Provider3 in Module3] -> [Provider2 in Module2]. It is started from [Provider1 in Module1].';
+        expect(() => mock.addToUnfinishedSearchDependecies(Module2, Provider2)).toThrow(msg);
+      });
+    });
+  });
+});
---
+++ packages/rest/src/module/rest-imports-resolver.ts
@@ -0,0 +1,309 @@
+import {
+  ModRefId,
+  Provider,
+  ExtensionCounters,
+  ModuleManager,
+  SystemLogMediator,
+  SystemErrorMediator,
+  getTokens,
+  NormalizedMeta,
+  getLastProviders,
+  getDependencies,
+  defaultProvidersPerApp,
+  Injector,
+  BaseAppOptions,
+  getDebugClassName,
+  ReflectiveDependency,
+  getProviderName,
+} from '@ditsmod/core';
+
+import { Level } from '#types/types.js';
+import { defaultProvidersPerRou } from '#providers/default-providers-per-rou.js';
+import { defaultProvidersPerReq } from '#providers/default-providers-per-req.js';
+import { RestImportedTokensMap, RestMetadataPerMod1 } from './rest-module-factory.js';
+import { RestNormalizedMeta } from '#types/rest-normalized-meta.js';
+
+export class RestProvidersForMod {
+  // providersPerMod: Provider[] = [];
+  providersPerRou: Provider[] = [];
+  providersPerReq: Provider[] = [];
+}
+
+export class RestImportsResolver {
+  protected unfinishedSearchDependecies: [ModRefId, Provider][] = [];
+  protected tokensPerApp: any[];
+  protected extensionsTokens: any[] = [];
+  protected extensionCounters = new ExtensionCounters();
+
+  constructor(
+    private moduleManager: ModuleManager,
+    protected providersPerApp: Provider[],
+    protected log: SystemLogMediator,
+    protected errorMediator: SystemErrorMediator,
+    protected metadataPerMod1?: RestMetadataPerMod1,
+  ) {}
+
+  resolve() {
+    const levels: Level[] = ['Rou', 'Req'];
+    this.tokensPerApp = getTokens(this.providersPerApp);
+    if (!this.metadataPerMod1) {
+      return;
+    }
+    const { importedTokensMap, guardsPerMod1, prefixPerMod, meta } = this.metadataPerMod1;
+    this.resolveImportedProviders(meta, importedTokensMap, levels);
+    meta.providersPerRou.unshift(...defaultProvidersPerRou);
+    meta.providersPerReq.unshift(...defaultProvidersPerReq);
+    return {
+      meta,
+      guardsPerMod1,
+      prefixPerMod,
+    };
+  }
+
+  protected resolveImportedProviders(
+    targetProviders: RestNormalizedMeta,
+    importedTokensMap: RestImportedTokensMap,
+    levels: Level[],
+  ) {
+    levels.forEach((level, i) => {
+      importedTokensMap[`per${level}`].forEach((importObj) => {
+        targetProviders[`providersPer${level}`].unshift(...importObj.providers);
+        importObj.providers.forEach((importedProvider) => {
+          this.grabDependecies(targetProviders, importObj.modRefId, importedProvider, levels.slice(i));
+        });
+      });
+
+      importedTokensMap[`multiPer${level}`].forEach((multiProviders, sourceModule) => {
+        targetProviders[`providersPer${level}`].unshift(...multiProviders);
+        multiProviders.forEach((importedProvider) => {
+          this.grabDependecies(targetProviders, sourceModule, importedProvider, levels.slice(i));
+        });
+      });
+    });
+  }
+
+  /**
+   * @param targetProviders These are metadata of the module where providers are imported.
+   * @param srcModRefId Module from where imports providers.
+   * @param importedProvider Imported provider.
+   * @param levels Search in this levels. The level order is important.
+   */
+  protected grabDependecies(
+    targetProviders: RestProvidersForMod,
+    srcModRefId: ModRefId,
+    importedProvider: Provider,
+    levels: Level[],
+    path: any[] = [],
+  ) {
+    const srcBaseMeta = this.moduleManager.getMetadata(srcModRefId, true);
+
+    for (const dep of this.getDependencies(importedProvider)) {
+      let found: boolean = false;
+      if (this.extensionsTokens.includes(dep.token)) {
+        continue;
+      }
+
+      for (const level of levels) {
+        const sourceProviders = getLastProviders(srcBaseMeta[`providersPer${level}`]);
+
+        getTokens(sourceProviders).forEach((sourceToken, i) => {
+          if (sourceToken === dep.token) {
+            const importedProvider2 = sourceProviders[i];
+            targetProviders[`providersPer${level}`].unshift(importedProvider2);
+            found = true;
+            this.grabDependeciesAgain(targetProviders, srcModRefId, importedProvider2, levels, path);
+
+            // The loop does not breaks because there may be multi providers.
+          }
+        });
+
+        if (found) {
+          break;
+        }
+      }
+
+      if (!found && !this.tokensPerApp.includes(dep.token)) {
+        this.grabImportedDependecies(targetProviders, srcModRefId, importedProvider, levels, path, dep);
+      }
+    }
+  }
+
+  /**
+   * @param targetProviders These are metadata of the module where providers are imported.
+   * @param sourceModule1 Module from where imports providers.
+   * @param importedProvider Imported provider.
+   * @param dep ReflectiveDependecy with token for dependecy of imported provider.
+   */
+  protected grabImportedDependecies(
+    targetProviders: RestProvidersForMod,
+    sourceModule1: ModRefId,
+    importedProvider: Provider,
+    levels: Level[],
+    path: any[] = [],
+    dep: ReflectiveDependency,
+  ) {
+    let found = false;
+    const metadataPerMod1 = this.appMetadataMap.get(sourceModule1)!;
+    for (const level of levels) {
+      const importObj = metadataPerMod1.importedTokensMap[`per${level}`].get(dep.token);
+      if (importObj) {
+        found = true;
+        path.push(dep.token);
+        const { modRefId: modRefId2, providers: sourceProviders2 } = importObj;
+        targetProviders[`providersPer${level}`].unshift(...sourceProviders2);
+
+        // Loop for multi providers.
+        for (const sourceProvider2 of sourceProviders2) {
+          this.grabDependeciesAgain(targetProviders, modRefId2, sourceProvider2, levels, path);
+        }
+        break;
+      }
+    }
+
+    if (!found && dep.required) {
+      this.throwError(metadataPerMod1.baseMeta, importedProvider, path, dep.token, levels);
+    }
+  }
+
+  protected grabDependeciesAgain(
+    targetProviders: RestProvidersForMod,
+    sourceModule: ModRefId,
+    importedProvider: Provider,
+    levels: Level[],
+    path: any[],
+  ) {
+    this.addToUnfinishedSearchDependecies(sourceModule, importedProvider);
+    this.grabDependecies(targetProviders, sourceModule, importedProvider, levels, path);
+    this.deleteFromUnfinishedSearchDependecies(sourceModule, importedProvider);
+  }
+
+  protected hasUnresolvedDependecies(module: ModRefId, provider: Provider, levels: Level[]) {
+    const baseMeta = this.moduleManager.getMetadata(module, true);
+
+    for (const dep of this.getDependencies(provider)) {
+      let found: boolean = false;
+      if (this.extensionsTokens.includes(dep.token)) {
+        continue;
+      }
+
+      forLevel: for (const level of levels) {
+        const providers = getLastProviders(baseMeta[`providersPer${level}`]);
+
+        for (const token of getTokens(providers)) {
+          if (token === dep.token) {
+            found = true;
+            break forLevel;
+          }
+        }
+      }
+
+      if (!found && !this.tokensPerApp.includes(dep.token)) {
+        if (this.hasUnresolvedImportedDependecies(module, levels, dep)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  protected hasUnresolvedImportedDependecies(module1: ModRefId, levels: Level[], dep: ReflectiveDependency) {
+    let found = false;
+    for (const level of levels) {
+      const importObj = this.appMetadataMap.get(module1)?.importedTokensMap[`per${level}`].get(dep.token);
+      if (importObj) {
+        found = true;
+        const { modRefId: modRefId2, providers } = importObj;
+
+        // Loop for multi providers.
+        for (const provider of providers) {
+          this.addToUnfinishedSearchDependecies(modRefId2, provider);
+          found = !this.hasUnresolvedDependecies(modRefId2, provider, levels);
+          this.deleteFromUnfinishedSearchDependecies(modRefId2, provider);
+          if (!found) {
+            return true;
+          }
+        }
+        break;
+      }
+    }
+
+    if (!found && dep.required) {
+      return true;
+    }
+    return false;
+  }
+
+  protected throwError(meta: NormalizedMeta, provider: Provider, path: any[], token: any, levels: Level[]) {
+    path = [provider, ...path, token];
+    const strPath = getTokens(path)
+      .map((t) => t.name || t)
+      .join(' -> ');
+
+    const levelsPath = levels
+      .concat('App' as any)
+      .map((level) => `providersPer${level}`)
+      .join(', ');
+    const partMsg = path.length > 1 ? `(${strPath}, searching in ${levelsPath})` : levelsPath;
+    // this.log.showProvidersInLogs(this, meta.name, meta.providersPerReq, meta.providersPerRou, meta.providersPerMod);
+
+    this.errorMediator.throwNoProviderDuringResolveImports(meta.name, token.name || token, partMsg);
+  }
+
+  protected getDependencies(provider: Provider) {
+    const deps = getDependencies(provider);
+
+    const defaultTokens = [
+      ...getTokens([
+        //
+        ...defaultProvidersPerApp,
+        // ...defaultProvidersPerReq,
+      ]),
+      Injector,
+      BaseAppOptions,
+    ];
+
+    return deps.filter((d) => !defaultTokens.includes(d.token));
+  }
+
+  protected addToUnfinishedSearchDependecies(module: ModRefId, provider: Provider) {
+    const index = this.unfinishedSearchDependecies.findIndex(([m, p]) => m === module && p === provider);
+    if (index != -1) {
+      this.throwCircularDependencies(index);
+    }
+    this.unfinishedSearchDependecies.push([module, provider]);
+  }
+
+  protected deleteFromUnfinishedSearchDependecies(module: ModRefId, provider: Provider) {
+    const index = this.unfinishedSearchDependecies.findIndex(([m, p]) => m === module && p === provider);
+    this.unfinishedSearchDependecies.splice(index, 1);
+  }
+
+  protected throwCircularDependencies(index: number) {
+    const items = this.unfinishedSearchDependecies;
+    const prefixChain = items.slice(0, index);
+    const circularChain = items.slice(index);
+
+    const prefixNames = prefixChain
+      .map(([m, p]) => {
+        const debugModuleName = getDebugClassName(m);
+        return `[${getProviderName(p)} in ${debugModuleName}]`;
+      })
+      .join(' -> ');
+
+    const [modRefId, provider] = items[index];
+    let circularNames = circularChain
+      .map(([m, p]) => {
+        const debugModuleName = getDebugClassName(m);
+        return `[${getProviderName(p)} in ${debugModuleName}]`;
+      })
+      .join(' -> ');
+
+    const debugModuleName = getDebugClassName(modRefId);
+    circularNames += ` -> [${getProviderName(provider)} in ${debugModuleName}]`;
+    let msg = `Detected circular dependencies: ${circularNames}.`;
+    if (prefixNames) {
+      msg += ` It is started from ${prefixNames}.`;
+    }
+    throw new Error(msg);
+  }
+}
