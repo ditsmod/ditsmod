f448584984851700c14b52cdd84f1421226c5b95
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-07-12 00:16:36 +0300

    fix(authjs): migration `CorsModule` to alpha.4

--- packages/authjs/src/authjs.module.ts
+++ packages/authjs/src/authjs.module.ts
@@ -1,5 +1,5 @@
 import { featureModule, isProvider, ModuleWithParams, Provider } from '@ditsmod/core';
-import { RoutesExtension, RestModule, UseInterceptorExtension } from '@ditsmod/rest';
+import { RoutesExtension, RestModule, UseInterceptorExtension, addRest } from '@ditsmod/rest';
 import { BodyParserModule, BodyParserExtension } from '@ditsmod/body-parser';
 
 import { AUTHJS_SESSION } from './constants.js';
@@ -14,11 +14,14 @@ import { AuthjsConfig } from './authjs.config.js';
  *
  * [1]: https://authjs.dev/
  */
+@addRest({
+  providersPerRou: [{ token: AuthjsGuard, useClass: AuthjsPerRouGuard }],
+  providersPerReq: [AuthjsGuard, { token: AUTHJS_SESSION }],
+  exports: [AUTHJS_SESSION, AuthjsGuard],
+})
 @featureModule({
   imports: [RestModule, BodyParserModule],
   providersPerMod: [AuthjsLogMediator],
-  providersPerRou: [{ token: AuthjsGuard, useClass: AuthjsPerRouGuard }],
-  providersPerReq: [AuthjsGuard, { token: AUTHJS_SESSION }],
   extensions: [
     {
       extension: AuthjsExtension,
@@ -27,7 +30,7 @@ import { AuthjsConfig } from './authjs.config.js';
       exportOnly: true,
     },
   ],
-  exports: [AUTHJS_SESSION, AuthjsGuard, BodyParserModule],
+  exports: [BodyParserModule],
 })
 export class AuthjsModule {
   /**
