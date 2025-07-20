12a0092c84cbca2e58e28bf91f179cb7cb07ebd5
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-07-20 18:50:00 +0300

    chore(root,auth): fixed test and debug configs

--- packages/authjs/jest.config.ts
+++ packages/authjs/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/src/'],
-  setupFiles: ['<rootDir>/dist-e2e/test-setup.js']
+  // setupFiles: ['<rootDir>/dist-e2e/test-setup.js']
 };
 
 export default config;
