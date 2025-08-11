20417459cc4d7554007dcd5190558cf1d34244c6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-11 16:43:44 +0300

    fix(examples): fixed example 9

--- examples/09-one-extension/src/app/app.module.ts
+++ examples/09-one-extension/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { Providers, rootModule } from '@ditsmod/core';
-import { PreRouterExtension, initRest } from '@ditsmod/rest';
+import { PreRouterExtension, RoutesExtension, initRest } from '@ditsmod/rest';
 
 import { HelloWorldController } from './hello-world.controller.js';
 import { MyExtension } from './my-extension.js';
 
 @initRest({
   providersPerApp: new Providers().useLogConfig({ level: 'info' }),
-  extensions: [{ extension: MyExtension, beforeExtensions: [PreRouterExtension] }],
+  extensions: [{ extension: MyExtension, afterExtensions: [RoutesExtension], beforeExtensions: [PreRouterExtension] }],
   controllers: [HelloWorldController],
 })
 @rootModule()
