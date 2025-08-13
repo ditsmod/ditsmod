414cd2ed5e8fd8d5121479c4308d562b9bbfd2dc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-13 21:55:10 +0300

    fix(examples): fixed example 11

--- examples/11-override-core-log-messages/src/app/modules/some.module.ts
+++ examples/11-override-core-log-messages/src/app/modules/some.module.ts
@@ -1,11 +1,9 @@
 import { featureModule } from '@ditsmod/core';
-import { RestModule } from '@ditsmod/rest';
 
 import { SomeLogMediator } from './some/some-log-mediator.js';
 import { SomeService } from './some/some.service.js';
 
 @featureModule({
-  imports: [RestModule],
   providersPerMod: [SomeLogMediator, SomeService],
   exports: [SomeService]
 })
