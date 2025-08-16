448fe4f027c2508c76043594dcd3d36dbf593b22
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-16 11:52:41 +0300

    refactor(core): removed `ShallowImportedModules`

--- packages/core/src/init/types.ts
+++ packages/core/src/init/types.ts
@@ -3,5 +3,4 @@ import { AnyFn, AnyObj, ModRefId } from '#types/mix.js';
 import { BaseMeta } from '#types/base-meta.js';
 
 export type ShallowImportsBase = Map<ModRefId, MetadataPerMod1>;
-export type ShallowImports = Map<ModRefId, MetadataPerMod1 & { shallowImportedModules: ShallowImportedModules }>;
-export type ShallowImportedModules<T extends AnyObj = AnyObj> = Map<AnyFn, { baseMeta: BaseMeta } & T>;
+export type ShallowImports<T extends AnyObj = AnyObj> = Map<ModRefId, MetadataPerMod1 & { shallowImportedModules: Map<AnyFn, { baseMeta: BaseMeta } & T> }>;
