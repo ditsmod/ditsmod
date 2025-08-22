c6d2a0406417cfaad8bf23ca01a3c319c1b29349
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-22 18:00:56 +0300

    refactor(examples): removed unused import

--- examples/06-body-parser/src/app/modules/routed/some.module.ts
+++ examples/06-body-parser/src/app/modules/routed/some.module.ts
@@ -1,6 +1,6 @@
 import { featureModule } from '@ditsmod/core';
 import { initRest } from '@ditsmod/rest';
-import { BodyParserModule, MulterExtendedOptions } from '@ditsmod/body-parser';
+import { MulterExtendedOptions } from '@ditsmod/body-parser';
 
 import { SomeController } from './some/some.controller.js';
 import { CtxController } from './some/some-ctx.controller.js';
@@ -8,7 +8,6 @@ import { CtxController } from './some/some-ctx.controller.js';
 const multerOptions: MulterExtendedOptions = { limits: { files: 20 }, errorLogLevel: 'debug' };
 
 @initRest({
-  imports: [BodyParserModule],
   providersPerMod: [{ token: MulterExtendedOptions, useValue: multerOptions }],
   controllers: [SomeController, CtxController],
 })
