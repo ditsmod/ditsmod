c5c80c35f75febb18602520ca3306224eb123046
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-27 00:55:36 +0300

    format(experiments): a bit fix import

--- experiments/01-trpc/src/app/app.module.ts
+++ experiments/01-trpc/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { PostModule } from '#modules/post/post.module.js';
 import { AuthModule } from '#modules/auth/auth.module.js';
 import { AuthService } from '#modules/auth/auth.service.js';
 import { MessageModule } from '#modules/message/message.module.js';
-import { AuthController } from './modules/auth/auth.controller.js';
-import { MessageController } from './modules/message/message.controller.js';
-import { PostController } from './modules/post/post.controller.js';
+import { AuthController } from '#modules/auth/auth.controller.js';
+import { MessageController } from '#modules/message/message.controller.js';
+import { PostController } from '#modules/post/post.controller.js';
 
 @rootModule({
   imports: [PostModule, AuthModule, MessageModule],
