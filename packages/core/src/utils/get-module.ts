8b21f2b0721188a18629514a166edbedf461cf23
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-29 15:15:12 +0300

    fix(core): fixed `getModule()`

--- packages/core/src/utils/get-module.ts
+++ packages/core/src/utils/get-module.ts
@@ -1,6 +1,8 @@
-import { ModRefId, ModuleType } from '#types/mix.js';
+import { resolveForwardRef } from '#di';
+import { AnyObj, ModRefId, ModuleType } from '#types/mix.js';
 import { isModuleWithParams } from './type-guards.js';
 
-export function getModule(modRefId: ModRefId) {
-  return (isModuleWithParams(modRefId) ? modRefId.module : modRefId) as ModuleType;
+export function getModule<T extends AnyObj>(modRefId: ModRefId<T>): ModuleType<T> {
+  modRefId = resolveForwardRef(modRefId);
+  return isModuleWithParams(modRefId) ? resolveForwardRef(modRefId.module) : modRefId;
 }
