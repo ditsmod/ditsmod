bf152512b28d748862aa01a5fcfb50edae413d94
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-08-30 20:47:42 +0300

    refactor(trpc): removed `SystemLogMediator` and `opts` property from `PreRouter`

--- packages/trpc/src/pre-router.ts
+++ packages/trpc/src/pre-router.ts
@@ -1,21 +1,17 @@
-import { injectable, Injector, SystemLogMediator } from '@ditsmod/core';
+import { injectable, Injector } from '@ditsmod/core';
 import { createHTTPHandler } from '@trpc/server/adapters/standalone';
 
 import { TRPC_OPTS } from './constants.js';
-import { RequestListener, TrpcOpts } from './types.js';
+import { RequestListener } from './types.js';
 
 @injectable()
 export class PreRouter {
-  protected opts: TrpcOpts;
   requestListener: RequestListener;
 
-  constructor(
-    protected injectorPerApp: Injector,
-    protected systemLogMediator: SystemLogMediator,
-  ) {}
+  constructor(protected injectorPerApp: Injector) {}
 
   setTrpcRequestListener() {
-    this.opts = this.injectorPerApp.get(TRPC_OPTS);
-    this.requestListener = createHTTPHandler(this.opts) as RequestListener;
+    const opts = this.injectorPerApp.get(TRPC_OPTS);
+    this.requestListener = createHTTPHandler(opts) as RequestListener;
   }
 }
