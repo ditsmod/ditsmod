4ebd9d2ec776ab798362e8b4fbc3a49bc21d5a49
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-09-03 15:26:59 +0300

    docs(trpc): rewrite note for `opts` decorator

--- packages/trpc/src/decorators/opts.ts
+++ packages/trpc/src/decorators/opts.ts
@@ -2,8 +2,7 @@ import { inject, InjectTransformResult, makeParamDecorator } from '@ditsmod/core
 import { TRPC_OPTS } from '../constants.js';
 
 /**
- * Intended for use in service method parameters when this parameter should return
- * context data such as `ctx`, `input`, `path` and `signal`.
+ * Intended for use in service method parameters to provide context data such as `ctx`, `input`, `path`, and `signal`.
  */
 export const opts = makeParamDecorator(() => {
   return { token: TRPC_OPTS } satisfies InjectTransformResult;
