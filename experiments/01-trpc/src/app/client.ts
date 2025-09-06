d2cbd64aef8e82037fd2bedc7b21450fd48e64e7
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-09-06 21:13:14 +0300

    refactor(experiments): removed top level wraper for client

--- experiments/01-trpc/src/app/client.ts
+++ experiments/01-trpc/src/app/client.ts
@@ -7,67 +7,63 @@ import type { MessageRouter } from './modules/message/message.module.js';
 import { AuthRouter } from './modules/auth/auth.module.js';
 import { inspect } from 'node:util';
 
-async function main() {
-  const url = 'http://localhost:2021/trpc';
-  const trpc = createTRPCClient<AnyTRPCRouter>({
-    links: [
-      function callback() {
-        return ({ op, next }) => {
-          // console.log('->', op.type, op.path, op.input);
+const url = 'http://localhost:2021/trpc';
+const trpc = createTRPCClient<AnyTRPCRouter>({
+  links: [
+    function callback() {
+      return ({ op, next }) => {
+        // console.log('->', op.type, op.path, op.input);
 
-          return next(op).pipe(
-            tap({
-              next(result) {
-                console.log(
-                  `${op.path}.${op.type}(${inspect(op.input, false, 2)}) ->`,
-                  `\x1b[34m ${inspect(result.result.data, false, 2)}\x1b[0m`,
-                );
-              },
-            }),
-          );
-        };
-      },
-      httpBatchLink({ url }),
-    ],
-  });
+        return next(op).pipe(
+          tap({
+            next(result) {
+              console.log(
+                `${op.path}.${op.type}(${inspect(op.input, false, 2)}) ->`,
+                `\x1b[34m ${inspect(result.result.data, false, 2)}\x1b[0m`,
+              );
+            },
+          }),
+        );
+      };
+    },
+    httpBatchLink({ url }),
+  ],
+});
 
-  const messageClient = trpc as TRPCClient<MessageRouter>;
+const messageClient = trpc as TRPCClient<MessageRouter>;
 
-  // parallel queries
-  await Promise.all([
-    //
-    messageClient.hello.query(),
-    messageClient.hello.query('client'),
-  ]);
-  await messageClient.message.addMessage.mutate('one more message!');
-  await messageClient.message.listMessages.query();
+// parallel queries
+await Promise.all([
+  //
+  messageClient.hello.query(),
+  messageClient.hello.query('client'),
+]);
+await messageClient.message.addMessage.mutate('one more message!');
+await messageClient.message.listMessages.query();
 
-  const trpcPost = trpc as TRPCClient<PostRouter>;
-  await trpcPost.post.createPost.mutate({ title: 'hello client' });
-  await trpcPost.post.listPosts.query();
-  await trpcPost.post.comments.listComments.query();
-  const authPublicClient = trpc as TRPCClient<AuthRouter>;
+const trpcPost = trpc as TRPCClient<PostRouter>;
+await trpcPost.post.createPost.mutate({ title: 'hello client' });
+await trpcPost.post.listPosts.query();
+await trpcPost.post.comments.listComments.query();
+const authPublicClient = trpc as TRPCClient<AuthRouter>;
 
-  try {
-    await authPublicClient.admin.secret.query();
-  } catch (cause) {
-    // will fail
-  }
-  const authedClient = createTRPCClient<AnyTRPCRouter>({
-    links: [
-      // loggerLink(),
-      httpBatchLink({
-        url,
-        headers: () => ({
-          authorization: 'secret',
-        }),
-      }),
-    ],
-  });
-
-  const authClient = authedClient as TRPCClient<AuthRouter>;
-  await authClient.admin.secret.query();
-  console.log('👌 should be a clean exit if everything is working right');
+try {
+  await authPublicClient.admin.secret.query();
+} catch (cause) {
+  // will fail
 }
+const authedClient = createTRPCClient<AnyTRPCRouter>({
+  links: [
+    // loggerLink(),
+    httpBatchLink({
+      url,
+      headers: () => ({
+        authorization: 'secret',
+      }),
+    }),
+  ],
+});
 
-void main();
+const authClient = authedClient as TRPCClient<AuthRouter>;
+await authClient.admin.secret.query();
+console.log('👌 should be a clean exit if everything is working right');
