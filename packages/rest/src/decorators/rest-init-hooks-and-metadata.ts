97e70e39d5fa13b279945e296defa5e0b8d26edc
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-09-17 17:01:14 +0300

    refactor(rest): restore `importModulesDeep()`

--- packages/rest/src/decorators/rest-init-hooks-and-metadata.ts
+++ packages/rest/src/decorators/rest-init-hooks-and-metadata.ts
@@ -3,10 +3,11 @@ import { makeClassDecorator, InitHooks, ModRefId, BaseMeta, InitDecorator, Provi
 import { RestInitRawMeta, RestModuleParams } from '#init/rest-init-raw-meta.js';
 import { RestModuleNormalizer } from '#init/rest-module-normalizer.js';
 import { RestShallowModulesImporter } from '#init/rest-shallow-modules-importer.js';
-import { ExportGlobalProvidersConfig, ImportModulesShallowConfig, RestShallowImports } from '#init/types.js';
+import { DeepModulesImporterConfig, ExportGlobalProvidersConfig, ImportModulesShallowConfig, RestShallowImports } from '#init/types.js';
 import { RestModRefId, RestInitMeta } from '#init/rest-init-meta.js';
 import { RestGlobalProviders } from '#types/types.js';
 import { RestModule } from '#init/rest.module.js';
+import { RestDeepModulesImporter } from '#init/rest-deep-modules-importer.js';
 
 export const restRootModule: InitDecorator<RestInitRawMeta, RestModuleParams, RestInitMeta> = makeClassDecorator(
   transformRootMeta,
@@ -36,6 +37,10 @@ export class RestInitHooks extends InitHooks<RestInitRawMeta> {
 
   override importModulesShallow(config: ImportModulesShallowConfig): Map<ModRefId, RestShallowImports> {
     return new RestShallowModulesImporter().importModulesShallow(config);
+  }
+
+  override importModulesDeep(config: DeepModulesImporterConfig) {
+    return new RestDeepModulesImporter(config).importModulesDeep();
   }
 
   override getProvidersToOverride(meta: RestInitMeta): Provider[][] {
--- packages/rest/src/init/rest-deep-modules-importer.ts
+++ packages/rest/src/init/rest-deep-modules-importer.ts
@@ -7,15 +7,15 @@ import {
   ShallowImports,
 } from '@ditsmod/core';
 
-import { DeepModulesImporterConfig, RestShallowImports } from './types.js';
+import { DeepModulesImporterConfig, RestMetadataPerMod2, RestProvidersOnly, RestShallowImports } from './types.js';
+import { defaultProvidersPerRou } from '#providers/default-providers-per-rou.js';
+import { defaultProvidersPerReq } from '#providers/default-providers-per-req.js';
 
 /**
  * By analyzing the dependencies of the providers returned by `ShallowModulesImporter`,
  * recursively collects providers for them from the corresponding modules.
  */
 export class RestDeepModulesImporter {
-  protected tokensPerApp: any[];
-
   protected shallowImports: RestShallowImports;
   protected moduleManager: ModuleManager;
   protected shallowImportsMap: Map<ModRefId, ShallowImports>;
@@ -37,5 +37,18 @@ export class RestDeepModulesImporter {
     this.shallowImportsMap = shallowImportsMap;
     this.providersPerApp = providersPerApp;
     this.log = log;
+  }
+
+  importModulesDeep(): RestMetadataPerMod2 | undefined {
+    const { guards1, prefixPerMod, meta, applyControllers } = this.shallowImports;
+    meta.providersPerRou.unshift(...defaultProvidersPerRou);
+    meta.providersPerReq.unshift(...defaultProvidersPerReq);
+    return {
+      baseMeta: this.shallowImports.baseMeta,
+      meta,
+      guards1,
+      prefixPerMod,
+      applyControllers,
+    };
   }
 }
