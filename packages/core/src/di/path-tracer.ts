b23dc63e9c22635f143fd1f86fb095435cc43faa
KostyaTretyak <ktretiak.in.ua@gmail.com>
2025-12-24 01:40:52 +0200

    refactor(core): refactoring `PathTracer`

--- packages/core/src/di/path-tracer.ts
+++ packages/core/src/di/path-tracer.ts
@@ -1,16 +1,28 @@
 import { CyclicDependency } from '#errors';
 import { getProviderName } from '#utils/get-provider-name.js';
-import { Injector } from './injector.js';
+import { type Injector } from './injector.js';
 
 export interface PathItem {
   token: any;
   injector: Injector | null | undefined;
+}
+export interface InjectorItems {
+  injector: Injector | null | undefined;
+  injectorNumber: number;
+}
+export interface MergedItem {
+  token: any;
+  injectors: InjectorItems[];
 }
 /**
  * Used by the {@link Injector} to trace when resolving provider dependencies.
  */
 export class PathTracer {
   protected items: PathItem[] = [];
+  /**
+   * The hierarchy of injectors has one level.
+   */
+  protected hasOneLevel: boolean;
 
   addItem(token: any, injector: Injector | null | undefined) {
     if (this.items.some((path) => path.injector === injector && path.token === token)) {
@@ -31,59 +43,45 @@ export class PathTracer {
    * 2. __`tokenName in injectorLevel`__ (if there are multiple levels of injector hierarchy).
    */
   get path() {
-    let prevInjector: Injector | null | undefined;
-    let level = 0;
     return this.mergeItems().map((item) => {
-      const tokenName = getProviderName(item.token);
       const injectorStr = item.injectors
         .map((inj) => {
-          if (inj?.level) {
-            prevInjector = inj;
-            return inj.level;
-          } else {
-            if (this.hasOneLevel()) {
-              return '';
-            }
-            if (prevInjector !== inj) {
-              ++level;
-              prevInjector = inj;
-            }
-            return `injector${level}`;
-          }
+          return inj.injector?.level ? inj.injector?.level : `injector${inj.injectorNumber}`;
         })
         .join(' >> ');
 
-      return injectorStr ? `[${tokenName} in ${injectorStr}]` : tokenName;
+      const tokenName = getProviderName(item.token);
+      return this.hasOneLevel ? tokenName : `[${tokenName} in ${injectorStr}]`;
     });
   }
 
+  /**
+   * If identical tokens follow each other in a trace, their injectors
+   * merge into a single array for that tokens.
+   */
   protected mergeItems() {
+    const mergedItems: MergedItem[] = [];
+    let prevInjector: Injector | null | undefined;
     let prevToken: any;
-    const mergedItems: { token: any; injectors: (Injector | null | undefined)[] }[] = [];
+    let injectorNumber = 0;
     this.items.forEach((item) => {
+      if (prevInjector !== item.injector) {
+        injectorNumber++;
+        prevInjector = item.injector;
+      }
       if (prevToken === item.token) {
         const prevItem = mergedItems.at(-1)!;
-        prevItem.injectors.unshift(item.injector);
+        prevItem.injectors.unshift({ injector: item.injector, injectorNumber });
       } else {
-        mergedItems.push({ token: item.token, injectors: [item.injector] });
+        mergedItems.push({
+          token: item.token,
+          injectors: [{ injector: item.injector, injectorNumber }],
+        });
+        prevToken = item.token;
       }
-      prevToken = item.token;
     });
+    this.hasOneLevel = injectorNumber == 1;
 
     return mergedItems;
-  }
-
-  protected hasOneLevel() {
-    let oneLevel = true;
-    let prevInjector: Injector | null | undefined = null;
-    for (let i = 0; i < this.items.length; i++) {
-      const curInjector = this.items[i].injector;
-      if (prevInjector && curInjector && curInjector !== prevInjector) {
-        oneLevel = false;
-        break;
-      }
-      prevInjector = curInjector;
-    }
-    return oneLevel;
   }
 }
