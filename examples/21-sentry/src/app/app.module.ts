1b10fadb0c90682979a46d6935a54d7cb9e7711c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2026-07-15 04:44:28 +0300

    fix(examples): fixed 21 example

--- examples/21-sentry/src/app/app.module.ts
+++ examples/21-sentry/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { ProviderBuilder } from '@ditsmod/core';
-import { restRootModule, HttpErrorHandler } from '@ditsmod/rest';
+import { restRootModule, HttpErrorHandler, RequestDispatcher } from '@ditsmod/rest';
 import { SentryModule, SentryOptions } from '@ditsmod/sentry';
 
 import { ExampleController } from './example.controller.js';
@@ -10,6 +10,7 @@ import { ExampleController } from './example.controller.js';
   providersPerMod: new ProviderBuilder().useValue(SentryOptions, {
     capture4xx: false, // Default is false, change to true if you want to capture 4xx client errors
   }),
+  resolvedCollisionPerApp: [[RequestDispatcher, SentryModule]],
   resolvedCollisionPerRou: [[HttpErrorHandler, SentryModule]],
 })
 export class AppModule {}
